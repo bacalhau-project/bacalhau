2308736e73c5e5f03f93cf09df007268d790ab5c
Kai Davenport <kaiyadavenport@gmail.com>
2022-05-11 15:06:07 +0100

    table based tests for storage driver

--- pkg/test/docker/executor_test.go
+++ pkg/test/docker/executor_test.go
@@ -3,56 +3,82 @@ package docker
 import (
 	"testing"
 
-	"github.com/filecoin-project/bacalhau/pkg/devstack"
 	_ "github.com/filecoin-project/bacalhau/pkg/logger"
-	"github.com/filecoin-project/bacalhau/pkg/storage"
-	"github.com/filecoin-project/bacalhau/pkg/storage/ipfs/api_copy"
-	"github.com/filecoin-project/bacalhau/pkg/storage/ipfs/fuse_docker"
 	"github.com/filecoin-project/bacalhau/pkg/types"
 )
 
 func TestIpfsDockerExecutor(t *testing.T) {
 
-	EXAMPLE_TEXT := `hello world`
 	MOUNT_PATH := "/data/file.txt"
+	HELLO_WORLD := `hello world`
+	FRUITS := `apple
+orange
+pineapple
+pear
+peach
+cherry
+kiwi is delicious
+strawberry
+lemon
+raspberry
+`
 
-	fuseStorageDriverFactory := func(stack *devstack.DevStack_IPFS) (storage.StorageProvider, error) {
-		return fuse_docker.NewIpfsFuseDocker(stack.Ctx, stack.Nodes[0].IpfsNode.ApiAddress())
-	}
-
-	apiCopyStorageDriverFactory := func(stack *devstack.DevStack_IPFS) (storage.StorageProvider, error) {
-		return api_copy.NewIpfsApiCopy(stack.Ctx, stack.Nodes[0].IpfsNode.ApiAddress())
-	}
-
-	getDataTest := func(stack *devstack.DevStack_IPFS) *IDataBasedTest {
-		return dataBasedTestSingleFile(
-			t,
-			stack,
-			EXAMPLE_TEXT,
-			MOUNT_PATH,
-			"stdout",
-		)
-	}
-
-	jobSpec := types.JobSpecVm{
-		Image: "ubuntu",
-		Entrypoint: []string{
-			"cat",
-			MOUNT_PATH,
+	tests := []struct {
+		fileContents   string
+		mountPath      string
+		expectedOutput string
+		expectedMode   IExpectedMode
+		getJobSpec     IGetJobSpec
+	}{
+		{
+			fileContents:   HELLO_WORLD,
+			mountPath:      MOUNT_PATH,
+			expectedOutput: HELLO_WORLD,
+			expectedMode:   ExpectedModeEquals,
+			getJobSpec: func(outputMode IOutputMode) types.JobSpecVm {
+				return types.JobSpecVm{
+					Image: "ubuntu",
+					Entrypoint: []string{
+						"cat",
+						MOUNT_PATH,
+					},
+				}
+			},
+		},
+		{
+			fileContents:   FRUITS,
+			mountPath:      MOUNT_PATH,
+			expectedOutput: "kiwi is delicious",
+			expectedMode:   ExpectedModeContains,
+			getJobSpec: func(outputMode IOutputMode) types.JobSpecVm {
+				return types.JobSpecVm{
+					Image: "ubuntu",
+					Entrypoint: []string{
+						"grep",
+						"kiwi",
+						MOUNT_PATH,
+					},
+				}
+			},
 		},
 	}
 
-	dockerExecutorStorageTest(
-		t,
-		fuseStorageDriverFactory,
-		getDataTest,
-		jobSpec,
-	)
+	for _, test := range tests {
 
-	dockerExecutorStorageTest(
-		t,
-		apiCopyStorageDriverFactory,
-		getDataTest,
-		jobSpec,
-	)
+		dockerExecutorStorageTest(
+			t,
+			singleFileSetupStorage(
+				t,
+				test.fileContents,
+				test.mountPath,
+			),
+			singleFileResultsChecker(
+				t,
+				test.expectedOutput,
+				test.expectedMode,
+			),
+			test.getJobSpec,
+		)
+
+	}
 }
--- pkg/test/docker/testutils.go
+++ pkg/test/docker/testutils.go
@@ -3,6 +3,7 @@ package docker
 import (
 	"fmt"
 	"os"
+	"strings"
 	"testing"
 
 	"github.com/filecoin-project/bacalhau/pkg/devstack"
@@ -10,28 +11,70 @@ import (
 	"github.com/filecoin-project/bacalhau/pkg/executor/docker"
 	_ "github.com/filecoin-project/bacalhau/pkg/logger"
 	"github.com/filecoin-project/bacalhau/pkg/storage"
+	"github.com/filecoin-project/bacalhau/pkg/storage/ipfs/api_copy"
+	"github.com/filecoin-project/bacalhau/pkg/storage/ipfs/fuse_docker"
 	"github.com/filecoin-project/bacalhau/pkg/test/ipfs"
 	"github.com/filecoin-project/bacalhau/pkg/types"
+	"github.com/rs/zerolog/log"
 	"github.com/stretchr/testify/assert"
+)
+
+type IExpectedMode int
+
+const (
+	ExpectedModeEquals IExpectedMode = iota
+	ExpectedModeContains
+)
+
+type IOutputMode int
+
+const (
+	OutputModeStdout IOutputMode = iota
+	OutputModeVolume
 )
 
 const TEST_STORAGE_DRIVER_NAME = "testdriver"
 
-type IDataBasedTest struct {
-	setupStorage func() ([]types.StorageSpec, error)
-	checkResults func(resultsDir string)
+type IGetStorageDriver func(stack *devstack.DevStack_IPFS) (storage.StorageProvider, error)
+type ISetupStorage func(stack *devstack.DevStack_IPFS) ([]types.StorageSpec, error)
+type ICheckResults func(resultsDir, resultsPath string)
+type IGetJobSpec func(outputMode IOutputMode) types.JobSpecVm
+
+/*
+
+	Storage Drivers
+
+*/
+func fuseStorageDriverFactory(stack *devstack.DevStack_IPFS) (storage.StorageProvider, error) {
+	return fuse_docker.NewIpfsFuseDocker(stack.Ctx, stack.Nodes[0].IpfsNode.ApiAddress())
 }
 
-// a storage setup function that writes the given string to a single file
-func dataBasedTestSingleFile(
+func apiCopyStorageDriverFactory(stack *devstack.DevStack_IPFS) (storage.StorageProvider, error) {
+	return api_copy.NewIpfsApiCopy(stack.Ctx, stack.Nodes[0].IpfsNode.ApiAddress())
+}
+
+var STORAGE_DRIVER_FACTORIES = []IGetStorageDriver{
+	fuseStorageDriverFactory,
+	apiCopyStorageDriverFactory,
+}
+
+var OUTPUT_MODES = []IOutputMode{
+	OutputModeStdout,
+	OutputModeVolume,
+}
+
+/*
+
+	Results Checkers
+
+*/
+
+func singleFileSetupStorage(
 	t *testing.T,
-	stack *devstack.DevStack_IPFS,
 	fileContents string,
 	mountPath string,
-	resultsPath string,
-) *IDataBasedTest {
-
-	setupStorage := func() ([]types.StorageSpec, error) {
+) ISetupStorage {
+	return func(stack *devstack.DevStack_IPFS) ([]types.StorageSpec, error) {
 		fileCid, err := stack.AddTextToNodes(1, []byte(fileContents))
 		assert.NoError(t, err)
 		inputStorageSpecs := []types.StorageSpec{
@@ -44,72 +87,99 @@ func dataBasedTestSingleFile(
 		return inputStorageSpecs, nil
 	}
 
-	checkResults := func(resultsDirectory string) {
-		stdout, err := os.ReadFile(fmt.Sprintf("%s/%s", resultsDirectory, resultsPath))
-		assert.NoError(t, err)
-		assert.Equal(t, string(stdout), fileContents)
-	}
+}
 
-	return &IDataBasedTest{
-		setupStorage: setupStorage,
-		checkResults: checkResults,
+func singleFileResultsChecker(
+	t *testing.T,
+	expectedString string,
+	expectedMode IExpectedMode,
+) ICheckResults {
+	return func(resultsDirectory, resultsPath string) {
+		resultsContent, err := os.ReadFile(fmt.Sprintf("%s/%s", resultsDirectory, resultsPath))
+		assert.NoError(t, err)
+
+		log.Debug().Msgf("resultsContent: %s", resultsContent)
+
+		if expectedMode == ExpectedModeEquals {
+			assert.Equal(t, string(resultsContent), expectedString)
+		} else if expectedMode == ExpectedModeContains {
+			assert.True(t, strings.Contains(string(resultsContent), expectedString))
+		} else {
+			t.Fail()
+		}
 	}
 }
 
+/*
+
+	Executor Job Tests
+
+*/
+
+// for each test we iterate over:
+//  * storage drivers (ipfs_fuse, ipfs_api_copy)
+//  * output types (stdout, named volume)
+
 func dockerExecutorStorageTest(
 	t *testing.T,
-	getStorageDriver func(stack *devstack.DevStack_IPFS) (storage.StorageProvider, error),
-	getDataTest func(stack *devstack.DevStack_IPFS) *IDataBasedTest,
-	jobSpec types.JobSpecVm,
+	setupStorage ISetupStorage,
+	checkResults ICheckResults,
+	getJobSpec IGetJobSpec,
 ) {
 
-	stack, cancelFunction := ipfs.SetupTest(
-		t,
-		1,
-	)
+	runTest := func(
+		getStorageDriver IGetStorageDriver,
+		outputMode IOutputMode,
+	) {
+		stack, cancelFunction := ipfs.SetupTest(
+			t,
+			1,
+		)
 
-	defer ipfs.TeardownTest(stack, cancelFunction)
+		defer ipfs.TeardownTest(stack, cancelFunction)
 
-	storageDriver, err := getStorageDriver(stack)
-	assert.NoError(t, err)
-
-	dockerExecutor, err := docker.NewDockerExecutor(stack.Ctx, "dockertest", map[string]storage.StorageProvider{
-		TEST_STORAGE_DRIVER_NAME: storageDriver,
-	})
-	assert.NoError(t, err)
-
-	dataBasedTest := getDataTest(stack)
-	assert.NoError(t, err)
-
-	inputStorageList, err := dataBasedTest.setupStorage()
-	assert.NoError(t, err)
-
-	job := &types.Job{
-		Id:    "test-job",
-		Owner: "test-owner",
-		Spec: &types.JobSpec{
-			Engine: executor.EXECUTOR_DOCKER,
-			Vm:     jobSpec,
-			Inputs: inputStorageList,
-		},
-		Deal: &types.JobDeal{
-			Concurrency:   1,
-			AssignedNodes: []string{},
-		},
-	}
-
-	isInstalled, err := dockerExecutor.IsInstalled()
-	assert.NoError(t, err)
-	assert.True(t, isInstalled)
-
-	for _, inputStorageSpec := range inputStorageList {
-		hasStorage, err := dockerExecutor.HasStorage(inputStorageSpec)
+		storageDriver, err := getStorageDriver(stack)
 		assert.NoError(t, err)
-		assert.True(t, hasStorage)
+
+		dockerExecutor, err := docker.NewDockerExecutor(stack.Ctx, "dockertest", map[string]storage.StorageProvider{
+			TEST_STORAGE_DRIVER_NAME: storageDriver,
+		})
+		assert.NoError(t, err)
+
+		inputStorageList, err := setupStorage(stack)
+		assert.NoError(t, err)
+
+		job := &types.Job{
+			Id:    "test-job",
+			Owner: "test-owner",
+			Spec: &types.JobSpec{
+				Engine: executor.EXECUTOR_DOCKER,
+				Vm:     getJobSpec(outputMode),
+				Inputs: inputStorageList,
+			},
+			Deal: &types.JobDeal{
+				Concurrency:   1,
+				AssignedNodes: []string{},
+			},
+		}
+
+		isInstalled, err := dockerExecutor.IsInstalled()
+		assert.NoError(t, err)
+		assert.True(t, isInstalled)
+
+		for _, inputStorageSpec := range inputStorageList {
+			hasStorage, err := dockerExecutor.HasStorage(inputStorageSpec)
+			assert.NoError(t, err)
+			assert.True(t, hasStorage)
+		}
+
+		resultsDirectory, err := dockerExecutor.RunJob(job)
+		assert.NoError(t, err)
+
+		checkResults(resultsDirectory, "stdout")
 	}
 
-	resultsDirectory, err := dockerExecutor.RunJob(job)
-	assert.NoError(t, err)
-
-	dataBasedTest.checkResults(resultsDirectory)
+	for _, getStorageDriver := range STORAGE_DRIVER_FACTORIES {
+		runTest(getStorageDriver, OutputModeStdout)
+	}
 }
