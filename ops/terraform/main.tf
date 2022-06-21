607c09e122a44e8f490c223b74906ff3adb3305d
David Aronchick <aronchick@gmail.com>
2022-06-21 22:36:28 +0000

    nginx now works

--- ops/terraform/main.tf
+++ ops/terraform/main.tf
@@ -24,7 +24,14 @@ sudo apt-get install -y docker.io
 # TODO: move this into two systemd units!
 
 # Lay down a very basic web server to report when the node is healthy
-sudo apt-get update && sudo apt-get install -y openssl nmap
+sudo apt-get -y install --no-install-recommends wget gnupg ca-certificates
+wget -O - https://openresty.org/package/pubkey.gpg | sudo apt-key add -
+echo "deb http://openresty.org/package/ubuntu $(lsb_release -sc) main" \
+    | sudo tee /etc/apt/sources.list.d/openresty.list
+sudo apt-get update
+sudo apt-get -y install openresty
+sudo apt-get -y install --no-install-recommends openresty
+
 sudo mkdir -p /var/www/health_checker
 
 sudo tee /var/www/health_checker/livez.sh > /dev/null <<'EOI'
