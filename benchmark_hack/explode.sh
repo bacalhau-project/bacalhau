67bf4ef3bed6d925d2f9aa43fa1df1f1e995920f
Kai Davenport <kaiyadavenport@gmail.com>
2022-06-28 16:29:59 +0100

    xargs concurrency

--- benchmark_hack/explode.sh
+++ benchmark_hack/explode.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -xeuo pipefail
-printf %s\\n {0..99} | xargs -n 1 -P 10 \
-    hyperfine --export-json=run-$(date +%s%N).json ./submit.sh
+printf %s\\n {0..99} | xargs -n 1 -P 2 -i \
+    hyperfine --export-json=run-$(date +%s%N)-{}.json ./submit.sh
