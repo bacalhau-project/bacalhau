b32e71b4ef6e294a423c43336bfc5f13f8273b9b
Kai Davenport <kaiyadavenport@gmail.com>
2022-06-29 15:10:46 +0100

    scripts to test rapidly submitting jobs without waiting for their results

---
+++ benchmark_hack/quick_read.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -xeuo pipefail
+
+while true; do
+  printf %s\\n {0..1000} | xargs -n 1 -P 100 -i \
+    bacalhau --api-port=$API_PORT_0 --api-host=localhost list --wide
+  sleep 1
+done
