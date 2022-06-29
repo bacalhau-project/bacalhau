d66c7c0a7444c7f50805c1f1f8b1e64576700082
Kai Davenport <kaiyadavenport@gmail.com>
2022-06-29 14:31:31 +0100

    ignore benchmark results files

--- benchmark_hack/explode.sh
+++ benchmark_hack/explode.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -xeuo pipefail
+mkdir -p results
 printf %s\\n {0..9999} | xargs -n 1 -P 10 -i \
-    hyperfine --export-json=run-$(date +%s%N)-{}.json ./submit.sh
+    hyperfine --export-json=results/run-$(date +%s%N)-{}.json ./submit.sh
