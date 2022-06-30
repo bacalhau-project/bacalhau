c5e5b1953ce0c12a44b59c94d26b21390ff2ea43
Kai Davenport <kaiyadavenport@gmail.com>
2022-06-30 15:28:31 +0100

    resource usage data type with human friendly parser

--- pkg/resourceusage/types.go
+++ pkg/resourceusage/types.go
@@ -1,9 +1,22 @@
 package resourceusage
 
 // a record for the "amount" of compute resources an entity has / can consume / is using
-// https://github.com/BTBurke/k8sresource strings
-type ResourceUsageData struct {
-	CPU    string `json:"cpu"`
+
+type ResourceUsageConfig struct {
+	// https://github.com/BTBurke/k8sresource string
+	CPU string `json:"cpu"`
+	// github.com/c2h5oh/datasize string
 	Memory string `json:"memory"`
-	Disk   string `json:"disk"`
+	// github.com/c2h5oh/datasize string
+	Disk string `json:"disk"`
+}
+
+// these are the numeric values in bytes for ResourceUsageConfig
+type ResourceUsageData struct {
+	// cpu units
+	CPU float64 `json:"cpu"`
+	// bytes
+	Memory uint64 `json:"memory"`
+	// bytes
+	Disk uint64 `json:"disk"`
 }
