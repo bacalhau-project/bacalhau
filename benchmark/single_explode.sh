a83a7f2073a1da96db7614848a834d1c66256cc2
David Aronchick <aronchick@gmail.com>
2022-07-19 15:22:54 -0700

    now doing performance builds from local

--- benchmark/single_explode.sh
+++ benchmark/single_explode.sh
@@ -3,16 +3,18 @@ set -euo pipefail
 
 export iterationid="$1"
 
-sudo tee ./results/parameters-${RUN_ID}.json > /dev/null <<EOI
+sudo tee "./results/parameters-${RUN_ID}.json" > /dev/null <<EOI
 {
-"TOTAL_JOBS": $TOTAL_JOBS,
-"BATCH_SIZE": $BATCH_SIZE,
-"CONCURRENCY": $CONCURRENCY
+"TOTAL_JOBS": ${TOTAL_JOBS},
+"BATCH_SIZE": ${BATCH_SIZE},
+"CONCURRENCY": ${CONCURRENCY}
 }
 EOI
 
 hyperfine \
+  --export-json="results/run-${RUN_ID}-${iterationid}.json" \
+  --runs "${BATCH_SIZE}" \
   --ignore-failure \
-  --export-json=results/run-${RUN_ID}-$iterationid.json \
-  --runs $BATCH_SIZE \
   "timeout 30s ./submit.sh"
+ 
+  # --show-output \
--- benchmark/submit.sh
+++ benchmark/submit.sh
@@ -4,10 +4,12 @@ set -xeuo pipefail
 # docker run busybox /bin/true
 # /bin/true
 
-ID=$(bacalhau --api-port=$API_PORT_0 --api-host=localhost docker run --concurrency=3 busybox -- /bin/true)
+API_PORT="$(cat /tmp/bacalhau-devstack.port)"
+ 
+ID=$(go run .. --api-port="${API_PORT}" --api-host=localhost docker run --concurrency=3 busybox -- /bin/true 2>&1)
 while true; do
     sleep 0.1
-    if [ $(bacalhau --api-port=$API_PORT_0 --api-host=localhost describe --id=$ID 2>&1|grep "state:"|wc -l) -ne 3 ]; then
+    if [ $(go run .. --api-port="${API_PORT}" --api-host=localhost describe $ID 2>&1|grep "state:"|wc -l) -ne 3 ]; then
         echo "JOB $ID FAILED"
     else
         echo "JOB $ID succeeded"
