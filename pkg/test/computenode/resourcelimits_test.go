8e36a516c472cb41289457b96b19fa998ddde243
Enrico Rotundo <enrico@winder.ai>
2022-07-22 20:46:53 +0200

    Fix ci fail on linux in resourcelimits test

--- pkg/test/computenode/resourcelimits_test.go
+++ pkg/test/computenode/resourcelimits_test.go
@@ -246,6 +246,7 @@ func (suite *ComputeNodeResourceLimitsSuite) TestTotalResourceLimits() {
 				verifier.VerifierNoop,
 				jobResources.CPU,
 				jobResources.Memory,
+				[]string{},
 				// pass the disk requirement of the job resources into the volume
 				// name so it can be returned from the GetVolumeSize function
 				[]string{
