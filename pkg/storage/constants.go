0fd96b800a2f609f1eb57b2b8a16d6b18e02a5f7
Kai Davenport <kaiyadavenport@gmail.com>
2022-07-26 13:13:39 +0100

    add conversion function for storage types

--- pkg/storage/constants.go
+++ pkg/storage/constants.go
@@ -1,5 +1,10 @@
 package storage
 
+import (
+	"fmt"
+	"strings"
+)
+
 // StorageSourceType is somewhere we can get data from
 // e.g. ipfs / S3 are storage sources
 // there can be multiple drivers for the same source
@@ -13,8 +18,26 @@ const (
 	storageSourceDone // must be last
 )
 
-const IPFSFuseDocker = "ipfs_fuse"
-const IPFSAPICopy = "ipfs_copy"
-const IPFSDefault = "ipfs"
+func ParseStorageSourceType(str string) (StorageSourceType, error) {
+	for typ := storageSourceUnknown + 1; typ < storageSourceDone; typ++ {
+		if equal(typ.String(), str) {
+			return typ, nil
+		}
+	}
+
+	return storageSourceUnknown, fmt.Errorf(
+		"executor: unknown engine type '%s'", str)
+}
+
+func equal(a, b string) bool {
+	a = strings.TrimSpace(a)
+	b = strings.TrimSpace(b)
+	return strings.EqualFold(a, b)
+}
+
+// StorageVolumeConnector is how an upstream storage source will present
+// the volume to a job - examples are "bind" or "library"
+//go:generate stringer -type=StorageVolumeConnectorType --trimprefix=StorageVolumeConnector
+type StorageVolumeConnectorType int
 
 const URLDownload = "url_download"
