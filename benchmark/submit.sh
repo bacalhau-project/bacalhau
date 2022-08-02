8147600a9bee2dcee5b40b6e067fdc6269d2c25e
Kai Davenport <kaiyadavenport@gmail.com>
2022-08-02 21:12:51 +0100

    fix the performance tests now are are reporting state per shard

--- benchmark/submit.sh
+++ benchmark/submit.sh
@@ -9,7 +9,8 @@ COUNTER=1
 while true; do
     sleep 0.1
     # trunk-ignore(shellcheck/SC2312)
-    if [[ $(${BACALHAU_BIN} --api-port="${API_PORT}" --api-host=localhost describe "${ID}" 2>&1|grep "State: Complete"|wc -l) -ne 3 ]]; then
+    # TODO: get the shard state to not be a number (which is brittle to test against)
+    if [[ $(${BACALHAU_BIN} --api-port="${API_PORT}" --api-host=localhost describe "${ID}" 2>&1|grep "state: 6"|wc -l) -ne 3 ]]; then
         echo "JOB ${ID} FAILED"
         (( COUNTER++ ))
         if (( COUNTER > 300 )); then
