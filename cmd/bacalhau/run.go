795907b2b5701fbf5bb06a56623e47f52312bd2d
Phil Winder <phil@WinderResearch.com>
2022-08-09 16:00:41 +0100

    feat(cli): check that client and server versions match

--- cmd/bacalhau/run.go
+++ cmd/bacalhau/run.go
@@ -1,6 +1,10 @@
 package bacalhau
 
 import (
+	"fmt"
+
+	"github.com/filecoin-project/bacalhau/pkg/version"
+	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
 )
 
@@ -12,4 +16,15 @@ func init() {
 var runCmd = &cobra.Command{
 	Use:   "run",
 	Short: "Run a job on the network (see subcommands for supported flavors)",
+	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+		// Check that the server version is compatible with the client version
+		serverVersion, _ := getAPIClient().Version(cmd.Context()) // Ok if this fails, version validation will skip
+		if err := ensureValidVersion(cmd.Context(), version.Get(), serverVersion); err != nil {
+			err = fmt.Errorf("version mismatch, please upgrade your client: %s", err)
+			log.Err(err)
+			return err
+		}
+
+		return nil
+	},
 }
