1c3fc6a5f6ecac0d3ef4b564b93f791314e7ff95
Phil Winder <phil@WinderResearch.com>
2022-08-10 10:20:38 +0100

    fix: restrict run submissions to work with same exact versions only

--- cmd/bacalhau/run.go
+++ cmd/bacalhau/run.go
@@ -1,8 +1,6 @@
 package bacalhau
 
 import (
-	"fmt"
-
 	"github.com/filecoin-project/bacalhau/pkg/version"
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
@@ -20,7 +18,6 @@ var runCmd = &cobra.Command{
 		// Check that the server version is compatible with the client version
 		serverVersion, _ := getAPIClient().Version(cmd.Context()) // Ok if this fails, version validation will skip
 		if err := ensureValidVersion(cmd.Context(), version.Get(), serverVersion); err != nil {
-			err = fmt.Errorf("version mismatch, please upgrade your client: %s", err)
 			log.Err(err)
 			return err
 		}
