09079d0187f5006716e46e0a85be933885669f0f
Walid Baruni <wdbaruni@gmail.com>
2022-08-15 15:23:18 -0700

    Fix starting transport twice

--- pkg/test/computenode/utils.go
+++ pkg/test/computenode/utils.go
@@ -121,8 +121,13 @@ func SetupTestNoop(
 		t.Fatal(err)
 	}
 
-	err = ctrl.Start(context.Background())
+	ctx := context.Background()
+	err = ctrl.Start(ctx)
 	if err != nil {
+		t.Fatal(err)
+	}
+
+	if err = transport.Start(ctx); err != nil {
 		t.Fatal(err)
 	}
 
