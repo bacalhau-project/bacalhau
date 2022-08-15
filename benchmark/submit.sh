f857b590400388a0066b7b64674943c74fe0eca0
Luke Marsden <me@lukemarsden.net>
2022-08-15 11:46:13 +0000

    use --wait

--- benchmark/submit.sh
+++ benchmark/submit.sh
@@ -7,11 +7,7 @@ set -xeuo pipefail
 ID=$(time ${BACALHAU_BIN} --api-port="${API_PORT}" --api-host=localhost docker run --concurrency=3 --wait --wait-timeout-secs 20 busybox -- /bin/true)
 if [[ $(time ${BACALHAU_BIN} --api-port="${API_PORT}" --api-host=localhost describe "${ID}" |grep "State: Complete"|wc -l) -ne 3 ]]; then
         echo "JOB ${ID} FAILED"
-        (( COUNTER++ ))
-        if (( COUNTER > 3000 )); then
-            echo "JOB ${ID} checked ${COUNTER} times. Assuming failure."
-            exit 1
-        fi
+        exit 1
     else
         echo "JOB ${ID} succeeded"
         exit 0
