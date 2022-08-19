6f2bbd74e9279c079716ec1daa06a4aff5af1ba7
Luke Marsden <me@lukemarsden.net>
2022-08-19 10:44:11 +0000

    fix go mod tidy

--- pkg/transport/libp2p/libp2p.go
+++ pkg/transport/libp2p/libp2p.go
@@ -8,7 +8,7 @@ import (
 
 	realsync "sync"
 
-	sync "github.com/RobinUS2/golang-mutex-tracer"
+	sync "github.com/lukemarsden/golang-mutex-tracer"
 
 	"github.com/filecoin-project/bacalhau/pkg/config"
 	"github.com/filecoin-project/bacalhau/pkg/executor"
