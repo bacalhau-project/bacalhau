f0ba7f03db021e66f31ab5fb2dcd4266c89e5583
Kai Davenport <kaiyadavenport@gmail.com>
2022-08-26 18:30:31 +0100

    fix linter

--- cmd/bacalhau/docker_run.go
+++ cmd/bacalhau/docker_run.go
@@ -251,9 +251,7 @@ var dockerRunCmd = &cobra.Command{
 	Long:    dockerRunLong,
 	Example: dockerRunExample,
 	Args:    cobra.MinimumNArgs(1),
-	PostRun: func(cmd *cobra.Command, args []string) {
-		//ODR = NewDockerRunOptions()
-	},
+	PostRun: func(cmd *cobra.Command, args []string) {},
 	RunE: func(cmd *cobra.Command, cmdArgs []string) error { // nolintunparam // incorrect that cmd is unused.
 		cm := system.NewCleanupManager()
 		defer cm.Cleanup()
