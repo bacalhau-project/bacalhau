062d201f5f432c12cec4c53f1e09456f2a8ce12f
Enrico Rotundo <enrico@winder.ai>
2022-08-30 15:46:29 +0200

    Remove sudo commands from storage provider tests [to be checked in CI]

--- pkg/test/ipfs/ipfs_host_storage_test.go
+++ pkg/test/ipfs/ipfs_host_storage_test.go
@@ -59,7 +59,6 @@ func (suite *IPFSHostStorageSuite) TestIpfsApiCopyFile() {
 			return apicopy.NewStorageProvider(cm, api)
 		},
 	)
-
 }
 
 func (suite *IPFSHostStorageSuite) TestIPFSAPICopyFolder() {
@@ -110,8 +109,12 @@ func runFileTest(t *testing.T, engine storage.StorageSourceType, getStorageDrive
 
 	// we should now be able to read our file content
 	// from the file on the host via fuse
-	result, err := system.RunCommandGetResults("sudo", []string{
-		"cat",
+	// TODO @enricorotundo #493: make sure sudo is not needed here
+	// result, err := system.RunCommandGetResults("sudo", []string{
+	// 	"cat",
+	// 	volume.Source,
+	// })
+	result, err := system.RunCommandGetResults("cat", []string{
 		volume.Source,
 	})
 	require.NoError(t, err)
@@ -163,8 +166,13 @@ func runFolderTest(t *testing.T, engine storage.StorageSourceType, getStorageDri
 
 	// we should now be able to read our file content
 	// from the file on the host via fuse
-	result, err := system.RunCommandGetResults("sudo", []string{
-		"cat",
+	
+	// TODO @enricorotundo #493: make sure sudo is not needed here
+	// result, err := system.RunCommandGetResults("sudo", []string{
+	// 	"cat",
+	// 	fmt.Sprintf("%s/file.txt", volume.Source),
+	// })
+	result, err := system.RunCommandGetResults("cat", []string{
 		fmt.Sprintf("%s/file.txt", volume.Source),
 	})
 	require.NoError(t, err)
