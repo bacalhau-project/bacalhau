77611f0919ecbbd4a42059230a553be945eaf773
Walid Baruni <wdbaruni@gmail.com>
2022-09-10 11:59:21 -0700

    GetAPIClient()

--- ops/aws/canary/src/main.go
+++ ops/aws/canary/src/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/filecoin-project/bacalhau/cmd/bacalhau"
 )
 
 func hello() (string, error) {
+	bacalhau.GetAPIClient()
 	return "Done λ!", nil
 }
 
