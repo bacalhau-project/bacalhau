f13f254123e4d876feb5d9808f6e344054a3feed
lukemarsden <me@lukemarsden.net>
2022-10-03 13:50:20 +0100

    mount disk on reboot as well as initial install/upgrade (#772)

--- ops/terraform/remote_files/scripts/start-bacalhau.sh
+++ ops/terraform/remote_files/scripts/start-bacalhau.sh
@@ -6,6 +6,17 @@ IFS=$'\n\t'
 # import the terraform vars
 source /terraform_node/variables
 
+# mount the disk - wait for /dev/sdb to exist
+# NB: do not reformat the disk if we can't mount it, unlike the initial
+# install/upgrade script!
+while [[ ! -e /dev/sdb ]]; do
+  sleep 1
+  echo "waiting for /dev/sdb to exist"
+done
+# mount /dev/sdb at /data
+mkdir -p /data
+mount /dev/sdb /data
+
 # import the secrets
 source /data/secrets.sh
 
