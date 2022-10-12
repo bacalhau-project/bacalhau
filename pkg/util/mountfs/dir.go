45b764888ce91d4a5c936fa5700be4659b6bfb2d
Simon Worthington <simon@register-dynamics.co.uk>
2022-10-12 21:24:18 +0100

    Add Go virtual filesystems for composing and writing
    This commit adds two virtual filesystems (fs.FS objects).
    
    The `mountfs` filesystem allows multiple other fs.FS objects to be
    composed under directory prefixes. This is useful because our WASM
    executor takes only a single fs.FS to represent the whole filesystem
    available to the executing code, so now we can compose our various
    input and output storage specs together into a single hierarchy.
    
    The `touchfs` filesystem allows opening files that do not yet exist.
    The default `os.DirFS` filesystem throws an error on this condition.
    This is necessary because the output files from our WASM programs are
    new, and hence we need some way to open them before creation.

---
+++ pkg/util/mountfs/dir.go
@@ -0,0 +1,95 @@
+package mountfs
+
+import (
+	"fmt"
+	"io/fs"
+	"time"
+)
+
+// MountDir represents the root of a mountfs filesystem. As such, it is always a
+// directory with name ".".
+//
+// It implements fs.FS, fs.File, fs.ReadDirFile and fs.FileInfo.
+type MountDir struct {
+	mounts  map[string]fs.FS
+	modtime time.Time
+}
+
+// type File
+func (m *MountDir) Stat() (fs.FileInfo, error) {
+	return m, nil
+}
+
+func (m *MountDir) Read([]byte) (int, error) {
+	return 0, &fs.PathError{
+		Op:   "read",
+		Path: ".",
+		Err:  fmt.Errorf("is a directory"),
+	}
+}
+
+func (m *MountDir) Close() error {
+	return nil
+}
+
+var _ fs.File = &MountDir{}
+
+// type ReadDirFile
+func (m *MountDir) ReadDir(n int) ([]fs.DirEntry, error) {
+	entries := []fs.DirEntry{}
+	for name, fs := range m.mounts {
+		file, err := fs.Open(".")
+		if err != nil {
+			return nil, err
+		}
+		defer file.Close()
+
+		stat, err := file.Stat()
+		if err != nil {
+			return nil, err
+		}
+
+		entries = append(entries, &mountDirEntry{
+			name: name,
+			mode: stat.Mode(),
+			dir:  m,
+		})
+	}
+
+	return entries, nil
+}
+
+var _ fs.ReadDirFile = &MountDir{}
+
+// type FileInfo
+// base name of the file
+func (*MountDir) Name() string {
+	return "."
+}
+
+// length in bytes for regular files; system-dependent for others
+func (*MountDir) Size() int64 {
+	return 0
+}
+
+// file mode bits
+func (*MountDir) Mode() fs.FileMode {
+	return fs.ModeDir
+}
+
+// modification time
+func (m *MountDir) ModTime() time.Time {
+	return m.modtime
+}
+
+// abbreviation for Mode().IsDir()
+func (m *MountDir) IsDir() bool {
+	return m.Mode().IsDir()
+}
+
+// underlying data source (can return nil)
+func (m *MountDir) Sys() any {
+	return m.mounts
+}
+
+var _ fs.FileInfo = &MountDir{}
---
+++ pkg/util/touchfs/fs.go
@@ -0,0 +1,51 @@
+// touchfs implements an FS.fs that will create any files that don't exist when
+// they are accessed.
+//
+// This is necessary because our WASM implementation takes an fs.FS as an
+// interface to a filesystem but the filesystem-backed FS returned by os.DirFS
+// is read-only by default and will return an os.ErrNotExist if a file is opened
+// for writing for the first time.
+//
+// To do this, touchfs tracks a directory prefix (as a string) which is both
+// where it will serve and create files. Deeply nested directories are not
+// supported, i.e. calling Open('a/b/c') where 'b' does not exist will sitll
+// throw an error.
+//
+// This is really a limitation of the fs.FS interface and wazero shoudn't be
+// using it to provide a writable filesystem.
+
+package touchfs
+
+import (
+	"errors"
+	"io/fs"
+	"os"
+	"path/filepath"
+)
+
+type touchFS string
+
+// New accepts a path to a directory and return a new fs.FS that will create
+// files when they are first accessed.
+func New(path string) fs.FS {
+	return touchFS(path)
+}
+
+// When Open returns an error, it should be of type *PathError
+// with the Op field set to "open", the Path field set to name,
+// and the Err field describing the problem.
+//
+// Open should reject attempts to open names that do not satisfy
+// ValidPath(name), returning a *PathError with Err set to
+// ErrInvalid or ErrNotExist.
+func (t touchFS) Open(name string) (fs.File, error) {
+	dir := os.DirFS(string(t))
+	file, err := dir.Open(name)
+
+	if err != nil && errors.Is(err, os.ErrNotExist) {
+		fullPath := filepath.Join(string(t), name)
+		return os.Create(fullPath)
+	} else {
+		return file, err
+	}
+}
