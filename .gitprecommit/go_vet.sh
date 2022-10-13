d1ba7feb5709d2a271272dd873de8962632667ed
David Aronchick <aronchick@gmail.com>
2022-10-13 14:58:24 -0700

    filtering on .go

--- .gitprecommit/go_vet.sh
+++ .gitprecommit/go_vet.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
-go vet ./cmd/...
-go vet ./pkg/...
+if git rev-parse --verify HEAD >/dev/null 2>&1
+then
+    against=HEAD
+else
+    # Initial commit: diff against an empty tree object
+    EMPTY_TREE=$(git hash-object -t tree /dev/null)
+    against=${EMPTY_TREE}
+fi
+
+FILES=$(git diff --cached --name-only "${against}")
+if [[ -n "${FILES}" ]]; then
+    for f in ${FILES}; do
+        if [[ "${f}" == *.go ]]; then
+            go vet "${f}"
+        fi
+    done
+fi
