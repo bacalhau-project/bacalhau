ce45161bda2d4eab0a10213b2f0b176ab8c23719
Kai Davenport <kaiyadavenport@gmail.com>
2022-10-26 13:10:39 +0100

    remove test in favour of get tests via CLI and IPFS

--- cmd/bacalhau/get_test.go
+++ cmd/bacalhau/get_test.go
@@ -3,8 +3,6 @@ package bacalhau
 import (
 	"context"
 	"fmt"
-	"net"
-	"net/url"
 	"os"
 	"path/filepath"
 	"strings"
@@ -12,11 +10,8 @@ import (
 
 	"github.com/filecoin-project/bacalhau/pkg/computenode"
 	"github.com/filecoin-project/bacalhau/pkg/ipfs"
-	"github.com/filecoin-project/bacalhau/pkg/publicapi"
-	"github.com/filecoin-project/bacalhau/pkg/storage/util"
 	"github.com/filecoin-project/bacalhau/pkg/system"
 	devstack_tests "github.com/filecoin-project/bacalhau/pkg/test/devstack"
-	"github.com/phayes/freeport"
 	"github.com/spf13/cobra"
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
@@ -51,101 +46,6 @@ func (suite *GetSuite) TearDownTest() {
 }
 
 func (suite *GetSuite) TearDownAllSuite() {
-
-}
-
-func (suite *GetSuite) TestGetJob() {
-	const NumberOfNodes = 3
-
-	numOfJobsTests := []struct {
-		numOfJobs int
-	}{
-		{numOfJobs: 1},
-		{numOfJobs: 21}, // one more than the default list length
-	}
-
-	host := "localhost"
-	port, _ := freeport.GetFreePort()
-	submittedJobID := ""
-
-	outputDir, _ := os.MkdirTemp(os.TempDir(), "bacalhau-get-test-*")
-	defer os.RemoveAll(outputDir)
-	for _, n := range numOfJobsTests {
-		func() {
-			c, cm := publicapi.SetupRequesterNodeForTestWithPort(suite.T(), port)
-			defer cm.Cleanup()
-
-			for i := 0; i < NumberOfNodes; i++ {
-				for i := 0; i < n.numOfJobs; i++ {
-					runNoopJob := true // Remove when gets are fixed in DevStack
-					if runNoopJob {
-						for i := 0; i < NumberOfNodes; i++ {
-							for i := 0; i < n.numOfJobs; i++ {
-								ctx := context.Background()
-								j := publicapi.MakeGenericJob()
-								s, err := c.Submit(ctx, j, nil)
-								require.NoError(suite.T(), err)
-								submittedJobID = s.ID // Default to the last job submitted, should be fine?
-							}
-						}
-					} else {
-						// Submit job and wait (so that we can download it later)
-						_, out, err := ExecuteTestCobraCommand(suite.T(), suite.rootCmd, "docker", "run",
-							"--api-host", host,
-							"--api-port", fmt.Sprintf("%d", port),
-							"ubuntu", "echo Random -> $RANDOM",
-							"--wait",
-						)
-
-						require.NoError(suite.T(), err)
-						submittedJobID = strings.TrimSpace(out) // Default to the last job submitted, should be fine?
-					}
-				}
-			}
-
-			parsedBasedURI, _ := url.Parse(c.BaseURI)
-			host, port, _ := net.SplitHostPort(parsedBasedURI.Host)
-
-			// No job id (should error)
-			_, _, err := ExecuteTestCobraCommand(suite.T(), suite.rootCmd, "get",
-				"--api-host", host,
-				"--api-port", port,
-			)
-			require.Error(suite.T(), err, "Submitting a get request with no id should error.")
-
-			outputDirWithID := filepath.Join(outputDir, submittedJobID)
-			os.Mkdir(outputDirWithID, util.OS_ALL_RWX)
-
-			// Job Id at the end
-			_, _, err = ExecuteTestCobraCommand(suite.T(), suite.rootCmd, "get",
-				"--api-host", host,
-				"--api-port", port,
-				"--output-dir", outputDirWithID,
-				submittedJobID,
-			)
-			require.NoError(suite.T(), err, "Error in getting job: %+v", err)
-
-			// Short Job ID
-			_, _, err = ExecuteTestCobraCommand(suite.T(), suite.rootCmd, "get",
-				"--api-host", host,
-				"--api-port", port,
-				"--output-dir", outputDirWithID,
-				submittedJobID[0:8],
-			)
-			require.NoError(suite.T(), err, "Error in getting short job: %+v", err)
-
-			// Get stdout from job
-			// _, out, err = ExecuteTestCobraCommand(suite.T(), suite.rootCmd, "get",
-			// 	"--api-host", host,
-			// 	"--api-port", port,
-			// 	"--output-dir", outputDirWithID,
-			// 	out)
-
-			// require.NoError(suite.T(), err, "Error in getting files from job: %+v", err)
-			// // TODO: #637 Need to do a lot more testing here, we don't do any analysis of output files
-			// fmt.Println(out)
-		}()
-	}
 
 }
 
@@ -254,6 +154,7 @@ func (s *GetSuite) TestGetWriteToJobFolderAutoDownload() {
 	ctx := context.Background()
 	stack, _ := devstack_tests.SetupTest(ctx, s.T(), 1, 0, false, computenode.ComputeNodeConfig{})
 	*ODR = *NewDockerRunOptions()
+	*OG = *NewGetOptions()
 
 	swarmAddresses, err := stack.Nodes[0].IPFSClient.SwarmAddresses(ctx)
 	require.NoError(s.T(), err)
@@ -291,6 +192,7 @@ func (s *GetSuite) TestGetWriteToJobFolderNamedDownload() {
 	ctx := context.Background()
 	stack, _ := devstack_tests.SetupTest(ctx, s.T(), 1, 0, false, computenode.ComputeNodeConfig{})
 	*ODR = *NewDockerRunOptions()
+	*OG = *NewGetOptions()
 
 	swarmAddresses, err := stack.Nodes[0].IPFSClient.SwarmAddresses(ctx)
 	require.NoError(s.T(), err)
