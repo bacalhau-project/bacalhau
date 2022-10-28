19f47c0be1bc77d45ca6c81099a652b94f9691d2
Simon Worthington <simon@register-dynamics.co.uk>
2022-10-28 19:46:59 +0100

    Point canary at version with fixed downloader
    This is only temporary – we can point it back at a real release when
    we do the next one.

--- ops/aws/canary/lambda/pkg/scenarios/submitAndGet.go
+++ ops/aws/canary/lambda/pkg/scenarios/submitAndGet.go
@@ -56,7 +56,7 @@ func SubmitAndGet(ctx context.Context) error {
 	if err != nil {
 		return err
 	}
-	body, err := os.ReadFile(filepath.Join(downloadSettings.OutputDir, "combined_results", "stdout"))
+	body, err := os.ReadFile(filepath.Join(downloadSettings.OutputDir, ipfs.DownloadVolumesFolderName, ipfs.DownloadFilenameStdout))
 	if err != nil {
 		return err
 	}
