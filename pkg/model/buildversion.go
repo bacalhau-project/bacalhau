47aed1fef200692a95be32cd50d8b75ae742650a
Will May <will.j.may@gmail.com>
2022-11-01 09:43:36 +0000

    Fetch build info from debug.BuildInfo struct
    Use the builtin `debug.BuildInfo` struct rather than having to pass
    information in via ldflags. This also aims to make the binary
    reproducible by using the `trimpath` flag and using the Git commit date
    as the `builddate`.
    
    This also changes how the binary is built, from using a file to a
    directory, so that the binary is built as a Go module.
    
    Fixes #998

--- pkg/model/buildversion.go
+++ pkg/model/buildversion.go
@@ -4,9 +4,7 @@ import (
 	"time"
 )
 
-type BuildVersion string
-
-// Version of a bacalhau binary (either client or server)
+// BuildVersionInfo is the version of a Bacalhau binary (either client or server)
 type BuildVersionInfo struct {
 	// Client Version: version.Info{Major:"1", Minor:"24", GitVersion:"v1.24.0",
 	// GitCommit:"4ce5a8954017644c5420bae81d72b09b735c21f0", GitTreeState:"clean",
