d45c84653d155ce6fd6158ae2e2faa6b15f62bc9
Will May <will.may@opencredo.com>
2022-11-23 11:23:35 +0000

    Make sure HTTP client responses are drained (#1308)
    The documentation for `response.Body` states:
    
    > It is the caller's responsibility to close Body. The default HTTP
    > client's Transport may not reuse HTTP/1.x "keep-alive" TCP connections
    > if the Body is not read to completion and closed.
    
    This change adds a helper function to make it easy to drain and close
    the HTTP connections so that they can be reused by the OS if possible.

--- pkg/util/closer/closer.go
+++ pkg/util/closer/closer.go
@@ -1,6 +1,7 @@
 package closer
 
 import (
+	"context"
 	"errors"
 	"io"
 	"net"
@@ -11,11 +12,29 @@ import (
 
 // CloseWithLogOnError will close the given resource and log any relevant failure
 func CloseWithLogOnError(name string, c io.Closer) {
+	closeCloser(name, c)
+}
+
+// DrainAndCloseWithLogOnError will first ensure the contents of the reader has been read before being closed. This is
+// useful when dealing with HTTP response bodies which need to be drained and closed so that the connection may be
+// re-used by the OS.
+func DrainAndCloseWithLogOnError(ctx context.Context, name string, c io.ReadCloser) {
+	if _, err := io.Copy(io.Discard, c); err != nil {
+		l := log.Ctx(ctx).With().CallerWithSkipFrameCount(3).Logger()
+		l.Err(err).Msgf("Failed to drain %s", name)
+	}
+
+	closeCloser(name, c)
+}
+
+func closeCloser(name string, c io.Closer) {
 	err := c.Close()
 	if err == nil || errors.Is(err, os.ErrClosed) || errors.Is(err, net.ErrClosed) {
 		return
 	}
 
-	l := log.With().CallerWithSkipFrameCount(3).Logger()
+	l := log.With().CallerWithSkipFrameCount(framesFromCaller).Logger()
 	l.Err(err).Msgf("Failed to close %s", name)
 }
+
+const framesFromCaller = 4
