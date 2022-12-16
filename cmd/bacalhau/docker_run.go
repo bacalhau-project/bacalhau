4cac737d758d1b8b019c08c03443243149c643f4
Simon Worthington <simon@register-dynamics.co.uk>
2022-12-16 15:39:29 +0000

    Add CLI options to enable networking.
    This commit adds the `—job-selection-accept-networked` flag to the
    `bacalhau serve` and `bacalhau devstack` commands and the `—network`
    flag to the `bacalhau docker run` command.
    
    `—job-selection-accept-networked` causes the compute node(s) to
    accept jobs that ask for networking. Naturally, this flag should be
    used with care and it’s not currently recommended to enable this for
    nodes accepting jobs in a public environment.
    
    `—network` allows a job to declare that it requires networking. The
    default value is `none` which is the current behaviour. The new value
    is `full` which will hook the container up to the host’s network.
    
    It’s not currently possible to enable networking for WebAssembly jobs
    because our implementation of WebAssembly doesn’t support sockets yet.

--- cmd/bacalhau/docker_run.go
+++ cmd/bacalhau/docker_run.go
@@ -58,6 +58,7 @@ type DockerRunOptions struct {
 	CPU              string
 	Memory           string
 	GPU              string
+	Networking       model.Network
 	WorkingDirectory string   // Working directory for docker
 	Labels           []string // Labels for the job on the Bacalhau network (for searching)
 
@@ -96,6 +97,7 @@ func NewDockerRunOptions() *DockerRunOptions {
 		CPU:                "",
 		Memory:             "",
 		GPU:                "",
+		Networking:         model.NetworkNone,
 		SkipSyntaxChecking: false,
 		WorkingDirectory:   "",
 		Labels:             []string{},
@@ -209,6 +211,10 @@ func newDockerRunCmd() *cobra.Command { //nolint:funlen
 	dockerRunCmd.PersistentFlags().StringVar(
 		&ODR.GPU, "gpu", ODR.GPU,
 		`Job GPU requirement (e.g. 1, 2, 8).`,
+	)
+	dockerRunCmd.PersistentFlags().Var(
+		NetworkFlag(&ODR.Networking), "network",
+		`Networking capability required by the job`,
 	)
 	dockerRunCmd.PersistentFlags().BoolVar(
 		&ODR.SkipSyntaxChecking, "skip-syntax-checking", ODR.SkipSyntaxChecking,
@@ -365,6 +371,7 @@ func CreateJob(ctx context.Context,
 		odr.CPU,
 		odr.Memory,
 		odr.GPU,
+		odr.Networking,
 		odr.InputUrls,
 		odr.InputVolumes,
 		odr.OutputVolumes,
