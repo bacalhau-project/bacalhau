ae4048d9eff9fdf06ac9edacea3090294b52e8e5
Luke Marsden <me@lukemarsden.net>
2023-01-23 15:34:21 +0000

    Crappy hacks and workarounds to get it working

--- dashboard/api/pkg/server/client.go
+++ dashboard/api/pkg/server/client.go
@@ -3,18 +3,47 @@ package server
 // client code for accessing bacalhau, literally ripped straight from the CLI
 
 import (
-	"github.com/filecoin-project/bacalhau/cmd/bacalhau"
+	"log"
+
+	"github.com/filecoin-project/bacalhau/pkg/system"
+	"github.com/filecoin-project/bacalhau/pkg/util/rundocker"
+	"github.com/spf13/cobra"
 )
 
+var configInitRan bool
+
 func runStableDiffusion(prompt string) (string, error) {
-	runOptions := bacalhau.NewDockerRunOptions()
+	if !configInitRan {
+		system.InitConfig()
+		configInitRan = true
+	}
+	runOptions := rundocker.NewDockerRunOptions()
 	runOptions.RunTimeSettings.WaitForJobToFinish = true
 	// need to set this to get the cid out
 	runOptions.RunTimeSettings.PrintNodeDetails = true
-	runOptions.GPU = "1"
-	return bacalhau.DockerRun(nil, []string{
-		"ghcr.io/bacalhau-project/examples/stable-diffusion-gpu:0.0.1",
-		"--", "python", "main.py", "--o", "./outputs", "--p",
-		prompt,
+
+	log.Printf("HELLO FISHES")
+	// because the rundocker machinery likes to run cmd.Print{,f}
+	nullCommand := &cobra.Command{
+		Use:   "null",
+		Short: "null",
+		Run: func(cmd *cobra.Command, args []string) {
+		},
+	}
+	// just to fill in contexts, etc... hacks hacks hacks
+	nullCommand.Execute()
+
+	// gpus:
+	// runOptions.GPU = "1"
+	// return rundocker.DockerRun(nullCommand, []string{
+	// 	"ghcr.io/bacalhau-project/examples/stable-diffusion-gpu:0.0.1",
+	// 	"python", "main.py", "--o", "./outputs", "--p",
+	// 	prompt,
+	// }, runOptions)
+
+	// testing only:
+	return rundocker.DockerRun(nullCommand, []string{
+		"ubuntu",
+		"echo", prompt,
 	}, runOptions)
 }
