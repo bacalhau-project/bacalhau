242d909b3a5fb8b04ba1ca111dff19fdffa9294b
Kai Davenport <kaiyadavenport@gmail.com>
2023-01-26 18:02:44 +0000

    local directory storage driver (#1782)
    This PR adds a local directory storage driver which means that you can
    configure local directories as storage sources for jobs which is useful
    for on-prem scenarios where you want to use node selectors to target
    specific nodes who have data locally in known folders.

--- pkg/model/storagesourcetype_string.go
+++ pkg/model/storagesourcetype_string.go
@@ -15,12 +15,13 @@ func _() {
 	_ = x[StorageSourceFilecoin-4]
 	_ = x[StorageSourceEstuary-5]
 	_ = x[StorageSourceInline-6]
-	_ = x[storageSourceDone-7]
+	_ = x[StorageSourceLocalDirectory-7]
+	_ = x[storageSourceDone-8]
 }
 
-const _StorageSourceType_name = "storageSourceUnknownIPFSURLDownloadFilecoinUnsealedFilecoinEstuaryInlinestorageSourceDone"
+const _StorageSourceType_name = "storageSourceUnknownIPFSURLDownloadFilecoinUnsealedFilecoinEstuaryInlineLocalDirectorystorageSourceDone"
 
-var _StorageSourceType_index = [...]uint8{0, 20, 24, 35, 51, 59, 66, 72, 89}
+var _StorageSourceType_index = [...]uint8{0, 20, 24, 35, 51, 59, 66, 72, 86, 103}
 
 func (i StorageSourceType) String() string {
 	if i < 0 || i >= StorageSourceType(len(_StorageSourceType_index)-1) {
