a298f9365f39453927705d638ffb50c5d9c6c4fd
Simon Worthington <simon@register-dynamics.co.uk>
2023-01-27 21:02:25 +1100

    Apply memory limits to WASM jobs
    Previously any memory limit applied to a WASM job was silently ignored
    and memory usage was unbounded.
    
    Now, the memory limit is respected and configured to be within 64kb of
    the requested size.

--- pkg/executor/wasm/loader.go
+++ pkg/executor/wasm/loader.go
@@ -2,9 +2,15 @@ package wasm
 
 import (
 	"context"
+	"fmt"
 	"os"
+	"path/filepath"
 
+	"github.com/filecoin-project/bacalhau/pkg/model"
+	"github.com/filecoin-project/bacalhau/pkg/storage"
+	"github.com/rs/zerolog/log"
 	"github.com/tetratelabs/wazero"
+	"golang.org/x/exp/maps"
 )
 
 func LoadModule(ctx context.Context, runtime wazero.Runtime, path string) (wazero.CompiledModule, error) {
@@ -19,4 +25,42 @@ func LoadModule(ctx context.Context, runtime wazero.Runtime, path string) (wazer
 	}
 
 	return module, nil
+}
+
+func LoadRemoteModule(
+	ctx context.Context,
+	runtime wazero.Runtime,
+	provider storage.StorageProvider,
+	spec model.StorageSpec,
+) (wazero.CompiledModule, error) {
+	volumes, err := storage.ParallelPrepareStorage(ctx, provider, []model.StorageSpec{spec})
+	if err != nil {
+		return nil, err
+	}
+	volume := maps.Values(volumes)[0]
+
+	programPath := volume.Source
+	info, err := os.Stat(programPath)
+	if err != nil {
+		return nil, err
+	}
+
+	// We expect the input to be a single WASM file. It is common however for
+	// IPFS implementations to wrap files into a directory. So we make a special
+	// case here – if the input is a single file in a directory, we will assume
+	// this is the program file and load it.
+	if info.IsDir() {
+		files, err := os.ReadDir(programPath)
+		if err != nil {
+			return nil, err
+		}
+
+		if len(files) != 1 {
+			return nil, fmt.Errorf("should be %d file in %s but there are %d", 1, programPath, len(files))
+		}
+		programPath = filepath.Join(programPath, files[0].Name())
+	}
+
+	log.Ctx(ctx).Debug().Msgf("Loading WASM module from %q", programPath)
+	return LoadModule(ctx, runtime, programPath)
 }
