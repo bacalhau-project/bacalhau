05a4445a69628f80a579739b2a55eb92dcfd2dfb
Walid Baruni <wdbaruni@gmail.com>
2023-01-29 13:03:09 -0800

    publish libp2p2 resource manager metrics

--- pkg/libp2p/rcmgr/defaults.go
+++ pkg/libp2p/rcmgr/defaults.go
@@ -1,10 +1,14 @@
 package rcmgr
 
 import (
+	ocprom "contrib.go.opencensus.io/exporter/prometheus"
 	"github.com/filecoin-project/bacalhau/pkg/transport/bprotocol"
 	"github.com/libp2p/go-libp2p"
 	libp2p_rcmgr "github.com/libp2p/go-libp2p/p2p/host/resource-manager"
 	"github.com/libp2p/go-libp2p/p2p/host/resource-manager/obs"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/rs/zerolog/log"
+	"go.opencensus.io/stats/view"
 )
 
 func SetDefaultServiceLimits(config *libp2p_rcmgr.ScalingLimitConfig) {
@@ -46,6 +50,19 @@ var DefaultResourceManager = func(cfg *libp2p.Config) error {
 	limits := libp2p_rcmgr.DefaultLimits
 	libp2p.SetDefaultServiceLimits(&limits)
 	SetDefaultServiceLimits(&limits)
+
+	// Hook up the trace reporter metrics. This will expose all opencensus
+	// stats via the default prometheus registry. See https://opencensus.io/exporters/supported-exporters/go/prometheus/ for other options.
+	err := view.Register(obs.DefaultViews...)
+	if err != nil {
+		log.Warn().Err(err).Msg("failed to register resource manager metrics")
+	}
+	_, err = ocprom.NewExporter(ocprom.Options{
+		Registry: prometheus.DefaultRegisterer.(*prometheus.Registry),
+	})
+	if err != nil {
+		log.Warn().Err(err).Msg("failed to register resource manager metric exporter")
+	}
 
 	str, err := obs.NewStatsTraceReporter()
 	if err != nil {
