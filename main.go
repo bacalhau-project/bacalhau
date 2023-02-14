664de4afbe7800cb51eb835cbf33b44d11c4da3f
Ross Jones <ross@register-dynamics.co.uk>
2023-02-14 15:59:45 +0000

    Shares devstack environment vars via a dotenv file
    Each time a developer runs the bacalhau devstack, they are presented
    with a few environment variables that they need to set in another window
    to configure the bacalhau command to connect to the local devstack.  It
    is possible to keep a persistent port, but the IPFS Swarm addresses do
    change.  If work there is a need to restart the devstack, the details
    have to be set again.
    
    Bacalhau already has support for loading `.env` files, and this PR
    extends the functionality to allow for it to load a .env file created by
    the devstack.
    
    When the devstack is launched, if it is able to find a value for the
    environment variable `DEVSTACK_ENV_FILE` then it will write the required
    environment files into the file described within.  This file will be
    cleaned up when the devstack is shut down.
    
    If `DEVSTACK_ENV_FILE` is present when running a normal bacalhau
    command, it will use the details found within.  This means that if the
    developer were to defined something like `DEVSTACK_ENV_FILE=.devstack.env`
    then there is no further need to keep copying and pasting environment
    variables between terminals.
    
    The filename `.devstack.env` is recommended in the running_locally
    documentation as this is now ignored by git to avoid accidentally
    committing it.

--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"os"
 
+	"github.com/filecoin-project/bacalhau/pkg/config"
 	_ "github.com/filecoin-project/bacalhau/pkg/version"
 
 	"github.com/filecoin-project/bacalhau/cmd/bacalhau"
+
 	"github.com/filecoin-project/bacalhau/pkg/system"
 	"github.com/joho/godotenv"
 	"github.com/rs/zerolog/log"
@@ -13,6 +15,13 @@ import (
 
 func main() {
 	_ = godotenv.Load()
+
+	devstackEnvFile := config.DevstackEnvFile()
+	if _, err := os.Stat(devstackEnvFile); err == nil {
+		log.Debug().Msgf("Loading environment from %s", devstackEnvFile)
+		_ = godotenv.Overload(devstackEnvFile)
+	}
+
 	if err := system.InitConfig(); err != nil {
 		log.Error().Msgf("Failed to initialize config: %s", err)
 		os.Exit(1)
