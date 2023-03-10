e8dbb1e92aa0be4f49c32e706a02a4fa3a72fc8e
enricorotundo <enrico.rotundo@gmail.com>
2023-03-10 10:11:08 +0000

    formatted py code

--- integration/airflow/bacalhau_airflow/__init__.py
+++ integration/airflow/bacalhau_airflow/__init__.py
@@ -1,5 +1,5 @@
 """Top-level package for bacalhau-airflow."""
 
 __author__ = """Audrey Roy Greenfeld"""
-__email__ = 'audreyr@example.com'
-__version__ = '0.0.3'
+__email__ = "audreyr@example.com"
+__version__ = "0.0.3"
