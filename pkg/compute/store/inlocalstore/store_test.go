09987495e81bd4537c937aca3b3c1c110015e3ee
Al Norman <gibbonsdatascience@gmail.com>
2023-03-16 14:27:44 +0000

    Add persistent local store for job count
    Station requires some form of persistence for the job count metric.
    A jobStats.JSON will be created in the default station config directory,
    which will store the job count.
    
    We might want to add some form of database in the future for persisting
    these metrics.

--- pkg/compute/store/inlocalstore/store_test.go
+++ pkg/compute/store/inlocalstore/store_test.go
@@ -4,7 +4,9 @@ import (
 	"context"
 	"testing"
 
+	"github.com/bacalhau-project/bacalhau/pkg/compute/store"
 	"github.com/bacalhau-project/bacalhau/pkg/compute/store/inmemory"
+	"github.com/bacalhau-project/bacalhau/pkg/model"
 	"github.com/bacalhau-project/bacalhau/pkg/system"
 	"github.com/stretchr/testify/require"
 )
@@ -20,19 +22,119 @@ func TestZeroExecutionsReturnsZeroCount(t *testing.T) {
 
 }
 
-//func TestOneExecutionReturnsOneCount(t *testing.T) {
-//	ctx := context.Background()
-//	system.InitConfigForTesting(t)
-//	proxy := NewStoreProxy(inmemory.NewStore())
-//	err := proxy.CreateExecution(ctx, )
-//	//create execution
-//	//proxy.GetExecutionCount returns 1?
+func TestOneExecutionReturnsOneCount(t *testing.T) {
+	ctx := context.Background()
+	id := "fakeid1234"
+	system.InitConfigForTesting(t)
+	proxy := NewStoreProxy(inmemory.NewStore())
+	//check jobStore is initialised to zero
+	count, err := proxy.GetExecutionCount(ctx)
+	require.NoError(t, err)
+	require.Equal(t, uint(0), count)
+	//create execution
+	defaultJob, err := model.NewJobWithSaneProductionDefaults()
+	require.NoError(t, err)
+	execution := store.NewExecution(
+		id,
+		*defaultJob,
+		"defaultRequestorNodeID",
+		model.ResourceUsageData{},
+	)
+	err = proxy.CreateExecution(ctx, *execution)
+	require.NoError(t, err)
+	err = proxy.UpdateExecutionState(ctx, store.UpdateExecutionStateRequest{
+		ExecutionID: execution.ID,
+		NewState:    store.ExecutionStateCompleted})
+	require.NoError(t, err)
+	count, err = proxy.GetExecutionCount(ctx)
+	require.NoError(t, err)
+	require.Equal(t, uint(1), count)
+}
 
-//Check that it reads the counter from the file without changing the file
-//Check it gives the right value ALWAYS
-//case : no executions - 0
-// case : run execution and it's completed - 1
-// case : run more than 1
-// case: Check that ONLY completed states are counted
-//create a proxy, give it nonzero count (i.e. a completed execution),
-//create a NEW proxy and check that the value is the same.
+func TestConsecutiveExecutionsReturnCorrectJobCount(t *testing.T) {
+	ctx := context.Background()
+	idList := []string{"id1", "id2", "id3"}
+	system.InitConfigForTesting(t)
+	proxy := NewStoreProxy(inmemory.NewStore())
+	defaultJob, err := model.NewJobWithSaneProductionDefaults()
+	require.NoError(t, err)
+	// 3 executions
+	for index, id := range idList {
+		count, err := proxy.GetExecutionCount(ctx)
+		require.NoError(t, err)
+		require.Equal(t, uint(index), count)
+
+		execution := store.NewExecution(
+			id,
+			*defaultJob,
+			"defaultRequestorNodeID",
+			model.ResourceUsageData{},
+		)
+		err = proxy.CreateExecution(ctx, *execution)
+		require.NoError(t, err)
+		err = proxy.UpdateExecutionState(ctx, store.UpdateExecutionStateRequest{
+			ExecutionID: execution.ID,
+			NewState:    store.ExecutionStateCompleted})
+		require.NoError(t, err)
+	}
+
+}
+
+func TestOnlyCompletedJobsIncreaseCounter(t *testing.T) {
+	ctx := context.Background()
+	system.InitConfigForTesting(t)
+	proxy := NewStoreProxy(inmemory.NewStore())
+	defaultJob, err := model.NewJobWithSaneProductionDefaults()
+	require.NoError(t, err)
+	execution := store.NewExecution(
+		"defaultID",
+		*defaultJob,
+		"defaultRequestorNodeID",
+		model.ResourceUsageData{},
+	)
+	err = proxy.CreateExecution(ctx, *execution)
+	require.NoError(t, err)
+	for executionState := 0; executionState < 7; executionState++ {
+		err = proxy.UpdateExecutionState(ctx, store.UpdateExecutionStateRequest{
+			ExecutionID: execution.ID,
+			NewState:    store.ExecutionState(executionState)})
+		require.NoError(t, err)
+		count, err := proxy.GetExecutionCount(ctx)
+		require.NoError(t, err)
+		require.Equal(t, uint(0), count)
+	}
+}
+
+func TestSecondProxy(t *testing.T) {
+	ctx := context.Background()
+	idList := []string{"id1", "id2", "id3"}
+	system.InitConfigForTesting(t)
+	proxy := NewStoreProxy(inmemory.NewStore())
+	defaultJob, err := model.NewJobWithSaneProductionDefaults()
+	require.NoError(t, err)
+	// 3 executions
+	for index, id := range idList {
+		count, err := proxy.GetExecutionCount(ctx)
+		require.NoError(t, err)
+		require.Equal(t, uint(index), count)
+
+		execution := store.NewExecution(
+			id,
+			*defaultJob,
+			"defaultRequestorNodeID",
+			model.ResourceUsageData{},
+		)
+		err = proxy.CreateExecution(ctx, *execution)
+		require.NoError(t, err)
+		err = proxy.UpdateExecutionState(ctx, store.UpdateExecutionStateRequest{
+			ExecutionID: execution.ID,
+			NewState:    store.ExecutionStateCompleted})
+		require.NoError(t, err)
+	}
+	count, err := proxy.GetExecutionCount(ctx)
+	require.NoError(t, err)
+	secondProxy := NewStoreProxy(inmemory.NewStore())
+	secondCount, err := secondProxy.GetExecutionCount(ctx)
+	require.NoError(t, err)
+	require.Equal(t, count, secondCount)
+}
