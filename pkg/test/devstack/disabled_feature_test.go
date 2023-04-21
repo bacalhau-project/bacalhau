a0b380a553d9469f038e6536d09b2eeb71c5ba03
Simon Worthington <simon@register-dynamics.co.uk>
2023-04-21 19:06:40 +0100

    Ensure feature tests pass when features not disabled.

--- pkg/test/devstack/disabled_feature_test.go
+++ pkg/test/devstack/disabled_feature_test.go
@@ -20,37 +20,47 @@ func TestDisabledFeatureSuite(t *testing.T) {
 	suite.Run(t, new(DisabledFeatureTestSuite))
 }
 
-var disabledTestSpec = scenario.Scenario{
-	Stack: &scenario.StackConfig{
-		DevStackOptions: &devstack.DevStackOptions{
-			NumberOfRequesterOnlyNodes: 1,
-			NumberOfComputeOnlyNodes:   1,
+var waitForError job.CheckStatesFunction = func(js model.JobState) (bool, error) {
+	return js.State == model.JobStateError, nil
+}
+
+func disabledTestSpec() scenario.Scenario {
+	return scenario.Scenario{
+		Stack: &scenario.StackConfig{
+			DevStackOptions: &devstack.DevStackOptions{
+				NumberOfRequesterOnlyNodes: 1,
+				NumberOfComputeOnlyNodes:   1,
+			},
 		},
-	},
-	Spec: scenario.WasmHelloWorld.Spec,
-	JobCheckers: []job.CheckStatesFunction{
-		func(js model.JobState) (bool, error) {
-			return js.State == model.JobStateError, nil
-		},
-	},
+		Spec:        scenario.WasmHelloWorld.Spec,
+		JobCheckers: []job.CheckStatesFunction{waitForError},
+	}
+}
+
+func (s *DisabledFeatureTestSuite) TestNothingDisabled() {
+	testCase := disabledTestSpec()
+	testCase.JobCheckers = []job.CheckStatesFunction{job.WaitForSuccessfulCompletion()}
+	testCase.Spec.Verifier = model.VerifierNoop
+	testCase.Spec.Publisher = model.PublisherIpfs
+	s.RunScenario(testCase)
 }
 
 func (s *DisabledFeatureTestSuite) TestDisabledEngine() {
-	testCase := disabledTestSpec
+	testCase := disabledTestSpec()
 	testCase.Stack.DevStackOptions.DisabledFeatures.Engines = []model.Engine{model.EngineWasm}
 
 	s.RunScenario(testCase)
 }
 
 func (s *DisabledFeatureTestSuite) TestDisabledStorage() {
-	testCase := disabledTestSpec
+	testCase := disabledTestSpec()
 	testCase.Stack.DevStackOptions.DisabledFeatures.Storages = []model.StorageSourceType{model.StorageSourceInline}
 
 	s.RunScenario(testCase)
 }
 
 func (s *DisabledFeatureTestSuite) TestDisabledVerifier() {
-	testCase := disabledTestSpec
+	testCase := disabledTestSpec()
 	testCase.Spec.Verifier = model.VerifierNoop
 	testCase.Stack.DevStackOptions.DisabledFeatures.Verifiers = []model.Verifier{model.VerifierNoop}
 
@@ -58,9 +68,9 @@ func (s *DisabledFeatureTestSuite) TestDisabledVerifier() {
 }
 
 func (s *DisabledFeatureTestSuite) TestDisabledPublisher() {
-	testCase := disabledTestSpec
-	testCase.Spec.Publisher = model.PublisherNoop
-	testCase.Stack.DevStackOptions.DisabledFeatures.Publishers = []model.Publisher{model.PublisherNoop}
+	testCase := disabledTestSpec()
+	testCase.Spec.Publisher = model.PublisherIpfs
+	testCase.Stack.DevStackOptions.DisabledFeatures.Publishers = []model.Publisher{model.PublisherIpfs}
 
 	s.RunScenario(testCase)
 }
