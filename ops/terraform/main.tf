6cadd0becaa1494298071104eb3b68e1bf973637
Ross Jones <ross@register-dynamics.co.uk>
2023-05-02 19:06:23 +0100

    Reset the disk size EU only allows 1TB (#2435)
    It appears that EU instances are only allowed 1TB of disk, and so this
    PR increases the disk size for US instances only.

--- ops/terraform/main.tf
+++ ops/terraform/main.tf
@@ -220,7 +220,7 @@ resource "google_compute_disk" "bacalhau_disk" {
   count    = var.protect_resources ? var.instance_count : 0
   type     = "pd-ssd"
   zone     = var.num_gpu_machines > 1 && count.index == (var.instance_count - 1) ? "europe-west4-a" : var.zone
-  size     = var.volume_size_gb
+  size     = count.index == 4  ? 1000 : var.volume_size_gb
   snapshot = count.index == 4 ? "bacalhau-disk-4-migration" : var.restore_from_backup
   lifecycle {
     prevent_destroy = true
@@ -233,7 +233,7 @@ resource "google_compute_disk" "bacalhau_disk_unprotected" {
   count    = var.protect_resources ? 0 : var.instance_count
   type     = "pd-ssd"
   zone     = var.num_gpu_machines > 1 && count.index == (var.instance_count - 1) ? "europe-west4-a" : var.zone
-  size     = var.volume_size_gb
+  size     = count.index == 4 ? 1000 : var.volume_size_gb 
   snapshot = var.restore_from_backup
 }
 
