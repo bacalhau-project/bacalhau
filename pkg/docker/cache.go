c22c2476e2e9d213f67f5d19917fbc370565d787
Ross Jones <ross@register-dynamics.co.uk>
2023-05-05 11:24:02 +0100

    Configure cache through environment

--- pkg/docker/cache.go
+++ pkg/docker/cache.go
@@ -1,18 +1,34 @@
 package docker
 
 import (
+	"strconv"
 	"time"
 
 	"github.com/bacalhau-project/bacalhau/pkg/cache"
 	"github.com/bacalhau-project/bacalhau/pkg/cache/basic"
+	"github.com/bacalhau-project/bacalhau/pkg/util"
 )
 
 //nolint:unused
 var DockerTagCache cache.Cache[string]
 
+const DefaultCacheSize = uint64(1000)
+const DefaultCacheDuration = time.Hour
+
+const TagCacheSizeEnvVar = "DOCKER_TAG_CACHE_SIZE"
+const TagCacheDurationEnvVar = "DOCKER_TAG_CACHE_DURATION"
+
 func init() { //nolint:gochecknoinits
+	duration := util.GetEnvAs[time.Duration](
+		TagCacheDurationEnvVar, DefaultCacheDuration, time.ParseDuration,
+	)
+	size := util.GetEnvAs[uint64](
+		TagCacheSizeEnvVar, DefaultCacheSize, func(k string) (uint64, error) {
+			return strconv.ParseUint(k, 10, 64)
+		})
+
 	DockerTagCache, _ = basic.NewCache[string](
-		basic.WithCleanupFrequency(time.Duration(1)*time.Hour),
-		basic.WithMaxCost(1000), //nolint:gomnd
+		basic.WithCleanupFrequency(duration),
+		basic.WithMaxCost(size),
 	)
 }
