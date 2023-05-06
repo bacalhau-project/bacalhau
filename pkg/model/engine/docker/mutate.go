4eb45681e80a7160fb742c55c7790cd8b0d3f551
frrist <forrest@protocol.ai>
2023-05-05 18:56:01 -0700

    feat(engine): implement docker engine IPLD spec
    - tests for round trip
    - tests for mutations

---
+++ pkg/model/engine/docker/mutate.go
@@ -0,0 +1,53 @@
+package docker
+
+import "github.com/bacalhau-project/bacalhau/pkg/model/engine"
+
+type MutateOpt func(spec *EngineSpec)
+
+func WithImage(image string) MutateOpt {
+	return func(spec *EngineSpec) {
+		spec.Image = image
+	}
+}
+
+func WithEntrypoint(entrypoint ...string) MutateOpt {
+	return func(spec *EngineSpec) {
+		spec.Entrypoint = entrypoint
+	}
+}
+
+func WithWorkingDirectory(dir string) MutateOpt {
+	return func(spec *EngineSpec) {
+		spec.WorkingDirectory = dir
+	}
+}
+
+func WithEnvironmentVariables(vars ...string) MutateOpt {
+	return func(spec *EngineSpec) {
+		spec.EnvironmentVariables = vars
+	}
+}
+
+func AppendEntrypoint(entrypoint ...string) MutateOpt {
+	return func(spec *EngineSpec) {
+		spec.Entrypoint = append(spec.Entrypoint, entrypoint...)
+	}
+}
+
+func AppendEnvironmentVariables(envvar ...string) MutateOpt {
+	return func(spec *EngineSpec) {
+		spec.EnvironmentVariables = append(spec.EnvironmentVariables, envvar...)
+	}
+}
+
+func Mutate(e *engine.Spec, mutations ...MutateOpt) (*engine.Spec, error) {
+	dockerSpec, err := Decode(e)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, mutate := range mutations {
+		mutate(dockerSpec)
+	}
+	return dockerSpec.AsSpec()
+}
---
+++ pkg/model/engine/docker/spec.go
@@ -0,0 +1,46 @@
+package docker
+
+import (
+	_ "embed"
+
+	dslschema "github.com/ipld/go-ipld-prime/schema/dsl"
+
+	"github.com/bacalhau-project/bacalhau/pkg/model/engine"
+)
+
+//go:embed spec.ipldsch
+var schema []byte
+
+func load() *engine.Schema {
+	dsl, err := dslschema.ParseBytes(schema)
+	if err != nil {
+		panic(err)
+	}
+	return (*engine.Schema)(dsl)
+}
+
+var (
+	EngineSchema *engine.Schema = load()
+)
+
+type EngineSpec struct {
+	// Image is the docker image to run. This must be pull-able by docker.
+	Image string `json:"Image,omitempty"`
+
+	// Entrypoint is an optional override for the default container entrypoint.
+	Entrypoint []string `json:"Entrypoint,omitempty"`
+
+	// EnvironmentVariables is a map of env to run the container with.
+	EnvironmentVariables []string `json:"EnvironmentVariables,omitempty"`
+
+	// WorkingDirectory is the working directory inside the container.
+	WorkingDirectory string `json:"WorkingDirectory,omitempty"`
+}
+
+func (e *EngineSpec) AsSpec() (*engine.Spec, error) {
+	return engine.Encode(e, EngineSchema)
+}
+
+func Decode(spec *engine.Spec) (*EngineSpec, error) {
+	return engine.Decode[EngineSpec](spec)
+}
