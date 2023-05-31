6cec1a0e3254ca4603eedf42c278bca2f1afa3e4
Ross Jones <ross@register-dynamics.co.uk>
2023-05-31 18:51:33 +0100

    Re-enable compute node cache

--- pkg/executor/docker/bidstrategy/semantic/image_platform_test.go
+++ pkg/executor/docker/bidstrategy/semantic/image_platform_test.go
@@ -9,6 +9,8 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"github.com/bacalhau-project/bacalhau/pkg/bidstrategy"
+	"github.com/bacalhau-project/bacalhau/pkg/cache"
+	"github.com/bacalhau-project/bacalhau/pkg/cache/fake"
 	"github.com/bacalhau-project/bacalhau/pkg/docker"
 	"github.com/bacalhau-project/bacalhau/pkg/executor/docker/bidstrategy/semantic"
 	"github.com/bacalhau-project/bacalhau/pkg/model"
@@ -51,39 +53,38 @@ func TestBidsBasedOnImagePlatform(t *testing.T) {
 		require.Equal(t, false, response.ShouldBid)
 	})
 
-	// TODO: Re-enable
-	// t.Run("cached manifest response for duplicate call", func(t *testing.T) {
+	t.Run("cached manifest response for duplicate call", func(t *testing.T) {
 
-	// 	previousCache := semantic.ManifestCache
+		previousCache := semantic.ManifestCache
 
-	// 	var fc *fake.FakeCache[docker.ImageManifest] = fake.NewFakeCache[docker.ImageManifest]()
-	// 	var cc cache.Cache[docker.ImageManifest] = fc
-	// 	semantic.ManifestCache = &cc
+		var fc *fake.FakeCache[docker.ImageManifest] = fake.NewFakeCache[docker.ImageManifest]()
+		var cc cache.Cache[docker.ImageManifest] = fc
+		semantic.ManifestCache = &cc
 
-	// 	response, err := strategy.ShouldBid(context.Background(), bidstrategy.BidStrategyRequest{
-	// 		Job: jobForDockerImage("ubuntu:latest"),
-	// 	})
+		response, err := strategy.ShouldBid(context.Background(), bidstrategy.BidStrategyRequest{
+			Job: jobForDockerImage("ubuntu:latest"),
+		})
 
-	// 	require.NoError(t, err)
-	// 	require.Equal(t, true, response.ShouldBid)
+		require.NoError(t, err)
+		require.Equal(t, true, response.ShouldBid)
 
-	// 	// Second time we expect should be cached
-	// 	response, err = strategy.ShouldBid(context.Background(), bidstrategy.BidStrategyRequest{
-	// 		Job: jobForDockerImage("ubuntu:latest"),
-	// 	})
+		// Second time we expect should be cached
+		response, err = strategy.ShouldBid(context.Background(), bidstrategy.BidStrategyRequest{
+			Job: jobForDockerImage("ubuntu:latest"),
+		})
 
-	// 	require.NoError(t, err)
-	// 	require.Equal(t, true, response.ShouldBid)
+		require.NoError(t, err)
+		require.Equal(t, true, response.ShouldBid)
 
-	// 	// We expect the cache to contain one item,
-	// 	// and have called Set twice, and Get twice with
-	// 	// one successful and one failed lookup.
-	// 	require.Equal(t, 1, fc.ItemCount())
-	// 	require.Equal(t, 2, fc.SetCalls)
-	// 	require.Equal(t, 2, fc.GetCalls)
-	// 	require.Equal(t, 1, fc.SuccessfulGetCalls)
+		// We expect the cache to contain one item,
+		// and have called Set twice, and Get twice with
+		// one successful and one failed lookup.
+		require.Equal(t, 1, fc.ItemCount())
+		require.Equal(t, 2, fc.SetCalls)
+		require.Equal(t, 2, fc.GetCalls)
+		require.Equal(t, 1, fc.SuccessfulGetCalls)
 
-	// 	// Reset the cache to the default impl
-	// 	semantic.ManifestCache = previousCache
-	// })
+		// Reset the cache to the default impl
+		semantic.ManifestCache = previousCache
+	})
 }
