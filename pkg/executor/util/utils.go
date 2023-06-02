a68c9d685dacf505e3ae71d84568f87e686749af
frrist <forrest@protocol.ai>
2023-06-02 10:25:13 -0700

    rebase: clean up after rebase, remove python and pythonwasm

--- pkg/executor/util/utils.go
+++ pkg/executor/util/utils.go
@@ -10,9 +10,7 @@ import (
 	"github.com/bacalhau-project/bacalhau/pkg/config"
 	"github.com/bacalhau-project/bacalhau/pkg/executor"
 	"github.com/bacalhau-project/bacalhau/pkg/executor/docker"
-	"github.com/bacalhau-project/bacalhau/pkg/executor/language"
 	noop_executor "github.com/bacalhau-project/bacalhau/pkg/executor/noop"
-	pythonwasm "github.com/bacalhau-project/bacalhau/pkg/executor/python_wasm"
 	"github.com/bacalhau-project/bacalhau/pkg/executor/wasm"
 	"github.com/bacalhau-project/bacalhau/pkg/ipfs"
 	"github.com/bacalhau-project/bacalhau/pkg/model"
@@ -153,20 +151,6 @@ func NewStandardExecutorProvider(
 		dockerengine.EngineType: dockerExecutor,
 		wasmengine.EngineType:   wasmExecutor,
 	})
-
-	// language executors wrap other executors, so pass them a reference to all
-	// the executors so they can look up the ones they need
-	exLang, err := language.NewExecutor(ctx, cm, executors)
-	if err != nil {
-		return nil, err
-	}
-	executors.Add(model.EngineLanguage, exLang)
-
-	exPythonWasm, err := pythonwasm.NewExecutor(executors)
-	if err != nil {
-		return nil, err
-	}
-	executors.Add(model.EnginePythonWasm, exPythonWasm)
 
 	return executors, nil
 }
