d230413638588504e256f5e17eb95dae0f3f8dd0
frrist <forrest@protocol.ai>
2023-06-06 13:23:22 -0700

    fix: update APIVersion to V1beta2
    - Because v1beta1 and v1alpha1 have been removed we may to remove any existing reference to them.

--- pkg/model/apiversion_string.go
+++ pkg/model/apiversion_string.go
@@ -11,12 +11,13 @@ func _() {
 	_ = x[apiVersionUnknown-0]
 	_ = x[V1alpha1-1]
 	_ = x[V1beta1-2]
-	_ = x[apiVersionDone-3]
+	_ = x[V1beta2-3]
+	_ = x[apiVersionDone-4]
 }
 
-const _APIVersion_name = "apiVersionUnknownV1alpha1V1beta1apiVersionDone"
+const _APIVersion_name = "apiVersionUnknownV1alpha1V1beta1V1beta2apiVersionDone"
 
-var _APIVersion_index = [...]uint8{0, 17, 25, 32, 46}
+var _APIVersion_index = [...]uint8{0, 17, 25, 32, 39, 53}
 
 func (i APIVersion) String() string {
 	if i < 0 || i >= APIVersion(len(_APIVersion_index)-1) {
