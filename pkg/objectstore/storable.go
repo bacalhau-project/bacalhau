288e60f5c7a2ec3c52f78b3ca45ab7bf10fd5dde
Ross Jones <ross@register-dynamics.co.uk>
2023-07-04 10:21:29 +0100

    Remove objectstore abstraction

--- pkg/objectstore/storable.go
+++
@@ -1,9 +0,0 @@
-package objectstore
-
-// Indexable defines the interface we expect any entities being stored to
-// implement.  This allows us to ask the entities how they would like to
-// be indexed (or how to clean up after deletion).
-type Indexable interface {
-	OnUpdate() []Indexer
-	OnDelete() []Indexer
-}
