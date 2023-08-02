026cf880d5466cb18301fd63f52cf3f343367eee
Enrico Rotundo <enrico.rotundo@gmail.com>
2023-08-02 10:12:42 +0000

    feat(flyte): add support for BacalhauConfig in Task

--- integration/flyte/plugins/flytekit-bacalhau/flytekitplugins/bacalhau/task.py
+++ integration/flyte/plugins/flytekit-bacalhau/flytekitplugins/bacalhau/task.py
@@ -3,6 +3,9 @@ import os
 from dataclasses import dataclass
 from typing import Any, Dict, Optional, Type
 
+from google.protobuf import json_format
+from google.protobuf import struct_pb2
+
 from flytekit.configuration import SerializationSettings
 from flytekit.extend import Interface, PythonTask, context_manager
 from flytekit.extend.backend.base_agent import AsyncAgentExecutorMixin
@@ -39,8 +42,16 @@ class BacalhauTask(AsyncAgentExecutorMixin, PythonTask):
     def __init__(
         self,
         name: str,
+        task_config=Optional[BacalhauConfig],
         **kwargs,
     ):
+        """
+        To be used to run a Bacalhau job.
+
+        :param name: Name of this task
+        :param task_config: BacalhauConfig object
+        :param kwargs: All other args required by Parent type - PythonTask
+        """
         interface = Interface(
             inputs=kwtypes(
                 spec=dict,
@@ -48,16 +59,22 @@ class BacalhauTask(AsyncAgentExecutorMixin, PythonTask):
             ),
             outputs=kwtypes(results=str)
         )
-
         super(BacalhauTask, self).__init__(
             task_type=self._TASK_TYPE,
             name=name,
-            task_config=None,
+            task_config=task_config,
             interface=interface,
             # environment put ENV VAR into this param?
             **kwargs,
         )
 
+    def get_custom(self, settings: SerializationSettings) -> Dict[str, Any]:
+        config = {
+            "BacalhauApiHost": self.task_config.BacalhauApiHost,
+            "BacalhauApiPort": self.task_config.BacalhauApiPort,
+            "BacalhauDir": self.task_config.BacalhauDir,
+        }
+        s = struct_pb2.Struct()
+        s.update(config)
+        return json_format.MessageToDict(s)
     
-    # def get_config(self) -> BacalhauConfig:
-    #     return self._task_config
