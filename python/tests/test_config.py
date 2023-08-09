53c100bff58c7121c2a7c3e54a57c6320f2a2bb9
Ross Jones <ross@register-dynamics.co.uk>
2023-08-09 15:33:02 +0100

    Update isort of python tests

--- python/tests/test_config.py
+++ python/tests/test_config.py
@@ -136,12 +136,7 @@ def test_sign_for_client():
     from Crypto.Hash import SHA256
     from Crypto.Signature import pkcs1_15
 
-    from bacalhau_sdk.config import (
-        get_client_id,
-        get_user_id_key,
-        init_config,
-        sign_for_client,
-    )
+    from bacalhau_sdk.config import get_client_id, get_user_id_key, init_config, sign_for_client
 
     _ = init_config()
 
