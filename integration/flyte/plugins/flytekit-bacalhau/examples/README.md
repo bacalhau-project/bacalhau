7a1841b8f32b281b0ea86e4c8e88b4b8ea0eb58b
enricorotundo <enrico.rotundo@gmail.com>
2023-08-18 18:29:09 +0200

    feat(flyte): examples WIP

--- integration/flyte/plugins/flytekit-bacalhau/examples/README.md
+++ integration/flyte/plugins/flytekit-bacalhau/examples/README.md
@@ -1,5 +1,17 @@
 # flytekit-bacalhau examples
 
 ```shell
-$ pyflyte run ./hello-world.py wf
+# build docker container
+$ make docker-build
+
+$ docker run --rm -v $(pwd):/examples \
+        -w /examples \
+		winderresearch/flytekit-bacalhau-examples:latest \
+		pyflyte run ./hello-world.py wf
+
+$ docker run --rm -v $(pwd):/examples \
+        -w /examples \
+        -a stdout -a stderr \
+		winderresearch/flytekit-bacalhau-examples:latest \
+		pyflyte run --verbose ./chained-jobs.py wf
 ```
--- integration/flyte/plugins/flytekit-bacalhau/examples/chained-jobs.py
+++ integration/flyte/plugins/flytekit-bacalhau/examples/chained-jobs.py
@@ -12,6 +12,14 @@ bacalhau_task = BacalhauTask(
         )
     )
 
+bacalhau_task_2 = BacalhauTask(
+        name="second_hello_world",
+        inputs=kwtypes(
+                spec=dict,
+                api_version=str,
+        )
+    )
+
 # https://docs.flyte.org/projects/cookbook/en/latest/auto_examples/basics/basic_workflow.html#how-does-a-flyte-workflow-work
 @task
 def print_cid(bac_task: str) -> str:
@@ -20,7 +28,7 @@ def print_cid(bac_task: str) -> str:
 
 @workflow
 def wf():
-    bac_task = bacalhau_task(
+    bac_task_1 = bacalhau_task(
         api_version="V1beta1",
         spec=dict(
             engine="Docker",
@@ -42,8 +50,37 @@ def wf():
             deal={"concurrency": 1},
         ),
     )
+
+    bac_task_2 = bacalhau_task_2(
+        api_version="V1beta1",
+        spec=dict(
+            engine="Docker",
+            verifier="Noop",
+            PublisherSpec={"type": "IPFS"},
+            docker={
+                "image": "ubuntu",
+                "entrypoint": ["echo", "Flyte is awesome!"],
+            },
+            language={"job_context": None},
+            wasm=None,
+            resources=None,
+            timeout=1800,
+            outputs=[{
+                    "storage_source": "IPFS",
+                    "name": "outputs",
+                    "path": "/outputs",
+            }],
+            inputs=[{
+                    "storage_source": "IPFS",
+                    "cid": bac_task_1,
+                    "name": "myinputs",
+                    "path": "/myinputs",
+            }],
+            deal={"concurrency": 1},
+        ),
+    )
     
-    print_cid(bac_task=bac_task)
+    print_cid(bac_task=bac_task_2)
 
 
 if __name__ == "__main__":
