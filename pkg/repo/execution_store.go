982c4f88daf62937e4af254dbb3e6df4186e8303
Ross Jones <ross@register-dynamics.co.uk>
2023-09-01 09:58:09 +0100

    Rename nodeID to less specific prefix

--- pkg/repo/execution_store.go
+++ pkg/repo/execution_store.go
@@ -18,13 +18,13 @@ import (
 // new ExecutionStore.  Where BoltDB is chosen, and no path is specified, then the database
 // will be created in the repo in a folder labeledafter the node ID.  For example:
 // `~/.bacalhau/Qmd1BEyR4RsLdYTEym1YxxaeXFdwWCMANYN7XCcpPYbTRs-compute/executions.db`
-func (fsr *FsRepo) InitExecutionStore(ctx context.Context, nodeID string) (store.ExecutionStore, error) {
+func (fsr *FsRepo) InitExecutionStore(ctx context.Context, prefix string) (store.ExecutionStore, error) {
 	if exists, err := fsr.Exists(); err != nil {
 		return nil, fmt.Errorf("failed to check if repo exists: %w", err)
 	} else if !exists {
 		return nil, fmt.Errorf("repo is uninitialized, cannot create ExecutionStore")
 	}
-	stateRootDir := filepath.Join(fsr.path, fmt.Sprintf("%s-compute", nodeID))
+	stateRootDir := filepath.Join(fsr.path, fmt.Sprintf("%s-compute", prefix))
 	if err := os.MkdirAll(stateRootDir, os.ModePerm); err != nil {
 		return nil, err
 	}
