63e6d980647775c986b31433b32eb885b38cf7a0
Walid Baruni <wdbaruni@gmail.com>
2023-09-18 14:14:19 +0200

    improve job submission error message

--- cmd/util/flags/cliflags/runtime.go
+++ cmd/util/flags/cliflags/runtime.go
@@ -16,6 +16,7 @@ type RunTimeSettings struct {
 	WaitForJobToFinish    bool // Wait for the job to finish before returning
 	WaitForJobTimeoutSecs int  // Timeout for waiting for the job to finish
 	PrintJobIDOnly        bool // Only print the Job ID as output
+	PrintNodeDetails      bool
 	Follow                bool // Follow along with the output of the job
 	DryRun                bool // iff true do not submit the job, but instead print out what will be submitted.
 }
@@ -30,6 +31,8 @@ func NewRunTimeSettingsFlags(settings *RunTimeSettings) *pflag.FlagSet {
 		`When using --wait, how many seconds to wait for the job to complete before giving up.`)
 	flags.BoolVar(&settings.PrintJobIDOnly, "id-only", settings.PrintJobIDOnly,
 		`Print out only the Job ID on successful submission.`)
+	flags.BoolVar(&settings.PrintNodeDetails, "node-details", settings.PrintNodeDetails,
+		`Print out details of all nodes (overridden by --id-only).`)
 	flags.BoolVarP(&settings.Follow, "follow", "f", settings.Follow,
 		`When specified will follow the output from the job as it runs`)
 	flags.BoolVar(
