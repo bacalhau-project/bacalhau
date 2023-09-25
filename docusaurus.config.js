5a3d4bba70cf8f0bfcd0b301baacc22eb56401c1
Alexandra <34590557+XandraMcC@users.noreply.github.com>
2023-09-25 13:08:10 +0200

    add script to config (#275)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,7 +18,15 @@ const config = {
   favicon: "img/logo.png",
   organizationName: "bacalhau-project", // Usually your GitHub org/user name.
   projectName: "docs.bacalhau.org", // Usually your repo name.
-  scripts: [{src: 'https://plausible.io/js/plausible.js', async: true, defer: true, 'data-domain': 'docs.bacalhau.org'}],
+  scripts: [
+    { src: 'https://plausible.io/js/plausible.js', async: true, defer: true, 'data-domain': 'docs.bacalhau.org' },
+    {
+      type: 'text/javascript',
+      innerHTML: `
+        !function(t){if(window.ko)return;window.ko=[],["identify","track","removeListeners","open","on","off","qualify","ready"].forEach(function(t){ko[t]=function(){var n=[].slice.call(arguments);return n.unshift(t),ko.push(n),ko}});var n=document.createElement("script");n.async=!0,n.setAttribute("src","https://cdn.getkoala.com/v1/pk_0946cebbb244e71a555bce34b10b6c0daed9/sdk.js"),(document.body || document.head).appendChild(n)}();
+        `,
+    },
+  ],
 
   presets: [
     [
