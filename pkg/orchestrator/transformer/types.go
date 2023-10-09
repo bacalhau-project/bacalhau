558fef52b6a77652e32c507e9c31eded4880bd1b
Walid Baruni <wdbaruni@gmail.com>
2023-10-09 10:25:12 +0200

    make  `name` optional in job spec and use jobID by default (#2884)

--- pkg/orchestrator/transformer/types.go
+++ pkg/orchestrator/transformer/types.go
@@ -6,18 +6,18 @@ import (
 	"github.com/bacalhau-project/bacalhau/pkg/models"
 )
 
-// Job is an interface that can be used to modify a job in place,
+// JobTransformer is an interface that can be used to modify a job in place,
 // such as setting default values or migrating.
-type Job interface {
+type JobTransformer interface {
 	Transform(context.Context, *models.Job) error
 }
 
-// JobFn is a function that implements Job transform interface.
+// JobFn is a function that implements JobTransformer transform interface.
 type JobFn func(context.Context, *models.Job) error
 
 func (fn JobFn) Transform(ctx context.Context, job *models.Job) error {
 	return fn(ctx, job)
 }
 
-// compile time check that JobFn implements Job
-var _ Job = JobFn(nil)
+// compile time check that JobFn implements JobTransformer
+var _ JobTransformer = JobFn(nil)
