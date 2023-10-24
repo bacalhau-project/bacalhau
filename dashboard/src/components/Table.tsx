6907cefeb1175077cb5e97cb863abd1ef12edbbd
AlexMcCarroll <alexandra@mccarrollhk.com>
2023-10-24 16:12:03 +0100

    adding dashboard with file structure and JobsDashboard

---
+++ dashboard/src/components/Table.tsx
@@ -0,0 +1,33 @@
+// src/components/Table.tsx
+
+import React from 'react'
+
+type Props = {
+  headers: string[]
+  data: string[][]
+}
+
+const Table: React.FC<Props> = ({ headers, data }) => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          {headers.map((header, index) => (
+            <th key={index}>{header}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((row, rowIndex) => (
+          <tr key={rowIndex}>
+            {row.map((cell, cellIndex) => (
+              <td key={cellIndex}>{cell}</td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  )
+}
+
+export default Table
---
+++ dashboard/src/pages/index.tsx
@@ -0,0 +1,16 @@
+// src/pages/index.tsx
+
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+
+const Home = () => {
+  const router = useRouter()
+
+  useEffect(() => {
+    router.replace('/JobsDashboard')
+  }, [])
+
+  return null
+}
+
+export default Home
