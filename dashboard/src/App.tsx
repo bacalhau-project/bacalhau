5e22a75033ea12455d359b7e4d9455d52b136371
AlexMcCarroll <alexandra@mccarrollhk.com>
2023-10-25 13:40:37 +0100

    adding sidebar, header, fixed scss

--- dashboard/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import '../styles/main.scss';
-import type { AppProps } from 'next/app'
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
-
-export default MyApp
