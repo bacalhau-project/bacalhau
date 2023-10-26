a625d95eacc1b2fd076056b5af1a57e5bc19fbb6
AlexMcCarroll <alexandra@mccarrollhk.com>
2023-10-26 17:26:58 +0100

    adding sidebar functionality, Nodes Dashboard, styling

--- dashboard/src/components/ProgramSummary.tsx
+++ dashboard/src/components/ProgramSummary.tsx
@@ -2,9 +2,11 @@
 
 import React from "react";
 import styles from "../../styles/ProgramSummary.module.scss";
+import cogwheel from "../../images/cogwheel-dark.png";
+import Image from "next/image";
 
 interface ProgramSummaryProps {
-  data: string; // Update this type based on the data you're passing
+    data: string;
 }
 
 const ProgramSummary: React.FC<ProgramSummaryProps> = ({ data }) => {
@@ -12,11 +14,11 @@ const ProgramSummary: React.FC<ProgramSummaryProps> = ({ data }) => {
     <div className={styles.programSummary}>
       <div className={styles.logo}>
         {/* Logo goes here */}
-        <img src="/logo.png" alt="Logo" />
+        <Image src={cogwheel} alt="Logo" width={20} height={20} />
       </div>
       <div className={styles.text}>
-        <div className={styles.header}>Header</div>
-        <div>{data}</div>
+        <div className={styles.header}>{data[0]}</div>
+        <div>{data[1]}</div>
       </div>
     </div>
   );
