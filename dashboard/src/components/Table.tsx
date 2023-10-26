cc4d86d9029bdcdb558f6c74798fb278dd962fa2
AlexMcCarroll <alexandra@mccarrollhk.com>
2023-10-26 09:51:58 +0100

    fixes

--- dashboard/src/components/Table.tsx
+++ dashboard/src/components/Table.tsx
@@ -1,41 +1,36 @@
 // src/components/Table.tsx
-import React from 'react';
-import ProgramSummary from './ProgramSummary';
+import React from "react";
+import ProgramSummary from "./ProgramSummary";
 import styles from "../../styles/Table.module.scss";
 
-
 interface TableProps {
-    headers: string[];
-    data: string[][];
+  headers: string[];
+  data: string[][];
 }
 
 const Table: React.FC<TableProps> = ({ headers, data }) => {
   return (
     <div className={styles.tableContainer}>
       <table>
-          <thead>
-              <tr>
-                  {headers.map((header, index) => (
-                      <th key={index}>{header}</th>
-                  ))}
-              </tr>
-          </thead>
-          <tbody>
-              {data.map((row, rowIndex) => (
-                  <tr key={rowIndex}>
-                      {row.map((cell, cellIndex) => (
-                          <td key={cellIndex}>
-                              {/* Use the ProgramSummary component in the 5th column (cellIndex 4) */}
-                              {cellIndex === 3 ? (
-                                  <ProgramSummary data={cell} />
-                              ) : (
-                                  cell
-                              )}
-                          </td>
-                      ))}
-                  </tr>
+        <thead>
+          <tr>
+            {headers.map((header, index) => (
+              <th key={index}>{header}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((row, rowIndex) => (
+            <tr key={rowIndex}>
+              {row.map((cell, cellIndex) => (
+                <td key={cellIndex}>
+                  {/* Use the ProgramSummary component in the 5th column (cellIndex 4) */}
+                  {cellIndex === 3 ? <ProgramSummary data={cell} /> : cell}
+                </td>
               ))}
-          </tbody>
+            </tr>
+          ))}
+        </tbody>
       </table>
     </div>
   );
