2e4becff4bac64d3ff9598a8a43ceb88e6184db1
AlexMcCarroll <alexandra@mccarrollhk.com>
2023-10-26 09:51:58 +0100

    fixes

--- dashboard/src/components/ProgramSummary.tsx
+++ dashboard/src/components/ProgramSummary.tsx
@@ -1,29 +1,25 @@
 // src/components/ProgramSummary.tsx
 
-import React from 'react';
-import styles from '../../styles/ProgramSummary.module.scss';
+import React from "react";
+import styles from "../../styles/ProgramSummary.module.scss";
 
 interface ProgramSummaryProps {
-    data: string;  // Update this type based on the data you're passing
+  data: string; // Update this type based on the data you're passing
 }
 
 const ProgramSummary: React.FC<ProgramSummaryProps> = ({ data }) => {
-    return (
-        <div className={styles.programSummary}>
-            <div className={styles.logo}>
-                {/* Logo goes here */}
-                <img src="/logo.png" alt="Logo" />
-            </div>
-            <div className={styles.text}>
-                <div className={styles.header}>
-                    Header
-                </div>
-                <div>
-                    {data}
-                </div>
-            </div>
-        </div>
-    );
+  return (
+    <div className={styles.programSummary}>
+      <div className={styles.logo}>
+        {/* Logo goes here */}
+        <img src="/logo.png" alt="Logo" />
+      </div>
+      <div className={styles.text}>
+        <div className={styles.header}>Header</div>
+        <div>{data}</div>
+      </div>
+    </div>
+  );
 };
 
 export default ProgramSummary;
