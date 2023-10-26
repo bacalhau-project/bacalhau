c6ef3553bafba2713d718207152410409f2c8f1d
AlexMcCarroll <alexandra@mccarrollhk.com>
2023-10-26 17:26:58 +0100

    adding sidebar functionality, Nodes Dashboard, styling

--- dashboard/src/components/Layout.tsx
+++ dashboard/src/components/Layout.tsx
@@ -16,7 +16,7 @@ const Layout: React.FC<LayoutProps> = ({ pageTitle, children }) => {
   return (
     <div className={styles.layout}>
       <Sidebar isCollapsed={isSidebarCollapsed} toggleSidebar={toggleSidebar} />
-      <div className={styles.rightSide}>
+      <div className={`${styles.rightSide} ${isSidebarCollapsed ? '' : styles.expandedSidebar}`}>
         <Header pageTitle={pageTitle} />
         <main>{children}</main>
       </div>
