73f9cda77a3afb884cb0f2ba29f12748b87a41f6
AlexMcCarroll <alexandra@mccarrollhk.com>
2023-11-02 14:39:50 +0000

    adding docker img

--- dashboard/src/components/ProgramSummary.tsx
+++ dashboard/src/components/ProgramSummary.tsx
@@ -1,24 +1,33 @@
 // src/components/ProgramSummary.tsx
 import React from "react";
 import styles from "../../styles/ProgramSummary.module.scss";
-import cogwheel from "../../images/cogwheel-dark.png";
 import Image from "next/image";
 import { EngineSpec } from "../interfaces";
+
+// Image imports
+import cogwheel from "../../images/cogwheel-dark.png";
+import dockerImage from "../../images/docker.png"; 
 
 interface ProgramSummaryProps {
   data: EngineSpec;
 }
 
+const getImageSource = (type: string) => {
+  switch(type) {
+    case 'docker':
+      return dockerImage;
+    default:
+      return cogwheel;
+  }
+}
+
 const ProgramSummary: React.FC<ProgramSummaryProps> = ({ data }) => {
   const { Image: image, Parameters: input } = data.Params;
-  console.log("data", data);
-  console.log("image", image);
-  console.log("input", input);
-
+  const imageSource = getImageSource(data.Type);
   return (
     <div className={styles.programSummary}>
       <div className={styles.logo}>
-        <Image src={cogwheel} alt="Logo" width={20} height={20} />
+        <Image src={imageSource} alt="Logo" width={20} height={20} />
       </div>
       <div className={styles.text}>
         <div className={styles.header}>{image}</div>
