d65a83f9ef93acd217d691c0ddfac7e66dafa928
AlexMcCarroll <alexandra@mccarrollhk.com>
2023-11-03 14:48:32 +0000

    styling updates + conditionals

--- dashboard/src/components/ActionButton.tsx
+++ dashboard/src/components/ActionButton.tsx
@@ -1,19 +1,19 @@
-import React from 'react';
+import React from "react";
 import Image from "next/image";
 import styles from "../../styles/ActionButton.module.scss";
-import icon from "../../images/view-icon.png"; 
+import icon from "../../images/view-icon.png";
 
 interface ActionButtonProps {
-    text: string;
+  text: string;
 }
 
 const ActionButton: React.FC<ActionButtonProps> = ({ text }) => {
-    return (
-        <button className={styles.actionButton}>
-            <Image src={icon} alt="Logo" width={20} />
-            {text}
-        </button>
-    );
-}
+  return (
+    <button className={styles.actionButton}>
+      <Image src={icon} alt="Logo" width={20} />
+      {text}
+    </button>
+  );
+};
 
 export default ActionButton;
