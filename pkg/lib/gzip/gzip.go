fde4e41c2e1f3cbd8c21ace2a4f449d9de1b77ce
Walid Baruni <wdbaruni@gmail.com>
2023-11-08 15:58:43 +0200

    Update pkg/lib/gzip/gzip.go
    Co-authored-by: Ross Jones <ross@register-dynamics.co.uk>

--- pkg/lib/gzip/gzip.go
+++ pkg/lib/gzip/gzip.go
@@ -18,7 +18,7 @@ func Decompress(tarGzPath, destDir string) error {
 }
 
 // DecompressWithMaxBytes takes the path to a .tar.gz file and decompresses it into the specified directory.
-// It enforces a maximum decompressed file size to prevent decompression bombs.
+// It enforces a maximum decompressed file size (per file) to prevent decompression bombs.
 func DecompressWithMaxBytes(tarGzPath, destDir string, maxDecompressSize int64) error {
 	// Open the tar.gz file for reading.
 	file, err := os.Open(tarGzPath)
