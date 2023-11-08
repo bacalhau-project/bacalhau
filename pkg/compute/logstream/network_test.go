271baaee0fc7b3722eaf2329a58c06a386dd19a6
Ross Jones <ross@register-dynamics.co.uk>
2023-11-08 11:38:22 +0000

    Remove unnecessary comments

--- pkg/compute/logstream/network_test.go
+++ pkg/compute/logstream/network_test.go
@@ -19,15 +19,6 @@ func TestNetworkTestSuite(t *testing.T) {
 	suite.Run(t, new(NetworkTestSuite))
 }
 
-// "127.0.0.0/8",    // IPv4 loopback
-// "10.0.0.0/8",     // RFC1918
-// "172.16.0.0/12",  // RFC1918
-// "192.168.0.0/16", // RFC1918
-// "169.254.0.0/16", // RFC3927 link-local
-// "::1/128",        // IPv6 loopback
-// "fe80::/10",      // IPv6 link-local
-// "fc00::/7",       // IPv6 unique local addr
-
 func (s *NetworkTestSuite) TestMultiAddrSorting() {
 
 	type testcase struct {
