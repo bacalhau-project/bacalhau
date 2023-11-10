c155f0d5b4971e67bda49f74839db8ebcd7fb454
Nasfame <laciferin@gmail.com>
2023-11-10 15:26:04 +0530

    test: remove mock implementations
    Took 32 seconds

--- pkg/executor/wasm/executor_test.go
+++ pkg/executor/wasm/executor_test.go
@@ -5,39 +5,10 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/mock"
 
 	"github.com/bacalhau-project/bacalhau/pkg/executor"
 	"github.com/bacalhau-project/bacalhau/pkg/models"
 )
-
-// Mocking necessary dependencies and structures
-
-type mockHandler struct {
-	mock.Mock
-}
-
-func (m *mockHandler) active() bool {
-	args := m.Called()
-	return args.Bool(0)
-}
-
-func (m *mockHandler) run(ctx context.Context) {
-	_ = m.Called(ctx)
-}
-
-type mockExecutor struct {
-	mock.Mock
-}
-
-func (m *mockExecutor) Get(executionID string) (*mockHandler, bool) {
-	args := m.Called(executionID)
-	return args.Get(0).(*mockHandler), args.Bool(1)
-}
-
-func (m *mockExecutor) Put(executionID string, handler *mockHandler) {
-	_ = m.Called(executionID, handler)
-}
 
 func TestFailingRequestedMemGreaterThan4GB(t *testing.T) {
 	e, err := NewExecutor()
