c62f92d873c9fabf2571516e9244f05704372cd1
AlexMcCarroll <alexandra@mccarrollhk.com>
2023-11-13 14:06:57 +0000

    create nodes dashboard

--- dashboard/src/pages/NodesDashboard.tsx
+++ dashboard/src/pages/NodesDashboard.tsx
@@ -1,13 +1,38 @@
-// src/pages/JobsDashboard.tsx
+// src/pages/NodesDashboard.tsx
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "../../styles/NodesDashboard.module.scss";
+import JobsTable from "../components/NodesTable";
 import Layout from "../components/Layout";
+// import { Nodes } from "../helpers/interfaces";
+import { bacalhauAPI } from "./api/bacalhau";
 
 const NodesDashboard: React.FC = () => {
+  // const [data, setData] = useState<Nodes[]>([]);
+
+  // async function getNodesData() {
+  //   try {
+  //     const response = await bacalhauAPI.listNodes();
+  //     if (response.Nodes) {
+  //       setData(response.Nodes);
+  //     }
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // }
+
+  // useEffect(() => {
+  //   getJobsData();
+  // }, []);
+
+  //TEMP
+  const data: any = ["dataa"]
+
   return (
     <Layout pageTitle="Nodes Dashboard">
-      <div className={styles.nodesdashboard}></div>
+      <div className={styles.nodesDashboard}>
+        <JobsTable data={data} />
+      </div>
     </Layout>
   );
 };
