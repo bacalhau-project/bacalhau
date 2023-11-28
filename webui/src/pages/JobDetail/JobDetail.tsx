a4ed0fbcb179ae218e9f080efc9e3576a222e3ea
AlexMcCarroll <alexandra@mccarrollhk.com>
2023-11-28 19:03:50 +0700

    Adding containers to Job Detail page

--- webui/src/pages/JobDetail/JobDetail.tsx
+++ webui/src/pages/JobDetail/JobDetail.tsx
@@ -1,11 +1,26 @@
-import React, { useEffect, useState } from "react";
 import styles from "./JobDetail.module.scss";
 import Layout from "../../layout/Layout";
+import Container from "../../components/Container/Container";
 
 const JobDetail: React.FC = () => {
   return (
     <Layout pageTitle="Job Detail">
       <div className={styles.jobDetail}>
+        <div>
+          <Container title={"Job Overview"}/>
+          <Container title={"Execution Record"}/>
+        </div>
+        <div>
+          <Container title={"Execution Details"}/>
+          <Container title={"Standard Output"}/>
+          <Container title={"Execution Logs"}/>
+        </div>
+        <div>
+          <Container title={"Inputs"}/>
+          <Container title={"Input"}/>
+          <Container title={"Outputs"}/>
+          <Container title={"Output"}/>
+        </div>
       </div>
     </Layout>
   );
