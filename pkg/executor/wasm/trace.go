36b1428b573bd8575600ca651fa4ac12184648ef
zach <zach@dylibso.com>
2023-11-30 16:46:19 -0800

    cleanup: don't call traceCtx.SetTraceId when traceCtx is nil

--- pkg/executor/wasm/trace.go
+++ pkg/executor/wasm/trace.go
@@ -143,7 +143,9 @@ func (t tracedFunction) Call(ctx context.Context, params ...uint64) ([]uint64, e
 	)
 	defer span.End()
 
-	t.traceCtx.SetTraceId(span.SpanContext().TraceID().String())
+	if t.traceCtx != nil {
+		t.traceCtx.SetTraceId(span.SpanContext().TraceID().String())
+	}
 	return telemetry.RecordErrorOnSpanTwo[[]uint64](span)(t.Function.Call(ctx, params...))
 }
 
@@ -158,8 +160,9 @@ func (t tracedFunction) CallWithStack(ctx context.Context, stack []uint64) error
 		trace.WithAttributes(semconv.CodeFunction(t.Function.Definition().Name())),
 	)
 	defer span.End()
-
-	t.traceCtx.SetTraceId(span.SpanContext().TraceID().String())
+	if t.traceCtx != nil {
+		t.traceCtx.SetTraceId(span.SpanContext().TraceID().String())
+	}
 	return telemetry.RecordErrorOnSpan(span)(t.Function.CallWithStack(ctx, stack))
 }
 
