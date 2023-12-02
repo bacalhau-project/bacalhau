430e062de57ee0d1445699eb1ac93aa957ab3eb3
AlexMcCarroll <alexandra@mccarrollhk.com>
2023-12-02 18:52:42 +0700

    refactor - still infinite looping

--- webui/src/pages/JobDetail/JobDetail.tsx
+++ webui/src/pages/JobDetail/JobDetail.tsx
@@ -4,15 +4,19 @@ import { bacalhauAPI } from "../../services/bacalhau";
 import { Job, Execution } from "../../helpers/jobInterfaces";
 import styles from "./JobDetail.module.scss";
 import Layout from "../../layout/Layout";
-import { getShortenedJobID } from "../../helpers/helperFunctions";
+import { getShortenedJobID, fromTimestamp, capitalizeFirstLetter } from "../../helpers/helperFunctions";
 import Container from "../../components/Container/Container";
+import Table from '../../components/Table/Table';
 import JobInfo from './JobInfo/JobInfo';
 
 const JobDetail: React.FC = () => {
   const { jobId } = useParams<{ jobId?: string }>();
   const [jobData, setJobData] = useState<Job | null>(null);
-  const [jobExData, setjobExData] = useState<Execution[] | null>(null);
+  const [jobExData, setJobExData] = useState<Execution[] | null>(null);
+  const [selectedExecution, setSelectedExecution] = useState<Execution | undefined>(undefined);
 
+
+  // TODO: WHY IS THIS AN INFINITE LOOOOPPPPP????
   async function getJobData() {
     if (jobId) {
       try {
@@ -31,7 +35,7 @@ const JobDetail: React.FC = () => {
       try {
         const response = await bacalhauAPI.jobExecution(jobId);
         if (response.Executions) {
-          setjobExData(response.Executions);
+          setJobExData(response.Executions);
         }
       } catch (error) {
         console.error('Failed to fetch job data:', error);
@@ -44,23 +48,53 @@ const JobDetail: React.FC = () => {
     getJobExecutionsData();
   }, []);
 
+  const manyExecutions = jobExData != null && jobExData.length > 1;
+  
+  const tableData = {
+    headers: ["ID", "Created", "Modified", "Node ID", "Status", "Action"],
+    rows: jobExData?.map(item => ({
+      "ID": item.ID,
+      "Created": fromTimestamp(item.CreateTime).toString(),
+      "Modified": fromTimestamp(item.ModifyTime).toString(),
+      "Node ID": item.NodeID,
+      "Status": capitalizeFirstLetter(item.DesiredState.Message),
+      "Action": <button onClick={() => handleShowClick(item)}>Show</button>
+    })) || []
+  };
+
+  useEffect(() => {
+    if (jobExData && jobExData.length > 0) {
+      setSelectedExecution(jobExData[0]);
+    } else {
+      setSelectedExecution(undefined);
+    }
+  }, [jobExData]);
+
+  const handleShowClick = (item: Execution) => {
+    setSelectedExecution(item);
+  };
+
   if (!jobData || !jobExData) {
     return <div>Loading...</div>;
   }
 
-  const pageTitle = `Job Detail | ${ getShortenedJobID(jobData.ID)}`;
-
   return (
-    <Layout pageTitle={pageTitle}>
+    <Layout pageTitle={`Job Detail | ${ getShortenedJobID(jobData.ID)}`}>
       <div className={styles.jobDetail}>
         <div>
           <Container title={"Job Overview"}>
-            <JobInfo job={jobData} executions={jobExData} section="overview"/>
+            <JobInfo job={jobData} execution={selectedExecution} section="overview"/>
+            {manyExecutions && (
+              <div>
+                <span className={styles.key}>Executions List:</span>
+                    <Table data={tableData} style={{ fontSize: '12px' }}></Table>
+              </div>
+            )}
           </Container>
         </div>
         <div>
           <Container title={"Execution Record"}>
-            <JobInfo job={jobData} executions={jobExData} section="executionRecord"/>
+            <JobInfo job={jobData} execution={selectedExecution} section="executionRecord"/>
           </Container>
           <Container title={"Standard Output"}>
             {/* <CliView data={} /> */}
--- webui/src/pages/JobDetail/JobInfo/JobInfo.tsx
+++ webui/src/pages/JobDetail/JobInfo/JobInfo.tsx
@@ -1,12 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Execution, Job } from '../../../helpers/jobInterfaces';
 import { fromTimestamp, capitalizeFirstLetter } from "../../../helpers/helperFunctions";
 import styles from "./JobInfo.module.scss";
-import Table from '../../../components/Table/Table';
 
 interface JobInfoProps {
     job: Job;
-    executions: Execution[];
+    execution: Execution | undefined;
     section: 'overview' | 'executionRecord' | 'stdout' | 'stderr' | 'inputs' | 'outputs';
 }
 
@@ -15,99 +14,63 @@ interface DataItem {
     value: string | undefined;
 }  
 
-const JobInfo: React.FC<JobInfoProps> = ({ job, executions, section }) => {
-    const [selectedExecution, setSelectedExecution] = useState<Execution>();
+const JobInfo: React.FC<JobInfoProps> = ({ job, execution, section }) => {
     const [dataToDisplay, setDataToDisplay] = useState<DataItem[]>([]);
 
-    const manyExecutions = executions.length > 1;
-    const tableData = {
-        headers: ["ID", "Created", "Modified", "Node ID", "Status", "Action"],
-        rows: executions.map(item => ({
-            "ID": item.ID,
-            "Created": fromTimestamp(item.CreateTime).toString(),
-            "Modified": fromTimestamp(item.ModifyTime).toString(),
-            "Node ID": item.NodeID,
-            "Status": capitalizeFirstLetter(item.DesiredState.Message),
-            "Action": <button onClick={() => handleShowClick(item)}>Show</button>
-        }))
-    };
-
-    useEffect(() => {
-        if (executions.length > 0) {
-            setSelectedExecution(executions[0]);
-        } else {
-            setSelectedExecution(undefined);
-        }
-    }, [executions]);
-
-    useEffect(() => {
-        console.log('Selected Execution Updated:', selectedExecution);
-        console.log('Current Section:', section);
-        switch (section) {
-            case 'overview':
-                setDataToDisplay([
-                    { label: 'Job ID', value: job.ID },
-                    { label: 'Job Type', value: capitalizeFirstLetter(job.Type) },
-                    { label: 'Created', value: fromTimestamp(job.CreateTime).toString() },
-                    { label: 'Modified', value: fromTimestamp(job.ModifyTime).toString() },
-                    { label: 'Status', value: job.State.StateType },
-                    { label: 'Timeout Deadline', value: job.Tasks[0].Timeouts.ExecutionTimeout.toString() },
-                    { label: 'Executor Type', value: capitalizeFirstLetter(job.Tasks[0].Engine.Type) },
-                    { label: 'Image', value: job.Tasks[0].Engine.Params.Image },
-                    { label: 'GPU Details', value: job?.Tasks[0]?.Resources?.GPU ? job?.Tasks[0]?.Resources?.GPU : "Not specified" },
-                    { label: 'Timeout', value: job?.Tasks[0]?.Timeouts.ExecutionTimeout.toString() },
-                    { label: 'Requestor Node', value: job.Meta["bacalhau.org/requester.id"]},
-                    // { label: 'Concurrency', value: '' }
-                ]);
-                break;
-            case 'executionRecord':
-                if (selectedExecution) { 
-                    console.log('INSIDE', selectedExecution);
-                    setDataToDisplay([
-                        { label: 'Execution ID', value: selectedExecution.ID },
-                        { label: 'Initiation Time', value: fromTimestamp(selectedExecution.CreateTime).toString() },
-                        { label: 'Completion Time', value: fromTimestamp(selectedExecution.ModifyTime).toString() },
-                        { label: 'Exit Code', value: selectedExecution.RunOutput.exitCode?.toString() },
-                        { label: 'Execution Note', value: capitalizeFirstLetter(selectedExecution.DesiredState.Message) }
-                    ]);
-                }
-                break;
-            case 'stdout':
-                if (selectedExecution) { 
-                    setDataToDisplay([
-                        { label: 'Initiation Time', value: fromTimestamp(selectedExecution.CreateTime).toString() },
-                    ]);
-                }
-                break;
-            case 'stderr':
-                if (selectedExecution) { 
-                    setDataToDisplay([
-                        { label: 'Initiation Time', value: fromTimestamp(selectedExecution.CreateTime).toString() },
-                    ]);
-                }
-                break;
-            case 'inputs':
-                if (selectedExecution) { 
-                    setDataToDisplay([
-                        { label: 'Initiation Time', value: fromTimestamp(selectedExecution.CreateTime).toString() },
-                    ]);
-                }
-                break;
-            case 'outputs':
-                if (selectedExecution) { 
-                    setDataToDisplay([
-                        { label: 'Initiation Time', value: fromTimestamp(selectedExecution.CreateTime).toString() },
-                    ]);
-                }
-                break;
-            default:
-                break;
-        }
-    }, [selectedExecution, section]);
-
-    const handleShowClick = (item: Execution) => {
-        setSelectedExecution(item);
-    };
+    switch (section) {
+        case 'overview':
+            setDataToDisplay([
+                { label: 'Job ID', value: job.ID },
+                { label: 'Job Type', value: capitalizeFirstLetter(job.Type) },
+                { label: 'Created', value: fromTimestamp(job.CreateTime).toString() },
+                { label: 'Modified', value: fromTimestamp(job.ModifyTime).toString() },
+                { label: 'Status', value: job.State.StateType },
+                { label: 'Timeout Deadline', value: job.Tasks[0].Timeouts.ExecutionTimeout.toString() },
+                { label: 'Executor Type', value: capitalizeFirstLetter(job.Tasks[0].Engine.Type) },
+                { label: 'Image', value: job.Tasks[0].Engine.Params.Image },
+                { label: 'GPU Details', value: job?.Tasks[0]?.Resources?.GPU ? job?.Tasks[0]?.Resources?.GPU : "Not specified" },
+                { label: 'Timeout', value: job?.Tasks[0]?.Timeouts.ExecutionTimeout.toString() },
+                { label: 'Requestor Node', value: job.Meta["bacalhau.org/requester.id"]},
+                // { label: 'Concurrency', value: '' }
+            ]);
+            break;
+        case 'executionRecord':
+            if (execution !== undefined)
+            setDataToDisplay([
+                { label: 'Execution ID', value: execution.ID },
+                { label: 'Initiation Time', value: fromTimestamp(execution.CreateTime).toString() },
+                { label: 'Completion Time', value: fromTimestamp(execution.ModifyTime).toString() },
+                { label: 'Exit Code', value: execution.RunOutput.exitCode?.toString() },
+                { label: 'Execution Note', value: capitalizeFirstLetter(execution.DesiredState.Message) }
+            ]);
+            break;
+        case 'stdout':
+            if (execution !== undefined)
+            setDataToDisplay([
+                { label: 'Initiation Time', value: fromTimestamp(execution.CreateTime).toString() },
+            ]);
+            break;
+        case 'stderr':
+            if (execution !== undefined)
+            setDataToDisplay([
+                { label: 'Initiation Time', value: fromTimestamp(execution.CreateTime).toString() },
+            ]);
+            break;
+        case 'inputs':
+            if (execution !== undefined)
+            setDataToDisplay([
+                { label: 'Initiation Time', value: fromTimestamp(execution.CreateTime).toString() },
+            ]);
+            break;
+        case 'outputs':
+            if (execution !== undefined)
+            setDataToDisplay([
+                { label: 'Initiation Time', value: fromTimestamp(execution.CreateTime).toString() },
+            ]);
+            break;
+        default:
+            break;
+    }
 
     return (
         <div className={styles.jobInfo}>
@@ -117,12 +80,6 @@ const JobInfo: React.FC<JobInfoProps> = ({ job, executions, section }) => {
                     <span className={styles.value}>{item.value}</span> 
                 </p>
             ))}
-            {manyExecutions && section=='overview' && (
-              <div>
-                <span className={styles.key}>Executions List:</span>
-                    <Table data={tableData} style={{ fontSize: '12px' }}></Table>
-              </div>
-            )}
         </div>
     );
 };
