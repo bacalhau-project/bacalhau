f65d1d67705271a1e7c0f2e0cc3b2025593332e3
Alaric Snell-Pym <alaric@expanso.io>
2023-12-13 15:36:33 +0000

    Fix bug in displaying resource requirements when nodes reject a job

--- pkg/bidstrategy/resource/capacity_available_strategy.go
+++ pkg/bidstrategy/resource/capacity_available_strategy.go
@@ -34,7 +34,7 @@ func (s *AvailableCapacityStrategy) ShouldBidBasedOnUsage(
 	runningCapacity := s.runningCapacityTracker.GetAvailableCapacity(ctx)
 	enqueuedCapacity := s.enqueuedCapacityTracker.GetAvailableCapacity(ctx)
 	totalCapacity := runningCapacity.Add(enqueuedCapacity)
-	return bidstrategy.NewBidResponse(usage.LessThanEq(*totalCapacity), capacityReason, totalCapacity, usage), nil
+	return bidstrategy.NewBidResponse(usage.LessThanEq(*totalCapacity), capacityReason, totalCapacity, &usage), nil
 }
 
 // compile-time interface check
