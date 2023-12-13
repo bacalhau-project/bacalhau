9346903af9e600fdb4a2cb9fd477adc4c8331fbc
Alaric Snell-Pym <alaric@expanso.io>
2023-12-13 16:07:04 +0000

    Retry failed batch service jobs, by scheduling a new evaluation.
    In order to do this, we cripple previous_executions.go to penalise, but not
    forbid, trying a job again on a node.

---
+++ pkg/orchestrator/planner/re_evaluator.go
@@ -0,0 +1,41 @@
+package planner
+
+import (
+	"context"
+
+	"github.com/bacalhau-project/bacalhau/pkg/models"
+	"github.com/bacalhau-project/bacalhau/pkg/orchestrator"
+	"github.com/rs/zerolog/log"
+)
+
+// ReEvaluator is a planner implementation that emits events based on the job state.
+type ReEvaluator struct {
+	id           string
+	evaluationBroker orchestrator.EvaluationBroker
+}
+
+// ReEvaluatorParams holds the parameters for creating a new ReEvaluator.
+type ReEvaluatorParams struct {
+	ID           string
+	EvaluationBroker orchestrator.EvaluationBroker
+}
+
+// NewReEvaluator creates a new instance of ReEvaluator.
+func NewReEvaluator(params ReEvaluatorParams) *ReEvaluator {
+	return &ReEvaluator{
+		id:           params.ID,
+		evaluationBroker: params.EvaluationBroker,
+	}
+}
+
+// Process creates new evaluations, if the plan demands it
+func (s *ReEvaluator) Process(ctx context.Context, plan *models.Plan) error {
+	if plan.NewEvaluation != nil {
+		log.Debug().Msgf("ABS DEBUG: Re-enqueuing evaluation from plan")
+		return s.evaluationBroker.Enqueue(plan.NewEvaluation)
+	}
+	return nil
+}
+
+// compile-time check whether the ReEvaluator implements the Planner interface.
+var _ orchestrator.Planner = (*ReEvaluator)(nil)
