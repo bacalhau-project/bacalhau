4f8910ffa52ad215f9297dc52d2dc9aca75129fa
Ross Jones <ross@register-dynamics.co.uk>
2023-12-19 13:00:36 +0000

    Always run full lint

--- .gitprecommit/golangci-lint.sh
+++ .gitprecommit/golangci-lint.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
-if git rev-parse --verify HEAD >/dev/null 2>&1
-then
-    against=HEAD
-else
-    # Initial commit: diff against an empty tree object
-    EMPTY_TREE=$(git hash-object -t tree /dev/null)
-    against=${EMPTY_TREE}
-fi
 
-FILES=$(git diff --cached --name-only "${against}")
-if [[ -n "${FILES}" ]]; then
-    golangci-lint run --allow-parallel-runners --fix
-fi
+golangci-lint run --allow-parallel-runners --fix
