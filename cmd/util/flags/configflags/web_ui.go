eab1ec724e700ca6d384ec8c9a22e57c0d8330e3
Ross Jones <ross@register-dynamics.co.uk>
2023-12-19 16:04:15 +0000

    Allow specifying which port to run the webui on
    Currently the webui only runs on port 80, which requires privileges that
    users may not have.  To support alternatives, this PR adds a new flag
    (--web-ui-port) which allows the port to be specified.
    
    e.g.
    ```
    bacalhau serve --node-type=requester,compute --web-ui --web-ui-port=2020
    ```
    
    will run the webui on 0.0.0.0:2020

--- cmd/util/flags/configflags/web_ui.go
+++ cmd/util/flags/configflags/web_ui.go
@@ -5,8 +5,14 @@ import "github.com/bacalhau-project/bacalhau/pkg/config/types"
 var WebUIFlags = []Definition{
 	{
 		FlagName:     "web-ui",
-		ConfigPath:   types.NodeWebUI,
+		ConfigPath:   types.NodeWebUIEnabled,
 		DefaultValue: false,
 		Description:  `Whether to start the web UI alongside the bacalhau node.`,
+	},
+	{
+		FlagName:     "web-ui-port",
+		ConfigPath:   types.NodeWebUIPort,
+		DefaultValue: 80,
+		Description:  `The port number to listen on for web-ui connections.`,
 	},
 }
