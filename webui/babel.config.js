9d091b6cd75b36234048a2415e1e739546263bee
David Aronchick <aronchick@gmail.com>
2023-12-22 17:03:04 -0800

    cherry pick from main

---
+++ webui/babel.config.js
@@ -0,0 +1,3 @@
+export default function(api) {
+    const presets = ['@babel/preset-env', '@babel/preset-react'];
+}
---
+++ webui/tests/mocks/axios.ts
@@ -0,0 +1,12 @@
+const mockAxios: any = jest.createMockFromModule('axios');
+
+const mockData = {
+    data: {
+        Jobs: [ { id: 'job1' }, { id: 'job2' } ]
+    },
+};
+
+mockAxios.create = jest.fn(() => mockAxios);
+mockAxios.get = jest.fn(() => Promise.resolve(mockData));
+
+export default mockAxios;
