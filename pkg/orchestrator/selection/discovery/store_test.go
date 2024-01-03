dfaf741d9b4aea415962378eb10af2344b4774a0
Simon Worthington <simon@register-dynamics.co.uk>
2024-01-03 15:08:52 +1100

    Show error message when nodes don't support Docker
    This commit allows the CLI to show a node ranking error message when
    nodes don't have the available engine installed.
    
    We do this by removing the FindNodes method previously used for node
    discovery. This used to pre-filter the returned nodes to remove any
    that did not support the engine, but this meant that they wouldn't
    even appear to the ranking system and wouldn't appear on the CLI
    either. This was only here as a performance optimisation, but it's
    premature (ranking will still take milliseconds even with a 10,000
    node cluster).
    
    Now, we just list all nodes and filter them using the engine ranking
    strategy instead. In fact, this strategy has been present for months
    and is even enabled, it just never did anything because it was never
    passed any nodes to filter out. It works just fine, and provides users
    with a helpful error messgae, as below:
    
        % bacalhau docker run ubuntu date
        Using default tag: latest. Please specify a tag/digest for better reproducibility.
        Job successfully submitted. Job ID: 270ad777-1279-48f1-bc51-c7aa900a90e8
        Checking job status... (Enter Ctrl+C to exit at any time, your job will continue running):
    
                Communicating with the network  ................  done ✅  0.0s
                   Creating job for submission  ................  err  ❌  0.5s
    
        Error submitting job: not enough nodes to run job. requested: 1, available: 0.
                Node QmT5mqWF: does not support docker, only wasm

--- pkg/orchestrator/selection/discovery/store_test.go
+++ pkg/orchestrator/selection/discovery/store_test.go
@@ -34,30 +34,29 @@ func TestStoreNodeDiscovererSuite(t *testing.T) {
 	suite.Run(t, new(StoreNodeDiscovererSuite))
 }
 
-func (s *StoreNodeDiscovererSuite) TestFindNodes() {
+func (s *StoreNodeDiscovererSuite) TestListNodes() {
 	ctx := context.Background()
 	nodeInfo1 := generateNodeInfo("node1", models.EngineDocker)
-	nodeInfo2 := generateNodeInfo("node2", models.EngineDocker, models.EngineWasm)
 	s.NoError(s.store.Add(ctx, nodeInfo1))
-	s.NoError(s.store.Add(ctx, nodeInfo2))
 
 	// both nodes are returned when asked for docker nodes
 	job := mock.Job()
 	job.Task().Engine.Type = models.EngineDocker
 
-	peerIDs, err := s.discoverer.FindNodes(context.Background(), *job)
+	peerIDs, err := s.discoverer.ListNodes(context.Background())
+	s.NoError(err)
+	s.ElementsMatch([]models.NodeInfo{nodeInfo1}, peerIDs)
+
+	nodeInfo2 := generateNodeInfo("node2", models.EngineDocker, models.EngineWasm)
+	s.NoError(s.store.Add(ctx, nodeInfo2))
+	// only node2 is returned when asked for noop nodes
+	peerIDs, err = s.discoverer.ListNodes(context.Background())
 	s.NoError(err)
 	s.ElementsMatch([]models.NodeInfo{nodeInfo1, nodeInfo2}, peerIDs)
-
-	// only node2 is returned when asked for noop nodes
-	job2 := mock.Job()
-	peerIDs, err = s.discoverer.FindNodes(context.Background(), *job2)
-	s.NoError(err)
-	s.Empty(peerIDs)
 }
 
-func (s *StoreNodeDiscovererSuite) TestFindNodes_Empty() {
-	peerIDs, err := s.discoverer.FindNodes(context.Background(), *mock.Job())
+func (s *StoreNodeDiscovererSuite) TestListNodes_Empty() {
+	peerIDs, err := s.discoverer.ListNodes(context.Background())
 	s.NoError(err)
 	s.Empty(peerIDs)
 }
