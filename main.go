23b905c77a7800e1b07d2dd07ec2df9c750bbf4a
David Aronchick <aronchick@gmail.com>
2024-01-16 05:46:50 -0500

    Swagger PRs - 1 - MVP (#3224)

--- main.go
+++ main.go
@@ -16,6 +16,57 @@ import (
 	"github.com/bacalhau-project/bacalhau/pkg/logger"
 )
 
+//	@title			Bacalhau API
+//	@version		v1
+//	@description	The Bacalhau API is a RESTful API that allows you to interact with the Bacalhau network.
+//	@termsOfService	http://bacalhau.org/terms/
+
+// TODO: #3165 Host the terms of service on bacalhau.org/terms
+
+//	@contact.name	API Support
+//	@contact.url	https://www.expanso.io/contact/
+//	@contact.email	support@bacalhau.org
+
+// TODO: #3166 Host an email address and a contact form on bacalhau.org/support
+
+//	@license.name	Apache 2.0
+//	@license.url	http://www.apache.org/licenses/LICENSE-2.0.html
+
+//	@host		localhost:1234
+//	@BasePath	/api/v1
+
+// @externalDocs.description 	Bacalhau Documentation
+// @externalDocs.url			https://docs.bacalhau.org
+
+////	@securityDefinitions.basic	BasicAuth
+
+////	@securityDefinitions.apikey	ApiKeyAuth
+////	@in							header
+////	@name						Authorization
+////	@description				Description for what is this security definition being used
+
+// -- Add authentication to swagger here
+//// 	@securitydefinitions.oauth2.application	OAuth2Application
+//// 	@tokenUrl								https://example.com/oauth/token
+//// 	@scope.write							Grants write access
+//// 	@scope.admin							Grants read and write access to administrative information
+//
+//// 	@securitydefinitions.oauth2.implicit	OAuth2Implicit
+//// 	@authorizationUrl						https://example.com/oauth/authorize
+//// 	@scope.write							Grants write access
+//// 	@scope.admin							Grants read and write access to administrative information
+//
+//// 	@securitydefinitions.oauth2.password	OAuth2Password
+//// 	@tokenUrl								https://example.com/oauth/token
+//// 	@scope.read								Grants read access
+//// 	@scope.write							Grants write access
+//// 	@scope.admin							Grants read and write access to administrative information
+//
+//// 	@securitydefinitions.oauth2.accessCode	OAuth2AccessCode
+//// 	@tokenUrl								https://example.com/oauth/token
+//// 	@authorizationUrl						https://example.com/oauth/authorize
+//// 	@scope.admin							Grants read and write access to administrative information
+
 func main() {
 	defer func() {
 		// Make sure any buffered logs are written if something failed before logging was configured.
