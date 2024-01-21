52bbc1e8da4b7c7827436969d814ff15ec854ba0
Walid Baruni <wdbaruni@gmail.com>
2024-01-22 00:40:46 +0300

    add versioing for subject names

--- pkg/nats/proxy/constants.go
+++ pkg/nats/proxy/constants.go
@@ -6,16 +6,16 @@ const (
 	ComputeEndpointSubjectPrefix = "node.compute"
 	CallbackSubjectPrefix        = "node.orchestrator"
 
-	AskForBid       = "AskForBid/1"
-	BidAccepted     = "BidAccepted/1"
-	BidRejected     = "BidRejected/1"
-	CancelExecution = "CancelExecution/1"
-	ExecutionLogs   = "ExecutionLogs/1"
+	AskForBid       = "AskForBid/v1"
+	BidAccepted     = "BidAccepted/v1"
+	BidRejected     = "BidRejected/v1"
+	CancelExecution = "CancelExecution/v1"
+	ExecutionLogs   = "ExecutionLogs/v1"
 
-	OnBidComplete    = "OnBidComplete/1"
-	OnRunComplete    = "OnRunComplete/1"
-	OnCancelComplete = "OnCancelComplete/1"
-	OnComputeFailure = "OnComputeFailure/1"
+	OnBidComplete    = "OnBidComplete/v1"
+	OnRunComplete    = "OnRunComplete/v1"
+	OnCancelComplete = "OnCancelComplete/v1"
+	OnComputeFailure = "OnComputeFailure/v1"
 )
 
 func computeEndpointPublishSubject(nodeID string, method string) string {
