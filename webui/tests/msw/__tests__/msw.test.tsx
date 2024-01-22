167ad4fdaa4c3c858c55c167e0599ff521f2a177
David Aronchick <aronchick@gmail.com>
2024-01-21 20:50:37 -0800

    fixing linting

--- webui/tests/msw/__tests__/msw.test.tsx
+++ webui/tests/msw/__tests__/msw.test.tsx
@@ -1,10 +1,9 @@
 /* eslint-disable @typescript-eslint/no-unsafe-argument */
-import React from "react"
-import { render, screen, act, waitFor, queryByAttribute, RenderResult } from "@testing-library/react"
-import { server } from "../server"
-import { mockTestDataArray } from "../handlers"
+import { render, screen, act, waitFor } from "@testing-library/react"
 import { useState, useEffect } from "react"
 import crypto from "crypto"
+import { server } from "../server"
+import { mockTestDataArray } from "../handlers"
 
 // This is a simple file to test to make sure the configuration of msw is working
 // properly. All components, types, and methods are self contained here.
@@ -33,13 +32,14 @@ type TestDataItemProps = {
 }
 
 function TestDataItem({ testData }: TestDataItemProps) {
-  return (<>
-    {`${testData.userId},${testData.id},${testData.date.toString()},${testData.bool}`}{" "}
+  return (
+    <>
+      {`${testData.userId},${testData.id},${testData.date.toString()},${testData.bool}`}{" "}
     </>
   )
 }
 
-async function fetchTestData(returnData:boolean = false) {
+async function fetchTestData(returnData: boolean = false) {
   try {
     const appendString = returnData ? `?${RETURN_DATA_PARAMETER}=true` : ""
     const res = await fetch(`http://localhost/testData${appendString}`)
@@ -57,18 +57,18 @@ type MSWProps = {
   ShouldReturnData?: boolean
 }
 
-export const MSWTestComponent: React.FC<MSWProps>  = ({
+export const MSWTestComponent: React.FC<MSWProps> = ({
   ShouldReturnData = false,
 }) => {
-  const [testDataArray, setTestDataArray] = useState<TestData[]>([]);
+  const [testDataArray, setTestDataArray] = useState<TestData[]>([])
 
   useEffect(() => {
     async function fetchTestDataAsync() {
       const fetchedTestDataArray = await fetchTestData(ShouldReturnData)
       setTestDataArray(fetchedTestDataArray)
     }
-    fetchTestDataAsync();
-  }, []);
+    fetchTestDataAsync()
+  }, [ShouldReturnData])
 
   return (
     <div id="dataList">
@@ -87,33 +87,30 @@ export const MSWTestComponent: React.FC<MSWProps>  = ({
 
 describe("Basic tests of mocked API", () => {
   it("should GET React component backed by /testData with no test data", async () => {
-    await act(() => {
+    act(() => {
       render(<MSWTestComponent />)
     })
-    
+
     await waitFor(() => {
       // Query the sampleQuery endpoint and get a response.
       // Print the response to the console.
       expect(screen.getByText("No TestData")).toBeInTheDocument()
     })
-
   })
   it("should GET React component backed by /testData with two entries", async () => {
-    let result: RenderResult;
-    result = render(<MSWTestComponent ShouldReturnData={true} />);
-    const dom = result.container;
-    
+    const result = render(<MSWTestComponent ShouldReturnData />)
+    const dom = result.container
+
     await waitFor(() => {
       // Get the expect for the div with id "dataList"
-      const dataList = dom.querySelector("#dataList");
-      
-      expect(dataList).toBeInTheDocument();
-      
+      const dataList = dom.querySelector("#dataList")
+
+      expect(dataList).toBeInTheDocument()
+
       // Query the sampleQuery endpoint and get a response.
       // Print the response to the console.
       expect(dataList).toHaveTextContent(mockTestDataArray[0].id.toString())
       expect(dataList).toHaveTextContent(mockTestDataArray[1].id.toString())
     })
-
   })
 })
