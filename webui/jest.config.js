d89d3155d1896e9511bea07847faa99d3b134391
David Aronchick <aronchick@gmail.com>
2024-01-24 15:05:58 -0800

    ejection building

--- webui/jest.config.js
+++ webui/jest.config.js
@@ -1,25 +1,47 @@
 module.exports = {
   roots: ["<rootDir>/src", "<rootDir>/tests"],
+  collectCoverageFrom: [
+    "src/**/*.{js,jsx,ts,tsx}",
+    "!src/**/*.d.ts"
+  ],
   testMatch: [
     "**/__tests__/**/*.[jt]s?(x)",
     "**/?(*.)+(spec|test).[jt]s?(x)"
   ],
   transform: {
     "^.+\\.(ts|tsx)$": "ts-jest",
-    "\\.(css|scss)$": "jest-css-modules-transform",
-    "^.+\\.(js|jsx)$": "babel-jest",
+    "^.+\\.css$": "<rootDir>/config/jest/cssTransform.js",
+    "^.+\\.(js|jsx|mjs|cjs|ts|tsx)$": "<rootDir>/config/jest/babelTransform.js",
+    "^(?!.*\\.(js|jsx|mjs|cjs|ts|tsx|css|json)$)": "<rootDir>/config/jest/fileTransform.js",
   },
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-  setupFiles: ['<rootDir>/jest.polyfills.js'],
+  transformIgnorePatterns: [
+    "[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs|cjs|ts|tsx)$",
+    "^.+\\.module\\.(css|sass|scss)$"
+  ],
+  modulePaths: [],
+  moduleFileExtensions: ["ts",
+    "tsx",
+    "js",
+    "jsx",
+    "json",
+    "node",
+    "web.js",
+    "web.ts",
+    "web.tsx",
+    "web.jsx",
+  ],
+  setupFiles: ["react-app-polyfill/jsdom", '<rootDir>/jest.polyfills.js'],
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
   testPathIgnorePatterns: [
-  "<rootDir>/tests/mocks",
-  "<rootDir>/tests/setupTests.ts",
+    "<rootDir>/tests/mocks",
+    "<rootDir>/tests/setupTests.ts",
   ],
   moduleNameMapper: {
     "^@pages/(.*)$": "<rootDir>/src/pages/$1",
     "^@components/(.*)$": "<rootDir>/src/components/$1",
     '\\.svg$': '<rootDir>/tests/mocks/svgMock.js',
+    "^react-native$": "react-native-web",
+    "^.+\\.module\\.(css|sass|scss)$": "identity-obj-proxy"
   },
   testEnvironment: 'jsdom',
   testEnvironmentOptions: {
@@ -29,4 +51,5 @@ module.exports = {
   watchPlugins: ['jest-watch-select-projects', 'jest-watch-typeahead/filename', 'jest-watch-typeahead/testname'],
   watchman: false,
   maxWorkers: 1,
+  resetMocks: true,
 }
