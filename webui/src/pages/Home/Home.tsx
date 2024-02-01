a100dfa9a8ff2fe3c6be00a0e24b824e686a2040
David Aronchick <aronchick@gmail.com>
2024-01-31 18:45:03 -0800

    just linting fixes

--- webui/src/pages/Home/Home.tsx
+++ webui/src/pages/Home/Home.tsx
@@ -1,14 +1,12 @@
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useEffect } from "react"
+import { useNavigate } from "react-router-dom"
 
-const Home = () => {
-  const navigate = useNavigate();
+export const Home = () => {
+  const navigate = useNavigate()
 
   useEffect(() => {
-    navigate("/JobsDashboard", { replace: false });
-  }, [navigate]);
+    navigate("/JobsDashboard", { replace: false })
+  }, [navigate])
 
-  return null;
-};
-
-export default Home;
+  return null
+}
