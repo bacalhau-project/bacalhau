c223eb4df15a294b0c389b3ea86f2767c014e2c2
David Aronchick <aronchick@gmail.com>
2024-02-02 10:06:13 -0800

    3368 - adding svgs in static dir and tests (#3374)

---
+++ webui/src/images/__mocks__/svg-image.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+export const SVGImage = ({ src, alt }: { src: string; alt: string }) => (
+  <div>
+    SRC: {src} ALT:{alt}{" "}
+  </div>
+)
---
+++ webui/svg.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.svg$/i,
+                issuer: /\.[jt]sx?$/,
+                use: [{ loader: '@svgr/webpack', options: { icon: true } }],
+            },
+        ],
+    },
+}
