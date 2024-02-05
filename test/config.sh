1e17a48c9a1530d91c2a05df8c0ea2aa9d1833f3
Simon Worthington <simon@register-dynamics.co.uk>
2024-02-05 15:14:33 +1100

    Add external tests of Bacalhau CLI
    This commit adds an external testing framework that runs the Bacalhau
    CLI and asserts on the output and exit codes produced. These sorts of
    tests are useful checks that what we are running in production
    actually behaves the way we expect, and it should be possible to have
    more confidence in these test results then in running Go-based tests,
    which can inherit a lot of configuration and setup.

---
+++ test/config.sh
@@ -0,0 +1,33 @@
+#!bin/bashtub
+
+source bin/bacalhau.sh
+
+testcase_config_file_remains_empty_after_list() {
+    subject bacalhau config list
+    assert_equal 0 $status
+
+    subject ls $BACALHAU_DIR/config.yaml
+    assert_equal 1 $status
+}
+
+testcase_config_set_is_persistent() {
+    TEST_VALUE=$RANDOM
+    subject bacalhau config set 'User.InstallationID' $TEST_VALUE
+    assert_equal 0 $status
+
+    subject file $BACALHAU_DIR/config.yaml
+    assert_equal 0 $status
+
+    subject bacalhau config list --output=csv
+    assert_match "user.installationid,$TEST_VALUE" "$stdout"
+}
+
+testcase_config_default_ignores_persisted_values() {
+    TEST_VALUE=$RANDOM
+    subject bacalhau config set 'User.InstallationID' $TEST_VALUE
+    assert_equal 0 $status
+
+    subject bacalhau config default
+    assert_equal 0 $status
+    assert_match 'InstallationID: ""' "$stdout"
+}
