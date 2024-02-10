e77f95e42371daa38d5f748e5420516a5c2da62e
David Aronchick <aronchick@gmail.com>
2024-02-10 09:39:19 -0800

    yarn.lock

--- webui/vite.config.mjs
+++ webui/vite.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import viteTsconfigPaths from "vite-tsconfig-paths"
-import { defineConfig } from "vite"
 import browserslistToEsbuild from "browserslist-to-esbuild"
 
 export default defineConfig({
