86c0bde28c894f61ae4f273b3c50c9153248a648
David Aronchick <aronchick@gmail.com>
2024-02-10 11:43:56 -0800

    adding vite env directory and beginning to strip out unnecessary stuff

--- webui/vite.config.mjs
+++ webui/vite.config.mjs
@@ -1,17 +1,35 @@
-import { defineConfig } from "vite"
+import { defineConfig, loadEnv } from "vite"
+import { fileURLToPath } from "url"
 import react from "@vitejs/plugin-react"
 import viteTsconfigPaths from "vite-tsconfig-paths"
 import browserslistToEsbuild from "browserslist-to-esbuild"
+import dotenv from "dotenv"
 
-export default defineConfig({
-  // depending on your application, base can also be "/"
-  base: "",
-  plugins: [react(), viteTsconfigPaths()],
-  server: {
-    // this ensures that the browser opens upon server start
-    open: true,
-    // this sets a default port to 3000
-    port: 3000,
-  },
-  target: browserslistToEsbuild([">0.2%", "not dead", "not op_mini all"]),
+export default defineConfig(({ mode }) => {
+  dotenv.config() // load env vars from .env
+  const env = loadEnv(mode, process.cwd(), "")
+  return {
+    // depending on your application, base can also be "/"
+    base: "",
+    plugins: [react(), viteTsconfigPaths()],
+    envDir: "vite/env",
+    define: {
+      "process.env": env,
+      __VALUE__: `"${process.env.VALUE}"`, // wrapping in "" since it's a string
+      _global: {},
+    },
+    server: {
+      // this ensures that the browser opens upon server start
+      open: true,
+      // this sets a default port to 3000
+      port: 3000,
+    },
+    target: browserslistToEsbuild([">0.2%", "not dead", "not op_mini all"]),
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
+      mainFields: ["browser"],
+    },
+  }
 })
