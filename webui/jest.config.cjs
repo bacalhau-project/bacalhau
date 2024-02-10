89f5d122b2afbfee19a4dc8ac6303de53e1912f5
David Aronchick <aronchick@gmail.com>
2024-02-10 09:31:27 -0800

    jest updated

--- webui/jest.config.cjs
+++ webui/jest.config.cjs
@@ -1,4 +1,5 @@
 module.exports = {
+  preset: "ts-jest/presets/js-with-ts",
   roots: ["<rootDir>/src", "<rootDir>/tests"],
   testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
   transform: {
@@ -14,12 +15,15 @@ module.exports = {
     "<rootDir>/tests/setupTests.ts",
   ],
   moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/tests/mocks/fileMock.js",
+    "\\.(css|less)$": "<rootDir>/tests/mocks/styleMock.js",
     "^@pages/(.*)$": "<rootDir>/src/pages/$1",
     "^@components/(.*)$": "<rootDir>/src/components/$1",
     "\\.svg$": "<rootDir>/tests/mocks/svgMock.mjs",
     uuid: require.resolve("uuid"),
   },
-  testEnvironment: "jsdom",
+  testEnvironment: "jest-environment-jsdom",
   testEnvironmentOptions: {
     customExportConditions: [""],
   },
