5da140a84cbe3af86683832e2505f2b339ed25aa
David Aronchick <aronchick@gmail.com>
2024-02-18 08:44:16 -1000

    fixing pre-commit issue

--- pkg/util/templates/normalizers_test.go
+++ pkg/util/templates/normalizers_test.go
@@ -3,7 +3,6 @@
 package templates
 
 import (
-	"runtime"
 	"testing"
 
 	"github.com/bacalhau-project/bacalhau/pkg/util/stringutils"
@@ -12,14 +11,12 @@ import (
 )
 
 func TestLongDesc(t *testing.T) {
-	actual := LongDesc(pus.CrossPlatformNormalizeLineEndings(`
+	actual := LongDesc(`
 		Create a job from a file or from stdin.
 
 		JSON and YAML formats are accepted.
 
-`))
-
-	want := pus.CrossPlatformNormalizeLineEndings(`Create a job from a file or from stdin.
+`)
 
 	actual = stringutils.CrossPlatformNormalizeLineEndings(actual)
 	want := `Create a job from a file or from stdin.
@@ -40,12 +37,17 @@ func TestExamples(t *testing.T) {
   # Describe a job and include all server and local events
   bacalhau describe --include-events j-b6ad164a`)
 
-	assert.Equal(t, `  # Describe a job with the full ID
+	want := `  # Describe a job with the full ID
   bacalhau describe j-e3f8c209-d683-4a41-b840-f09b88d087b9
 
   # Describe a job with the a shortened ID
   bacalhau describe j-47805f5c
 
   # Describe a job and include all server and local events
-  bacalhau describe --include-events j-b6ad164a`, actual)
+  bacalhau describe --include-events j-b6ad164a`
+
+	actual = stringutils.CrossPlatformNormalizeLineEndings(actual)
+	want = stringutils.CrossPlatformNormalizeLineEndings(want)
+
+	assert.Equal(t, want, actual)
 }
