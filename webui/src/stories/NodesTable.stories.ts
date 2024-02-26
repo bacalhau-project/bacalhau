06dd2d97f3898b46642889d8c87f734e4a967de1
David Aronchick <aronchick@gmail.com>
2024-02-26 13:52:08 -0800

    fixing linter

--- webui/src/stories/NodesTable.stories.ts
+++ webui/src/stories/NodesTable.stories.ts
@@ -3,24 +3,24 @@ import { Node } from "../helpers/nodeInterfaces"
 import { NodesTable } from "../pages/NodesDashboard/NodesTable/NodesTable"
 
 export default {
-    component: NodesTable,
-    title: "NodesTable",
-    tags: ["autodocs"],
+  component: NodesTable,
+  title: "NodesTable",
+  tags: ["autodocs"],
 }
 
 export const fullDataGenerator = (numberToGenerate: number): Node[] => {
-    // Create a list of 10 jobs
-    const jobs: Node[] = []
-    for (let i = 0; i < numberToGenerate; i += 1) {
-        jobs.push(generateMockNode())
-    }
-    return jobs
+  // Create a list of 10 jobs
+  const jobs: Node[] = []
+  for (let i = 0; i < numberToGenerate; i += 1) {
+    jobs.push(generateMockNode())
+  }
+  return jobs
 }
 
 export const Default = {
-    args: { data: [] },
+  args: { data: [] },
 }
 
 export const FullData = {
-    args: { data: fullDataGenerator(10) },
+  args: { data: fullDataGenerator(10) },
 }
