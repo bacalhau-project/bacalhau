60d6cb07b817e37d4dda2bf6d671715c7f8ea659
Alaric Snell-Pym <alaric@expanso.io>
2024-03-01 14:04:31 +0000

    Altered retry strategy to return a retry delay as well as a boolean.
     - Basic plumbing added to RestryStrategy interface, RetryRequest struct, chain strategy and fixed strategy
     - Moved retry delay computation into Deadline strategy
     - Refactored BatchServiceJobScheduler to invoke retry strategy before deciding whether to retry now, delay, or abort

--- pkg/orchestrator/retry/chain.go
+++ pkg/orchestrator/retry/chain.go
@@ -3,6 +3,7 @@ package retry
 import (
 	"context"
 	"reflect"
+	"time"
 
 	"github.com/bacalhau-project/bacalhau/pkg/orchestrator"
 	"github.com/rs/zerolog/log"
@@ -20,18 +21,20 @@ func (c *Chain) Add(strategies ...orchestrator.RetryStrategy) {
 	c.strategies = append(c.strategies, strategies...)
 }
 
-func (c *Chain) ShouldRetry(ctx context.Context, request orchestrator.RetryRequest) bool {
+func (c *Chain) ShouldRetry(ctx context.Context, request orchestrator.RetryRequest) (bool, time.Duration) {
 	doRetry := false
+	finalDelay := time.Duration(0)
 	for _, strategy := range c.strategies {
-		shouldRetry := strategy.ShouldRetry(ctx, request)
+		shouldRetry, delay := strategy.ShouldRetry(ctx, request)
 		if !shouldRetry {
 			log.Ctx(ctx).Debug().Msgf("retry strategy %s decided not to retry", reflect.TypeOf(strategy).String())
-			return false
+			return false, 0
 		}
 		if shouldRetry {
-			log.Ctx(ctx).Debug().Msgf("retry strategy %s decided okay to retry", reflect.TypeOf(strategy).String())
+			log.Ctx(ctx).Debug().Msgf("retry strategy %s decided okay to retry in %s", reflect.TypeOf(strategy).String(), delay.String())
 			doRetry = true
+			finalDelay = delay
 		}
 	}
-	return doRetry
+	return doRetry, finalDelay
 }
--- pkg/orchestrator/retry/deadline.go
+++ pkg/orchestrator/retry/deadline.go
@@ -2,6 +2,7 @@ package retry
 
 import (
 	"context"
+	"math"
 	"time"
 
 	"github.com/bacalhau-project/bacalhau/pkg/orchestrator"
@@ -17,7 +18,7 @@ func NewDeadlineStrategy(params DeadlineStrategyParams) *DeadlineStrategy {
 	return &DeadlineStrategy{}
 }
 
-func (s *DeadlineStrategy) ShouldRetry(ctx context.Context, request orchestrator.RetryRequest) bool {
+func (s *DeadlineStrategy) ShouldRetry(ctx context.Context, request orchestrator.RetryRequest) (bool, time.Duration) {
 	policy := request.Job.ReschedulingPolicy
 
 	// Retry if the job's scheduling deadline is in the future
@@ -25,12 +26,40 @@ func (s *DeadlineStrategy) ShouldRetry(ctx context.Context, request orchestrator
 	deadline := request.Job.CreateTime + timeoutAsDuration
 	now := time.Now().UTC().UnixNano()
 
-	if deadline > now {
-		log.Debug().Msgf("Retrying job because %d second deadline %d is in the future (from %d)", policy.SchedulingTimeout, deadline, now)
+	secondsUntilDeadline := (deadline - now) / int64(time.Second) // -ve if deadline is past
+
+	if secondsUntilDeadline > 0 {
+		// delay = RetryDelay * RetryDelayGrowthFactor ^ failures
+		// ...but never more than MaximumRetryDelay
+		delay := int64(math.Round(math.Min(
+			float64(policy.BaseRetryDelay)*
+				math.Pow(policy.RetryDelayGrowthFactor,
+					float64(request.PastFailures)),
+			float64(policy.MaximumRetryDelay))))
+
+		if secondsUntilDeadline > delay {
+			log.Debug().Msgf("Deferring job execution for %d seconds (%d * %f ^ %d max %d) as deadline is %d seconds away",
+				delay,
+				policy.BaseRetryDelay,
+				policy.RetryDelayGrowthFactor,
+				request.PastFailures,
+				policy.MaximumRetryDelay,
+				secondsUntilDeadline)
+			return true, time.Duration(delay * int64(time.Second))
+		} else {
+			log.Debug().Msgf("Aborting job because a delay of %d seconds (%d * %f ^ %d max %d) would exceed the deadline of %d seconds",
+				delay,
+				policy.BaseRetryDelay,
+				policy.RetryDelayGrowthFactor,
+				request.PastFailures,
+				policy.MaximumRetryDelay,
+				secondsUntilDeadline)
+			return false, 0
+		}
 	} else {
-		log.Debug().Msgf("Aborting job because %d second deadline %d is in the future (from %d)", policy.SchedulingTimeout, deadline, now)
+		log.Debug().Msgf("Aborting job because deadline is %d seconds ago", -secondsUntilDeadline)
+		return false, 0
 	}
-	return deadline > now
 }
 
 // compile-time interface checks
