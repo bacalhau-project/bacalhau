c96a2591b71c81c69c309e14826fd918e46846b6
Simon Worthington <simon@register-dynamics.co.uk>
2024-03-01 11:53:45 +1100

    Trigger CircleCI builds on releasable tags.

--- .circleci/trigger_pipeline.sh
+++ .circleci/trigger_pipeline.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
-curl -X POST --header "Content-Type: application/json" --header "Circle-Token: ${CIRCLE_TOKEN}" -d "{
+set -xeuo pipefail
+IFS=$'\n\t'
+
+if [[ "${BRANCH}" =~ "refs/tags" ]]; then
+   TAG=$(echo "${BRANCH}" | sed 's:refs/tags/::')
+   TARGET="\"tag\": \"${TAG}\""
+else
+   TARGET="\"branch\": \"${BRANCH}\""
+fi
+
+curl --fail -X POST --header "Content-Type: application/json" --header "Circle-Token: ${CIRCLE_TOKEN}" -d "{
  \"parameters\": {
     \"GHA_Action\": \"trigger_pipeline\"
  },
- \"branch\": \"${BRANCH}\"
+ ${TARGET}
 }" https://circleci.com/api/v2/project/gh/bacalhau-project/bacalhau/pipeline
