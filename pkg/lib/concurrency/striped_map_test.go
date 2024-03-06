a94d11aa520c84ca272156fa931d913217797e9d
Ross Jones <ross@register-dynamics.co.uk>
2024-03-06 14:06:10 +0000

    Update comment on fuzz test

--- pkg/lib/concurrency/striped_map_test.go
+++ pkg/lib/concurrency/striped_map_test.go
@@ -82,30 +82,9 @@ func (s *StripedMapSuite) TestEdgeCases() {
 	s.Require().Equal(3, m.Len())
 }
 
-func subtest(m *cc.StripedMap[int]) {
-	// No longer necessary in Go 1.2 to call Seed
-	// rand.Seed(time.Now().UnixNano())
-
-	min, max := 20, 200
-	iters := rand.Intn(max-min+1) + min
-
-	min, max = 0, 1000
-	for i := 0; i < iters; i++ {
-		putVal := rand.Intn(max-min+1) + min
-		getVal := rand.Intn(max-min+1) + min
-
-		m.Put(strconv.Itoa(i), putVal)
-		_, _ = m.Get(strconv.Itoa(getVal))
-
-		// Occassionally delete some stuff
-		pc := rand.Int31n(100)
-		if pc > 90 {
-			m.Delete(strconv.Itoa(getVal))
-		}
-	}
-}
-
 func (s *StripedMapSuite) TestConcurrent() {
+	// Run lots of go routines doing hundreds of ops as a fuzz test.
+	// This is more of a fuzz test than a proper unit test.
 	wg := sync.WaitGroup{}
 	m := cc.NewStripedMap[int](16)
 
@@ -131,5 +110,28 @@ func (s *StripedMapSuite) TestConcurrent() {
 		return
 	case <-time.After(3 * time.Second):
 		s.Fail("striped map concurrency test took too long to complete, is it blocked?")
+	}
+}
+
+func subtest(m *cc.StripedMap[int]) {
+	// No longer necessary in Go 1.2 to call Seed
+	// rand.Seed(time.Now().UnixNano())
+
+	min, max := 20, 200
+	iters := rand.Intn(max-min+1) + min
+
+	min, max = 0, 1000
+	for i := 0; i < iters; i++ {
+		putVal := rand.Intn(max-min+1) + min
+		getVal := rand.Intn(max-min+1) + min
+
+		m.Put(strconv.Itoa(i), putVal)
+		_, _ = m.Get(strconv.Itoa(getVal))
+
+		// Occassionally delete some stuff
+		pc := rand.Int31n(100)
+		if pc > 90 {
+			m.Delete(strconv.Itoa(getVal))
+		}
 	}
 }
