b574556c34e96acc387c901bf59325a9fb901f91
Ross Jones <ross@register-dynamics.co.uk>
2024-03-12 18:36:02 +0000

    Manually cleanup repo directory to work around flaky window test. (#3602)
    On windows we've been seeing failing tests with directories already
    existing, presumably from previous/parallel runs. This PR switches to
    using a tempdir provided by the test framework so that it is cleaned up
    when the test completes.
    
    Edit: It appears this is a common problem on windows when removing
    directories under certain conditions. We've not moved to manually
    cleaning up the folder.

--- pkg/setup/setup.go
+++ pkg/setup/setup.go
@@ -8,6 +8,7 @@ import (
 	"time"
 
 	"github.com/bacalhau-project/bacalhau/pkg/repo/migrations"
+	"github.com/pkg/errors"
 	"github.com/spf13/viper"
 
 	"github.com/bacalhau-project/bacalhau/pkg/repo"
@@ -50,7 +51,12 @@ func SetupBacalhauRepo(repoDir string) (*repo.FsRepo, error) {
 func SetupBacalhauRepoForTesting(t testing.TB) *repo.FsRepo {
 	viper.Reset()
 
-	path := filepath.Join(os.TempDir(), fmt.Sprint(time.Now().UnixNano()))
+	tmpDir, err := os.MkdirTemp("", "")
+	if err != nil {
+		t.Fatal(errors.Wrap(err, "failed to create temporary directory in test setup"))
+	}
+
+	path := filepath.Join(tmpDir, fmt.Sprint(time.Now().UnixNano()))
 	t.Logf("creating repo for testing at: %s", path)
 	t.Setenv("BACALHAU_ENVIRONMENT", "local")
 	t.Setenv("BACALHAU_DIR", path)
@@ -58,5 +64,13 @@ func SetupBacalhauRepoForTesting(t testing.TB) *repo.FsRepo {
 	if err != nil {
 		t.Fatal(err)
 	}
+
+	t.Cleanup(func() {
+		// This may fail on windows, and if so then we'll log the error but not fail the test.
+		if err := os.RemoveAll(tmpDir); err != nil {
+			t.Logf("failed to clean up repo at: %s: %s", path, err)
+		}
+	})
+
 	return fsRepo
 }
