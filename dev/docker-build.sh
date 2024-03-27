6c2f918856761c1aeb7dbfa634eaacebfa8717af
David Aronchick <aronchick@gmail.com>
2024-03-27 01:35:23 +0000

    updating container

--- dev/docker-build.sh
+++ dev/docker-build.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+# Allow direnv to work in the current shell
+eval "$(direnv hook bash)"
+direnv allow
+
+# Add the current directory to the safe directory list
 git config --global --add safe.directory /workspaces/bacalhau
 python3 --version
 python3 -m ensurepip
@@ -7,4 +12,3 @@ python3 -m pip install uv
 uv venv
 . .venv/bin/activate
 uv pip install poetry
-poetry install --no-root
