feb399ddcbc4a0025c435650f68207479a07f303
js-ts <vedantpadwalinfi@gmail.com>
2024-03-29 23:31:24 +0530

    Improve bashtub test coverage

---
+++ test/version.sh
@@ -0,0 +1,14 @@
+#!bin/bashtub
+
+source bin/bacalhau.sh
+
+testcase_can_list_version() {
+    create_node requester
+    VERSION_INFO=$(curl -s https://get.bacalhau.org/version)
+    EXPECTED_GITVERSION=$(echo "$VERSION_INFO" | jq -r '.version.gitversion')
+    subject bacalhau version --output json
+    ACTUAL_GITVERSION=$(echo $stdout | jq -r '.latestVersion.GitVersion')
+    assert_equal 0 $status
+    assert_equal "$EXPECTED_GITVERSION" "$ACTUAL_GITVERSION"
+    assert_equal '' $stderr
+}
