f577d040ad7cc1b9a631270f40dc4280492b3752
Michael Hoepler <147169582+MichaelHoepler@users.noreply.github.com>
2024-04-11 09:53:26 -0700

    Adjusting and adding redirects

--- docs/redirects.js
+++ docs/redirects.js
@@ -1,4 +1,8 @@
 module.exports = [
     { to: '/integration/python-sdk', from: '/sdk/python-sdk' },
     { to: '/integration/python-sdk', from: '/sdk' },
+    { to: '/setting-up/running-node', from: '/running-node' },
+    { to: '/setting-up/data-ingestion', from: '/data-ingestion' },
+    { to: '/dev/cli-reference', from: '/dev/cli-reference/all-flags' },
+    { to: '/setting-up/data-ingestion', from: '/data-ingestion' },
 ];
