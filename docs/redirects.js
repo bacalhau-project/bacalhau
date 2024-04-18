483c683af501e59c0cd71e25f8fefe8cf4c8d972
Michael Hoepler <147169582+MichaelHoepler@users.noreply.github.com>
2024-04-18 15:38:05 +0200

    Update redirect

--- docs/redirects.js
+++ docs/redirects.js
@@ -1,11 +1,11 @@
 module.exports = [
-    { to: '/integration/python-sdk', from: '/sdk/python-sdk' },
-    { to: '/integration/python-sdk', from: '/sdk' },
+    { to: '/integration/python-sdk/', from: '/sdk/python-sdk/' },
+    { to: '/integration/python-sdk', from: '/sdk/' },
     { to: '/setting-up/running-node', from: '/running-node' },
     { to: '/setting-up/data-ingestion', from: '/data-ingestion' },
     { to: '/dev/cli-reference', from: '/dev/cli-reference/all-flags' },
     { to: '/setting-up/data-ingestion', from: '/data-ingestion' },
     { to: '/dev/other-specifications', from: '/setting-up/other-specifications' },
-    { to: '/dev/other-specifications', from: '/setting-up/other-specifications' },
+    { to: '/dev/other-specifications/publishers/', from: '/setting-up/other-specifications/publishers/ipfs.md' },
     { to: '/setting-up/running-node', from: '/dev/running-node' },
 ];
