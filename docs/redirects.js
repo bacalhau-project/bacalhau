06c2a30ce77957a87cd64831918940cffc2a4c36
Michael Hoepler <147169582+MichaelHoepler@users.noreply.github.com>
2024-04-23 16:04:07 +0200

    Updating redirect

--- docs/redirects.js
+++ docs/redirects.js
@@ -1,11 +1,9 @@
 module.exports = [
     { to: '/integration/python-sdk', from: '/sdk/python-sdk' },
     { to: '/integration/python-sdk', from: '/sdk' },
-    { to: '/setting-up/running-node/quick-start-docker.md', from: '/running-node/quick-start-docker.md' },
     { to: '/setting-up/data-ingestion', from: '/data-ingestion' },
     { to: '/dev/cli-reference', from: '/dev/cli-reference/all-flags' },
     { to: '/setting-up/data-ingestion', from: '/data-ingestion' },
-    { to: '/dev/other-specifications', from: '/setting-up/other-specifications' },
-    { to: '/dev/other-specifications/publishers/', from: '/setting-up/other-specifications/publishers/ipfs.md' },
+    { to: '/dev/other-specifications/publishers/ipfs.md', from: '/setting-up/other-specifications/publishers/ipfs.md' },
     { to: '/setting-up/running-node', from: '/dev/running-node' },
 ];
