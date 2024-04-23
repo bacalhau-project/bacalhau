ec3c043e8f584c736eb9252b8eb8c2333f5d2d82
Michael Hoepler <147169582+MichaelHoepler@users.noreply.github.com>
2024-04-23 15:58:43 +0200

    Fixing all broken links

--- docs/redirects.js
+++ docs/redirects.js
@@ -1,7 +1,7 @@
 module.exports = [
-    { to: '/integration/python-sdk/', from: '/sdk/python-sdk/' },
-    { to: '/integration/python-sdk', from: '/sdk/' },
-    { to: '/setting-up/running-node', from: '/running-node' },
+    { to: '/integration/python-sdk', from: '/sdk/python-sdk' },
+    { to: '/integration/python-sdk', from: '/sdk' },
+    { to: '/setting-up/running-node/quick-start-docker.md', from: '/running-node/quick-start-docker.md' },
     { to: '/setting-up/data-ingestion', from: '/data-ingestion' },
     { to: '/dev/cli-reference', from: '/dev/cli-reference/all-flags' },
     { to: '/setting-up/data-ingestion', from: '/data-ingestion' },
