0558672c29731386d6c79aa3f5af644d2ed1a208
frrist <forrest@expanso.io>
2024-06-11 14:17:11 -0700

    fix: get-result.sh bash test

--- cmd/cli/get/get.go
+++ cmd/cli/get/get.go
@@ -8,7 +8,7 @@ func NewCmd() *cobra.Command {
 	return &cobra.Command{
 		Use:        "get [id]",
 		Short:      "DEPRECATED: Get the results of a job",
-		Deprecated: "This command has moved! Please us `job get` to download results of a job.",
+		Deprecated: "This command has moved! Please use `job get` to download results of a job.",
 		RunE:       func(cmd *cobra.Command, cmdArgs []string) error { return nil },
 	}
 }
