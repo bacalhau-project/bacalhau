43b311c355e857e92109bf48228431ed90d82553
frrist <forrest@expanso.io>
2024-06-17 13:46:26 -0700

    feat: implement UnmarshalJob method with clear errors
    - provids clear errors when a job contains:
      - un-settable fields
      - invalid field types
      - uknow fields
    
    - closes #3719

--- pkg/lib/marshaller/utils.go
+++ pkg/lib/marshaller/utils.go
@@ -2,13 +2,15 @@ package marshaller
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"reflect"
 	"strings"
 
-	"github.com/bacalhau-project/bacalhau/pkg/models"
 	"github.com/c2h5oh/datasize"
 	"sigs.k8s.io/yaml"
+
+	"github.com/bacalhau-project/bacalhau/pkg/models"
 )
 
 type KeyString string
@@ -97,3 +99,90 @@ func normalizeIfApplicable(obj interface{}) {
 		normalizable.Normalize()
 	}
 }
+
+// UnmarshalJob unmarshalled `in` into a models.Job. It returns an error if:
+// - `in` cannot be marshaled to json.
+// - `in` contains an un-settable or unknown field.
+// - `in` cannot be marshaled into a models.Job.
+func UnmarshalJob(in []byte) (*models.Job, error) {
+	// json is a subset of yaml, if `in` is already json this is a noop.
+	jsonBytes, err := yaml.YAMLToJSON(in)
+	if err != nil {
+		return nil, fmt.Errorf("converting yaml to json: %w", err)
+	}
+
+	// unmarshal the job into a generic map
+	var raw map[string]interface{}
+	if err := json.Unmarshal(jsonBytes, &raw); err != nil {
+		return nil, err
+	}
+	// error(s) if un-settable or unknown fields are provided.
+	if err := validateRawJob(raw); err != nil {
+		return nil, err
+	}
+
+	var out *models.Job
+	if err := json.Unmarshal(jsonBytes, &out); err != nil {
+		switch v := err.(type) {
+		// json.UnmarshalTypeError describes a JSON value that as not appropriate for the value of the specified Go type.
+		// e.g. if the Count field was not an int, or the Name field was not a string
+		case *json.UnmarshalTypeError:
+			return nil, fmt.Errorf("field: '%s' in '%s' is invalid type: '%s' expected: '%s'", v.Field, v.Struct, v.Value, v.Type.String())
+		default:
+			return nil, err
+		}
+	}
+	return out, nil
+}
+
+// validateRawJob returns an error if `jobSpec` contains a job field that may not be set by the user or if unknown
+// fields were included in `jobSpec`.
+func validateRawJob(jobSpec map[string]interface{}) error {
+	var mErr error
+	// first check if any un-settable fields were provided.
+	for key := range jobSpec {
+		specKey := strings.ToLower(key)
+		if _, found := disallowedKeys[specKey]; found {
+			mErr = errors.Join(mErr, fmt.Errorf("field: '%s' in 'Job' is not allowed", key))
+		}
+	}
+	// remove all known fields from the provided spec.
+	for key := range jobSpec {
+		specKey := strings.ToLower(key)
+		if _, ok := disallowedKeys[specKey]; ok {
+			delete(jobSpec, key)
+		}
+		if _, ok := allowedKeys[specKey]; ok {
+			delete(jobSpec, key)
+		}
+	}
+	// any fields that remain are considered unknown
+	for key := range jobSpec {
+		mErr = errors.Join(mErr, fmt.Errorf("unknown field: '%s' in 'Job' is ignored", key))
+	}
+	return mErr
+}
+
+var (
+	// disallowedKeys contains system fields that users may not set when providing a job spec.
+	disallowedKeys = map[string]struct{}{
+		"id":         {},
+		"state":      {},
+		"version":    {},
+		"revision":   {},
+		"createtime": {},
+		"modifytime": {},
+	}
+	// allowedKeys contains fields users are permitted to set when providing a job spec.
+	allowedKeys = map[string]struct{}{
+		"name":        {},
+		"namespace":   {},
+		"type":        {},
+		"priority":    {},
+		"count":       {},
+		"constraints": {},
+		"meta":        {},
+		"labels":      {},
+		"tasks":       {},
+	}
+)
