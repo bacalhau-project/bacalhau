202b0c3919022521a5ba0fcf9886f8564b3082bf
David Aronchick <aronchick@gmail.com>
2024-06-18 12:56:52 -0700

    removing flyte

--- scripts/generate_swagger.sh
+++ scripts/generate_swagger.sh
@@ -6,21 +6,20 @@ cd "${PATH_TO_PROJECT_ROOT}" || exit
 
 echo "Currently executing in ${PWD}"
 swag init \
-  --outputTypes "go,json" \
-  --parseDependency \
-  --parseInternal \
-  --parseDepth 1 \
-  -g "./pkg/publicapi/server.go" \
-  --overridesFile .swaggo \
-  --output "${SWAGGER_DIR}"
+--outputTypes "go,json" \
+--parseDependency \
+--parseInternal \
+--parseDepth 1 \
+-g "./pkg/publicapi/server.go" \
+--overridesFile .swaggo \
+--output "${SWAGGER_DIR}"
 
 echo "swagger.json generated - moving from ${SWAGGER_DIR} to ${PUBLIC_PATH}"
 
 # See if the path exists, if not create it
 if [[ ! -d "${PUBLIC_PATH}" ]]; then
-  mkdir -p "${PUBLIC_PATH}"
+    mkdir -p "${PUBLIC_PATH}"
 fi
 mv "${SWAGGER_DIR}/swagger.json" "${PUBLIC_PATH}"
 
 echo "swagger.json also copied to ${PUBLIC_PATH}/swagger.json"
-cp "${PUBLIC_PATH}/swagger.json" "${PATH_TO_PROJECT_ROOT}/docs/swagger.json"
