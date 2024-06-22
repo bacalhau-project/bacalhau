83b35c67bebfe30d12b4b2576693bffb0a4ff8ca
David Aronchick <aronchick@gmail.com>
2024-06-22 13:53:14 -0700

    docker build working

--- docker/build-container/build-and-push.sh
+++ docker/build-container/build-and-push.sh
@@ -2,10 +2,14 @@
 
 set -e
 
-flox push
+CHECKOUT_BRANCH=${CHECKOUT_BRANCH:-main}
 
 # Read the VERSION
 VERSION=$(cat VERSION)
+
+cp ../../go.mod .
+cp ../../go.sum .
+cp ../../requirements.txt .
 
 # Increment the version
 MAJOR=$(echo $VERSION | cut -d. -f1)
@@ -18,4 +22,6 @@ NEW_VERSION="$MAJOR.$MINOR.$PATCH"
 echo $NEW_VERSION > VERSION
 
 # Build the new version container
-docker build --push -t docker.io/bacalhauproject/build-container:$NEW_VERSION .
+docker buildx build --build-arg=BRANCH="${CHECKOUT_BRANCH}" --platform linux/amd64 --push -t docker.io/bacalhauproject/build-container:$NEW_VERSION .
+
+docker pull docker.io/bacalhauproject/build-container:$NEW_VERSION
