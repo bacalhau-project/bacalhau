f13be457b78da823546b01edb3e14006045b9856
David Aronchick <aronchick@gmail.com>
2024-06-23 10:40:41 -0700

    fixing webui building

--- webui/src/reportWebVitals.ts
+++ webui/src/reportWebVitals.ts
@@ -1,15 +1,31 @@
-import { ReportHandler } from "web-vitals"
+// import { ReportHandler } from "web-vitals"
 
-const reportWebVitals = (onPerfEntry?: ReportHandler) => {
-  if (onPerfEntry && onPerfEntry instanceof Function) {
-    import("web-vitals").then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry)
-      getFID(onPerfEntry)
-      getFCP(onPerfEntry)
-      getLCP(onPerfEntry)
-      getTTFB(onPerfEntry)
-    })
+// const reportWebVitals = (onPerfEntry?: ReportHandler) => {
+//   if (onPerfEntry && onPerfEntry instanceof Function) {
+//     import("web-vitals").then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+//       getCLS(onPerfEntry)
+//       getFID(onPerfEntry)
+//       getFCP(onPerfEntry)
+//       getLCP(onPerfEntry)
+//       getTTFB(onPerfEntry)
+//     })
+//   }
+// }
+
+// export default reportWebVitals
+
+import type { Metric } from 'web-vitals';
+
+const reportWebVitals = (onPerfEntry?: (metric: Metric) => void) => {
+  if (onPerfEntry && typeof onPerfEntry === 'function') {
+    import('web-vitals').then(({ onCLS, onFID, onFCP, onLCP, onTTFB }) => {
+      onCLS(onPerfEntry);
+      onFID(onPerfEntry);
+      onFCP(onPerfEntry);
+      onLCP(onPerfEntry);
+      onTTFB(onPerfEntry);
+    });
   }
-}
+};
 
-export default reportWebVitals
+export default reportWebVitals;
