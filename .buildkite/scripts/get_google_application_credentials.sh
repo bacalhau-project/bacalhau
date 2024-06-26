e8f997d64e785ee9ee1f050105f329ca617f1d29
David Aronchick <aronchick@gmail.com>
2024-06-26 13:40:35 -0700

    updating secret getter

--- .buildkite/scripts/get_google_application_credentials.sh
+++ .buildkite/scripts/get_google_application_credentials.sh
@@ -22,16 +22,25 @@ fi
 
 tmp_location="${GOOGLE_APPLICATION_CREDENTIALS:-'/tmp/bacalhau-buildkite-sa@bacalhau-infra.iam.gserviceaccount.com-credentials.json'}"
 
+# Get the first argument of the script - show an error if not set
+if [[ -z "$1" ]]; then
+    critical_error "No secret name for BuildKite agent set"
+fi
+
+secret_name=$1
+
 # If the file doesn't exist, create it
 if [[ ! -f ${tmp_location} ]]; then
     debug_msg "Creating GOOGLE_APPLICATION_CREDENTIALS file at ${tmp_location}"
 
-    # If the GOOGLE_APPLICATION_CREDENTIALS_CONTENT is not set, use critical_error function
-    if [[ -z "${GOOGLE_APPLICATION_CREDENTIALS_CONTENT}" ]]; then
-        critical_error "GOOGLE_APPLICATION_CREDENTIALS_CONTENT is not set"
+    secret_contents=$(buildkite-agent buildkite-agent secret get "${secret_name}")
+
+    # If the secret_contents is not set, use critical_error function
+    if [[ -z "${secret_contents}" ]]; then
+        critical_error "$secret_name is empty or not set"
     fi
 
-    echo "${GOOGLE_APPLICATION_CREDENTIALS_CONTENT}" > "${tmp_location}"
+    echo "${secret_contents}" > "${tmp_location}"
 fi
 
 # Set the GOOGLE_APPLICATION_CREDENTIALS environment variable
