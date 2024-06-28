7a8394d9e970bcafded6ee34ccfacaff40180354
David Aronchick <aronchick@gmail.com>
2024-06-28 15:14:18 -0700

    adding test.sh build script

--- .buildkite/scripts/generate_gcp_credentials.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-# Get .env from the same directory this is in
-. "$(dirname "$0")/.env"
-
-gcloud iam service-accounts keys create "$(dirname "$0")/${GCP_SERVICE_ACCOUNT_EMAIL}-credentials.json" --iam-account="${GCP_SERVICE_ACCOUNT_EMAIL}"
---
+++ .buildkite/scripts/test.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+flox_env="aronchick/bacalhau"
+
+# Check if $_FLOX_ACTIVE_ENVIRONMENTS is set and equal to the desired environment
+if [ -z "${FLOX_ENV_DESCRIPTION:-}" ] || [ "$FLOX_ENV_DESCRIPTION" != "$flox_env" ]; then
+  echo "Activating flox environment: $flox_env"
+
+  # shellcheck disable=SC1090,SC1091,SC2312
+  . <(FLOX_DISABLE_METRICS=true SHELL=$(command -v bash) flox activate -r aronchick/bacalhau -t;)
+else
+  echo "Flox environment $flox_env is already active"
+fi
+
+
+# Set up environment variables
+export LOG_LEVEL='debug'
+export TEST_BUILD_TAGS="${BUILD_TAGS}"
+export TEST_PARALLEL_PACKAGES='4'
+export GOMAXPROCS="2"
+
+# Run tests
+just test-and-report
