3e301d8698d8013184d98f49bc7cba2b32fbb3b7
David Aronchick <aronchick@gmail.com>
2024-06-28 09:46:04 -0700

    fix webui

--- .buildkite/scripts/build_webui.sh
+++ .buildkite/scripts/build_webui.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 shell=$(command -v bash)
-FLOX_DISABLE_METRICS=true SHELL=$shell flox activate -r aronchick/bacalhau -t -- .buildkite/scripts/build-webui
+FLOX_DISABLE_METRICS=true SHELL=$shell flox activate -r aronchick/bacalhau -t -- just build-webui
