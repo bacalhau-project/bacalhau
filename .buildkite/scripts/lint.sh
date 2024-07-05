4205c489dc1c3fc1f41892485d399453c778f6b0
David Aronchick <aronchick@gmail.com>
2024-07-05 13:36:18 -0700

    updating lint.sh

--- .buildkite/scripts/lint.sh
+++ .buildkite/scripts/lint.sh
@@ -1,22 +1,9 @@
 #!/usr/bin/env bash
 
+export SHELL=$(command -v bash)
+. <(FLOX_DISABLE_METRICS=true flox activate -r aronchick/bacalhau -t;);
+
 go env
 
-# If no BUILDKITE_JOB_ID then it's not running in CI
-if [ -z "$BUILDKITE_JOB_ID" ]; then
-    # shellcheck disable=SC1090,SC2312
-    source <(.buildkite/scripts/get_google_application_credentials.sh GOOGLE_APPLICATION_CREDENTIALS_CONTENT)
-
-    # shellcheck disable=SC1090,SC2312
-    source <(./.buildkite/scripts/manage_env_secrets.sh get)
-fi
-
 # shellcheck disable=SC2312
-SHELL=$(command -v bash) FLOX_DISABLE_METRICS=true flox activate -r aronchick/bacalhau \
-                                                        -t -- \
-                                                        pre-commit run --show-diff-on-failure --color=always --all-files
-
-# If no BUILDKITE_JOB_ID then it's not running in CI
-if [ -z "$BUILDKITE_JOB_ID" ]; then
-    buildkite-agent annotate --style "info" --context "lint" --message "Lint passed"
-fi
+pre-commit run --show-diff-on-failure --color=always --all-files
