77db6585e5feb79c554b0eb107497cbf0e6e9a9e
David Aronchick <aronchick@gmail.com>
2024-07-05 09:03:17 -0700

    sleeping

--- .buildkite/scripts/build_webui.sh
+++ .buildkite/scripts/build_webui.sh
@@ -4,20 +4,10 @@ unset LD_LIBRARY_PATH
 export SHELL=$(command -v bash)
 . <(FLOX_DISABLE_METRICS=true flox activate -r aronchick/bacalhau -t;);
 
-# Download vendor and webui/node_modules artifacts
-buildkite-agent artifact download "vendor.tar.gz" .
-buildkite-agent artifact download "webui_node_modules.tar.gz" .
-
-# Extract the artifacts if they exist
-if [ -f vendor.tar.gz ]; then
-    tar -xzf vendor.tar.gz
-fi
-
-if [ -f webui_node_modules.tar.gz ]; then
-    tar -xzf webui_node_modules.tar.gz -C webui/
-fi
-
 just build-webui
+
+# Sleep for 10 minutes
+sleep 600
 
 # Create new artifacts
 tar -czf vendor.tar.gz vendor
