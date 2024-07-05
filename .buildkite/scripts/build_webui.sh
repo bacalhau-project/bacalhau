396da1bad0c717315f6afa4c9269b122f033e83c
David Aronchick <aronchick@gmail.com>
2024-07-05 13:01:49 -0700

    adding tar

--- .buildkite/scripts/build_webui.sh
+++ .buildkite/scripts/build_webui.sh
@@ -5,3 +5,9 @@ export SHELL=$(command -v bash)
 . <(FLOX_DISABLE_METRICS=true flox activate -r aronchick/bacalhau -t;);
 
 just build-webui
+
+# Make the build archive dir if it doesn't exist
+mkdir -p webui/archive
+
+# Create the build archive
+tar -czf webui/build/archive/webui_build.tar.gz -C webui/build
