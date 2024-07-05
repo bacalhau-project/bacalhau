93c89cb7aabe8b5775543450887bda754165685e
David Aronchick <aronchick@gmail.com>
2024-07-04 18:19:34 -0700

    merge

--- .buildkite/scripts/build_webui.sh
+++ .buildkite/scripts/build_webui.sh
@@ -2,5 +2,30 @@
 
 unset LD_LIBRARY_PATH
 
+# Download vendor and webui/node_modules artifacts
+buildkite-agent artifact download "vendor.tar.gz" .
+buildkite-agent artifact download "webui_node_modules.tar.gz" .
+
+# Extract the artifacts if they exist
+if [ -f vendor.tar.gz ]; then
+    tar -xzf vendor.tar.gz
+fi
+
+if [ -f webui_node_modules.tar.gz ]; then
+    tar -xzf webui_node_modules.tar.gz -C webui/
+fi
+
 shell=$(command -v bash)
 FLOX_DISABLE_METRICS=true SHELL=$shell flox activate -r aronchick/bacalhau -t -- just build-webui
+
+# Create new artifacts
+tar -czf vendor.tar.gz vendor
+tar -czf webui_node_modules.tar.gz -C webui/node_modules
+
+# Upload the new artifacts
+buildkite-agent artifact upload vendor.tar.gz
+buildkite-agent artifact upload webui_node_modules.tar.gz
+
+# Upload the build artifacts
+tar -czf webui_build.tar.gz -C webui/build
+buildkite-agent artifact upload webui_build.tar.gz
