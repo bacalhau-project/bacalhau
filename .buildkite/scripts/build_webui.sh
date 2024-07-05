b11b593ea2c209cc2818b5e599c8806863b1ce25
David Aronchick <aronchick@gmail.com>
2024-07-05 09:42:56 -0700

    moving artifacts out of script

--- .buildkite/scripts/build_webui.sh
+++ .buildkite/scripts/build_webui.sh
@@ -5,18 +5,3 @@ export SHELL=$(command -v bash)
 . <(FLOX_DISABLE_METRICS=true flox activate -r aronchick/bacalhau -t;);
 
 just build-webui
-
-# Sleep for 10 minutes
-sleep 600
-
-# Create new artifacts
-tar -czf vendor.tar.gz vendor
-tar -czf webui_node_modules.tar.gz -C webui/node_modules
-
-# Upload the new artifacts
-buildkite-agent artifact upload vendor.tar.gz
-buildkite-agent artifact upload webui_node_modules.tar.gz
-
-# Upload the build artifacts
-tar -czf webui_build.tar.gz -C webui/build
-buildkite-agent artifact upload webui_build.tar.gz
