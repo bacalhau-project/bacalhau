1c12ab64191b679f147aa5ed81bb8338ccd6170b
udsamani <uditpsamani@hotmail.com>
2024-08-02 12:28:14 +0100

    uploading test results

--- buildkite/scripts/test.sh
+++ buildkite/scripts/test.sh
@@ -7,3 +7,19 @@ export BACALHAU_ENVIRONMENT=test
 
 make build-webui
 make test-and-report
+
+curl \
+  -X POST \
+  --fail-with-body \
+  -H "Authorization: Token token=\"$BUILDKITE_ANALYTICS_TOKEN\"" \
+  -F "data=@unittest.xml" \
+  -F "format=junit" \
+  -F "run_env[CI]=buildkite" \
+  -F "run_env[key]=$BUILDKITE_BUILD_ID" \
+  -F "run_env[number]=$BUILDKITE_BUILD_NUMBER" \
+  -F "run_env[job_id]=$BUILDKITE_JOB_ID" \
+  -F "run_env[branch]=$BUILDKITE_BRANCH" \
+  -F "run_env[commit_sha]=$BUILDKITE_COMMIT" \
+  -F "run_env[message]=$BUILDKITE_MESSAGE" \
+  -F "run_env[url]=$BUILDKITE_BUILD_URL" \
+  https://analytics-api.buildkite.com/v1/uploads
