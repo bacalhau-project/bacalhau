e0b59fabb8ebaaa341364ab22e5196bd0e7c9fe5
udsamani <uditpsamani@hotmail.com>
2024-08-02 11:48:26 +0100

    try fix for failing unit tests

--- buildkite/scripts/test.sh
+++ buildkite/scripts/test.sh
@@ -2,7 +2,8 @@
 
 export LOG_LEVEL=DEBUG
 export TEST_BUILD_TAGS=$1
-export TEST_PARALLEL_PACKAGES=4
+export TEST_PARALLEL_PACKAGES=3
+export BACALHAU_ENVIRONMENT=test
 
 make build-webui
 make test-and-report
