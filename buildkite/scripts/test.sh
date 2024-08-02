99126ffdbb629a32aabb374c1ccbeb5235d9b502
udsamani <uditpsamani@hotmail.com>
2024-08-02 13:10:08 +0100

    uploading test results

--- buildkite/scripts/test.sh
+++ buildkite/scripts/test.sh
@@ -4,6 +4,7 @@ export LOG_LEVEL=DEBUG
 export TEST_BUILD_TAGS=$1
 export TEST_PARALLEL_PACKAGES=3
 export BACALHAU_ENVIRONMENT=test
+export BUILDKITE_ANALYTICS_TOKEN=$(buildkite-agent secret get TEST_ANALYTICS)
 
 make build-webui
 make test-and-report
