70a59b78fd31459b4617c1cb46d3cefeee5eb438
udsamani <uditpsamani@hotmail.com>
2024-08-02 10:58:33 +0100

    keys are fixed

--- buildkite/scripts/build_tarball.sh
+++ buildkite/scripts/build_tarball.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+source "${BASH_ENV}"
+echo "$PRIVATE_PEM_B64" | base64 --decode > /tmp/private.pem
+echo "$PUBLIC_PEM_B64" | base64 --decode > /tmp/public.pem
+export PRIVATE_KEY_PASSPHRASE="$(echo $PRIVATE_KEY_PASSPHRASE_B64 | base64 --decode)"
+# Prevent rebuilding web ui, we should have already attached it
+find webui -exec touch -c '{}' +
+
 GOOS=$1 GOARCH=$2 make build-bacalhau-tgz
