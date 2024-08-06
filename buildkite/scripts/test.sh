a707e1a65af329b9a5277ed01350565dfa68949d
udsamani <uditpsamani@hotmail.com>
2024-08-06 10:31:35 +0100

    parallelization of tests try

--- buildkite/scripts/test.sh
+++ buildkite/scripts/test.sh
@@ -2,9 +2,8 @@
 
 export LOG_LEVEL=DEBUG
 export TEST_BUILD_TAGS=$1
-export TEST_PARALLEL_PACKAGES=3
+export TEST_PARALLEL_PACKAGES=8
 export BACALHAU_ENVIRONMENT=test
 
 make build-webui
 make test-and-report
-
