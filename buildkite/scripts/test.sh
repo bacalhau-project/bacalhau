952df7abe4adbf8e28acb3f4c28bece5c250b78e
udsamani <uditpsamani@hotmail.com>
2024-08-08 00:45:47 +0100

    read s3 credentials from buildkite agent

--- buildkite/scripts/test.sh
+++ buildkite/scripts/test.sh
@@ -5,7 +5,7 @@ export TEST_BUILD_TAGS=$1
 export TEST_PARALLEL_PACKAGES=8
 export BACALHAU_ENVIRONMENT=test
 export AWS_ACCESS_KEY_ID=$(buildkite-agent secret get AWS_ACCESS_KEY_ID)
-export AWS_SECRET_ACCESS_KEY=$(buildkite-agent secret get AWS_SECRET_ACCESS_KEY
+export AWS_SECRET_ACCESS_KEY=$(buildkite-agent secret get AWS_SECRET_ACCESS_KEY)
 export AWS_REGION=eu-west-1
 
 make build-webui
