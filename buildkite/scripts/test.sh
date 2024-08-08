1b478b3e7af798656dbd5c22cff1ad8bf5eaaf97
udsamani <uditpsamani@hotmail.com>
2024-08-08 10:55:38 +0100

    testsuite integration via curl

--- buildkite/scripts/test.sh
+++ buildkite/scripts/test.sh
@@ -17,3 +17,20 @@ export BACALHAU_NODE_IPFS_CONNECT=/ip4/127.0.0.1/tcp/5001
 
 make build-webui
 make test-and-report
+
+curl \
+  -X POST \
+  --fail-with-body \
+  --verbose \
+  -H "Authorization: Token token=\"$BUILDKITE_ANALYTICS_TOKEN\"" \
+  -F "data=@junit.xml" \
+  -F "format=junit" \
+  -F "run_env[CI]=buildkite" \
+  -F "run_env[key]=$BUILDKITE_BUILD_ID" \
+  -F "run_env[number]=$BUILDKITE_BUILD_NUMBER" \
+  -F "run_env[job_id]=$BUILDKITE_JOB_ID" \
+  -F "run_env[branch]=$BUILDKITE_BRANCH" \
+  -F "run_env[commit_sha]=$BUILDKITE_COMMIT" \
+  -F "run_env[message]=$BUILDKITE_MESSAGE" \
+  -F "run_env[url]=$BUILDKITE_BUILD_URL" \
+  https://analytics-api.buildkite.com/v1/uploads
