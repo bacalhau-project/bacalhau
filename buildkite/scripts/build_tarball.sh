2b05e8aa7e68a0adfcfb0c5888f58a60e3d7266a
udsamani <uditpsamani@hotmail.com>
2024-08-12 22:06:56 +0100

    artificat upload

--- buildkite/scripts/build_tarball.sh
+++ buildkite/scripts/build_tarball.sh
@@ -7,3 +7,8 @@ export PRIVATE_KEY_PASSPHRASE="$(echo $PRIVATE_KEY_PASSPHRASE_B64 | base64 --dec
 find webui -exec touch -c '{}' +
 
 GOOS=$1 GOARCH=$2 make build-bacalhau-tgz
+
+
+if [ -z "$BUILDKITE_TAG" ]; then
+    buildkite-agent artifact upload dist/bacalhau_*
+fi
