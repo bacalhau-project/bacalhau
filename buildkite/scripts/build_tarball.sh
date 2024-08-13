20bfde057025cda538eec17b90f96c9cd9205fe8
udsamani <uditpsamani@hotmail.com>
2024-08-13 08:41:37 +0100

    release as bart of build itself

--- buildkite/scripts/build_tarball.sh
+++ buildkite/scripts/build_tarball.sh
@@ -3,6 +3,8 @@
 echo "$PRIVATE_PEM_B64" | base64 --decode > /tmp/private.pem
 echo "$PUBLIC_PEM_B64" | base64 --decode > /tmp/public.pem
 export PRIVATE_KEY_PASSPHRASE="$(echo $PRIVATE_KEY_PASSPHRASE_B64 | base64 --decode)"
+export BACALHAU_RELEASE_TOKEN=$(buildkite-agent secret get BACALHAU_RELEASE_TOKEN)
+
 # Prevent rebuilding web ui, we should have already attached it
 find webui -exec touch -c '{}' +
 
@@ -10,7 +12,7 @@ GOOS=$1 GOARCH=$2 make build-bacalhau-tgz
 
 
 if [ -z "$BUILDKITE_TAG" ]; then
-    buildkite-agent artifact upload "dist/bacalhau_*"
-    echo buildkite-agent meta-data set "triggered_build_id" $BUILDKITE_BUILD_ID --build $BUILDKITE_TRIGGERED_FROM_BUILD_ID
-    buildkite-agent meta-data set "triggered_build_id" $BUILDKITE_BUILD_ID --build $BUILDKITE_TRIGGERED_FROM_BUILD_ID
+    cd dist
+    echo $BACALHAU_RELEASE_TOKEN | gh auth login --with-token
+    gh release upload "1.4.1-dev" bacalhau_*
 fi
