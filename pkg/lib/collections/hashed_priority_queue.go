f5ce2e1c40979c1d8128895cb7627464c423b259
frrist <forrest@expanso.io>
2024-09-19 12:28:36 -0700

    fix: lock when accessing internal map
    - fixes #4481

--- pkg/lib/collections/hashed_priority_queue.go
+++ pkg/lib/collections/hashed_priority_queue.go
@@ -27,6 +27,8 @@ func NewHashedPriorityQueue[K comparable, T any](indexer IndexerFunc[K, T]) *Has
 // Contains will return true if the provided identifier (of type K)
 // will be found in this queue, false if it is not present.
 func (q *HashedPriorityQueue[K, T]) Contains(id K) bool {
+	q.mu.Lock()
+	defer q.mu.Unlock()
 	_, ok := q.identifiers[id]
 	return ok
 }
