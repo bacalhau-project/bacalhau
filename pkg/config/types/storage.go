0e042bc3783ec182b1676a1bdb010b31cb0195f3
udsamani <uditpsamani@hotmail.com>
2024-10-01 12:46:43 +0100

    remove s3 storage

--- pkg/config/types/storage.go
+++ pkg/config/types/storage.go
@@ -19,7 +19,6 @@ type InputSourcesConfig struct {
 
 type InputSourcesTypes struct {
 	IPFS IPFSStorage `yaml:"IPFS,omitempty"`
-	S3   S3Storage   `yaml:"S3,omitempty"`
 }
 
 func (i InputSourcesConfig) IsNotDisabled(kind string) bool {
@@ -31,13 +30,4 @@ func (i InputSourcesConfig) IsNotDisabled(kind string) bool {
 type IPFSStorage struct {
 	// Endpoint specifies the multi-address to connect to for IPFS. e.g /ip4/127.0.0.1/tcp/5001
 	Endpoint string `yaml:"Endpoint,omitempty"`
-}
-
-type S3Storage struct {
-	// Endpoint specifies the endpoint URL for the S3 input source.
-	Endpoint string `yaml:"Endpoint,omitempty"`
-	// AccessKey specifies the access key for the S3 input source.
-	AccessKey string `yaml:"AccessKey,omitempty"`
-	// SecretKey specifies the secret key for the S3 input source.
-	SecretKey string `yaml:"SecretKey,omitempty"`
 }
