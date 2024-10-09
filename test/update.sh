409ae9eb98142fca0a4191060274da4b14097577
udsamani <uditpsamani@hotmail.com>
2024-10-09 07:16:56 +0100

    fix update.sh test

--- test/update.sh
+++ test/update.sh
@@ -1,10 +1,10 @@
 #!bin/bashtub
 
-# source bin/bacalhau.sh
+ source bin/bacalhau.sh
 
-# testcase_default_config_has_updates_enabled() {
-#     subject bacalhau config list --output=csv
-#     assert_equal 0 $status
-#     assert_not_equal $(echo "$stdout" | grep 'update.checkfrequency' | cut -d, -f2) '0'
-#     assert_not_equal $(echo "$stdout" | grep 'update.checkfrequency' | cut -d, -f2) ''
-# }
+ testcase_default_config_has_updates_enabled() {
+     subject bacalhau config list --output=csv
+     assert_equal 0 $status
+     assert_not_equal $(echo "$stdout" | grep 'UpdateConfig.Interval' | cut -d, -f2) '0'
+     assert_not_equal $(echo "$stdout" | grep 'UpdateConfig.Interval' | cut -d, -f2) ''
+ }
