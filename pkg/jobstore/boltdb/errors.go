d42d027ff2825f9c9eb040fdd6322e9cc27de443
jamlo <jamilshamy@gmail.com>
2024-10-23 09:34:38 -0400

    Use more idiomatic Go to print error messages
    Linear Issue: https://linear.app/expanso/issue/ENG-267/lint-is-failing-but-build-is-still-passing
    Github Issue: #4605

--- pkg/jobstore/boltdb/errors.go
+++ pkg/jobstore/boltdb/errors.go
@@ -24,38 +24,38 @@ const (
 func NewBoltDBError(err error) bacerrors.Error {
 	switch {
 	case errors.Is(err, bbolt.ErrBucketNotFound):
-		return bacerrors.New("%s", err.Error()).
+		return bacerrors.New("%s", err).
 			WithCode(BoltDBBucketNotFound).
 			WithHTTPStatusCode(http.StatusNotFound).
 			WithComponent(BoltDBComponent)
 	case errors.Is(err, bbolt.ErrBucketExists):
-		return bacerrors.New("%s", err.Error()).
+		return bacerrors.New("%s", err).
 			WithCode(BoltDBBucketExists).
 			WithHTTPStatusCode(http.StatusConflict).
 			WithComponent(BoltDBComponent)
 	case errors.Is(err, bbolt.ErrTxNotWritable):
-		return bacerrors.New("%s", err.Error()).
+		return bacerrors.New("%s", err).
 			WithCode(BoltDBTxNotWritable).
 			WithHTTPStatusCode(http.StatusInternalServerError).
 			WithComponent(BoltDBComponent)
 	case errors.Is(err, bbolt.ErrIncompatibleValue):
-		return bacerrors.New("%s", err.Error()).
+		return bacerrors.New("%s", err).
 			WithCode(BoltDBIncompatibleValue).
 			WithComponent(BoltDBComponent)
 	case errors.Is(err, bbolt.ErrKeyRequired):
-		return bacerrors.New("%s", err.Error()).
+		return bacerrors.New("%s", err).
 			WithCode(BoltDBKeyRequired).
 			WithComponent(BoltDBComponent)
 	case errors.Is(err, bbolt.ErrKeyTooLarge):
-		return bacerrors.New("%s", err.Error()).
+		return bacerrors.New("%s", err).
 			WithCode(BoltDBKeyTooLarge).
 			WithComponent(BoltDBComponent)
 	case errors.Is(err, bbolt.ErrValueTooLarge):
-		return bacerrors.New("%s", err.Error()).
+		return bacerrors.New("%s", err).
 			WithCode(BoltDBValueTooLarge).
 			WithComponent(BoltDBComponent)
 	default:
-		return bacerrors.New("%s", err.Error()).
+		return bacerrors.New("%s", err).
 			WithCode(bacerrors.BadRequestError).
 			WithComponent(BoltDBComponent)
 	}
