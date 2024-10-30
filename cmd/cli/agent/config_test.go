32444d545a8ed85b9d970aae077069f37cfa671f
frrist <forrest@expanso.io>
2024-10-30 14:44:27 -0700

    fix mistake in config_test

--- cmd/cli/agent/config_test.go
+++ cmd/cli/agent/config_test.go
@@ -3,14 +3,15 @@
 package agent_test
 
 import (
+	"encoding/json"
 	"testing"
 
 	"github.com/stretchr/testify/suite"
+	"gopkg.in/yaml.v3"
 
 	cmdtesting "github.com/bacalhau-project/bacalhau/cmd/testing"
 	"github.com/bacalhau-project/bacalhau/cmd/util/output"
-	"github.com/bacalhau-project/bacalhau/pkg/lib/marshaller"
-	"github.com/bacalhau-project/bacalhau/pkg/publicapi/apimodels"
+	"github.com/bacalhau-project/bacalhau/pkg/config/types"
 )
 
 func TestConfigSuite(t *testing.T) {
@@ -22,23 +23,24 @@ type ConfigSuite struct {
 }
 
 func (s *ConfigSuite) TestConfigJSONOutput() {
-	_, out, err := s.ExecuteTestCobraCommand("agent", "config",
-		"--output", string(output.JSONFormat),
+	_, out, err := s.ExecuteTestCobraCommand(
+		"agent", "config", "--output", string(output.JSONFormat), "--pretty=false",
 	)
 	s.Require().NoError(err, "Could not request config with json output.")
 
-	resp := &apimodels.GetAgentConfigResponse{}
-	err = marshaller.JSONUnmarshalWithMax([]byte(out), &resp)
+	var cfg types.Bacalhau
+	err = json.Unmarshal([]byte(out), &cfg)
 	s.Require().NoError(err, "Could not unmarshal the output into json - %+v", err)
-	s.Require().True(resp.Config.Orchestrator.Enabled)
+	s.Require().True(cfg.Orchestrator.Enabled)
 }
 
 func (s *ConfigSuite) TestConfigYAMLOutput() {
+	// NB: the default output is yaml, thus we don't specify it here.
 	_, out, err := s.ExecuteTestCobraCommand("agent", "config")
 	s.Require().NoError(err, "Could not request config with yaml output.")
 
-	resp := &apimodels.GetAgentConfigResponse{}
-	err = marshaller.YAMLUnmarshalWithMax([]byte(out), &resp)
+	var cfg types.Bacalhau
+	err = yaml.Unmarshal([]byte(out), &cfg)
 	s.Require().NoError(err, "Could not unmarshal the output into yaml - %+v", out)
-	s.Require().True(resp.Config.Orchestrator.Enabled)
+	s.Require().True(cfg.Orchestrator.Enabled)
 }
