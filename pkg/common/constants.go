ca5786a12cdbd28f791dee39905a8f31379abe09
jamlo <jamilshamy@gmail.com>
2025-03-19 17:49:44 -0400

    fix linting

--- pkg/common/constants.go
+++ pkg/common/constants.go
@@ -1,5 +1,6 @@
 package common
 
+//nolint:gosec // G101: Potential hardcoded credentials
 const BacalhauAPIKey = "BACALHAU_API_KEY"
 const BacalhauAPIUsername = "BACALHAU_API_USERNAME"
 const BacalhauAPIPassword = "BACALHAU_API_PASSWORD"
