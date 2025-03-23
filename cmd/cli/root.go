bacalhau-project/bacalhau

commit e1b784a283ce9db8bed0b83fffac49c51540e0d9

cleanup dead config code (#4874)
This PR removes legacy config that existed prior to v1.5, clean up code
that depends on those configs, and removes support for migrating configs
and data directories in preparation for v1.7

<!-- This is an auto-generated comment: release notes by coderabbit.ai
-->
## Summary by CodeRabbit

- **Configuration Improvements**
- Renamed and consolidated environment variables (e.g., IPFS
connectivity) and removed deprecated flags to simplify command-line
usage.
- **Docker & Credential Enhancements**
- Centralized Docker credential management for smoother image pulling
and distribution.
- **Migration & Repository Updates**
- Refined migration processes and version handling to improve system
stability and streamline repository setup.
- **Testing & Documentation Updates**
- Updated tests to utilize new configuration structures, enhancing
reliability and maintainability.

These internal refinements deliver a more consistent and maintainable
experience without altering core functionality for users.
<!-- end of auto-generated comment: release notes by coderabbit.ai -->

cmd/cli/root.go
@@ -47,9 +47,8 @@ func NewRootCmd() *cobra.Command {
 	// when these flags are provided their value will be used instead of the value present in the config file.
 	// If no flg is provided, and the config file doesn't have a value defined then the default value will be used.
 	rootFlags := map[string][]configflags.Definition{
-		"api":     configflags.ClientAPIFlags,
-		"logging": configflags.LogFlags,
-		"repo":    configflags.DataDirFlag,
+		"api":  configflags.ClientAPIFlags,
+		"repo": configflags.DataDirFlag,
 	}
 	// register the flags on the command.
 	if err := configflags.RegisterFlags(RootCmd, rootFlags); err != nil {

cmd/util/flags/configflags/logging.go
@@ -1,26 +1,0 @@
-package configflags
-
-import (
-	"github.com/bacalhau-project/bacalhau/pkg/config"
-	"github.com/bacalhau-project/bacalhau/pkg/config/types"
-)
-
-var LogFlags = []Definition{
-	{
-		FlagName:          "log-mode",
-		DefaultValue:      config.Default.Logging.Mode,
-		ConfigPath:        types.LoggingModeKey,
-		Description:       `Log format: 'default','station','json','combined','event'`,
-		Deprecated:        true,
-		DeprecatedMessage: makeDeprecationMessage(types.LoggingModeKey),
-	},
-	{
-		FlagName:             "log-level",
-		DefaultValue:         config.Default.Logging.Level,
-		ConfigPath:           types.LoggingLevelKey,
-		Description:          `Log level: 'trace', 'debug', 'info', 'warn', 'error', 'fatal', 'panic'`,
-		EnvironmentVariables: []string{"LOG_LEVEL"},
-		Deprecated:           true,
-		DeprecatedMessage:    makeDeprecationMessage(types.LoggingLevelKey),
-	},
-}

commit 551a374f8388b4265edf6fdd57d7284d2e08aed7

fix regression in not setting log level (#4910)
Fix regression introduced in
https://github.com/bacalhau-project/bacalhau/pull/4874 where `LOG_LEVEL`
env var stopped working

<!-- This is an auto-generated comment: release notes by coderabbit.ai
-->

## Summary by CodeRabbit

- **New Features**
- Introduced a new command-line option to configure logging. Users can
now set log levels and related parameters directly via CLI flags and
environment variables, further enhancing the application's
configurability while preserving existing options.

<!-- end of auto-generated comment: release notes by coderabbit.ai -->

cmd/cli/root.go
@@ -6,10 +6,11 @@ import (
 	"os"
 	"strings"
 
-	"github.com/bacalhau-project/bacalhau/cmd/cli/auth"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 	"go.opentelemetry.io/otel/trace"
+
+	"github.com/bacalhau-project/bacalhau/cmd/cli/auth"
 
 	"github.com/bacalhau-project/bacalhau/cmd/cli/agent"
 	configcli "github.com/bacalhau-project/bacalhau/cmd/cli/config"
@@ -48,8 +49,9 @@ func NewRootCmd() *cobra.Command {
 	// when these flags are provided their value will be used instead of the value present in the config file.
 	// If no flg is provided, and the config file doesn't have a value defined then the default value will be used.
 	rootFlags := map[string][]configflags.Definition{
-		"api":  configflags.ClientAPIFlags,
-		"repo": configflags.DataDirFlag,
+		"api":     configflags.ClientAPIFlags,
+		"logging": configflags.LogFlags,
+		"repo":    configflags.DataDirFlag,
 	}
 	// register the flags on the command.
 	if err := configflags.RegisterFlags(RootCmd, rootFlags); err != nil {

cmd/util/flags/configflags/logging.go
@@ -1,0 +1,31 @@
+package configflags
+
+import (
+	"fmt"
+
+	"github.com/bacalhau-project/bacalhau/pkg/config"
+	"github.com/bacalhau-project/bacalhau/pkg/config/types"
+)
+
+// TODO: remove this once we have a proper way to register LOG_LEVEL env var into the config.
+//
+//	Currently we utilize cli flags to also register env vars.
+var LogFlags = []Definition{
+	{
+		FlagName:             "log-level",
+		DefaultValue:         config.Default.Logging.Level,
+		ConfigPath:           types.LoggingLevelKey,
+		Description:          `Log level: 'trace', 'debug', 'info', 'warn', 'error', 'fatal', 'panic'`,
+		EnvironmentVariables: []string{"LOG_LEVEL"},
+		Deprecated:           true,
+		DeprecatedMessage:    makeDeprecationMessage(types.LoggingLevelKey),
+	},
+}
+
+func makeDeprecationMessage(key string) string {
+	return fmt.Sprintf("Use %s to set this configuration", makeConfigFlagDeprecationCommand(key))
+}
+
+func makeConfigFlagDeprecationCommand(key string) string {
+	return fmt.Sprintf("--config %s=<value>", key)
+}