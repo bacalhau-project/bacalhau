ce47e8c02733922137dea1b852ff5f3d441a77ff
oleg_k <olgk.work@proton.me>
2025-04-22 09:57:55 -0700

    Ignore webui build artifacts

--- webui/build/_next/static/chunks/app/not-found-6aa57d4950591b77.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[160],{56841:function(n,e,u){Promise.resolve().then(u.t.bind(u,72972,23))}},function(n){n.O(0,[972,971,117,744],function(){return n(n.s=56841)}),_N_E=n.O()}]);
