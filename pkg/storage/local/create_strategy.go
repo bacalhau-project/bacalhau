88910e5d06137d25e92bb779216d32cee8750e94
oleg_k <olgk.work@proton.me>
2025-04-29 14:16:31 -0700

    Make nocreate to be the default create strategy in local input provider

--- pkg/storage/local/create_strategy.go
+++ pkg/storage/local/create_strategy.go
@@ -1,19 +1,14 @@
 package local
 
 import (
-	"path/filepath"
 	"strings"
 
 	"github.com/bacalhau-project/bacalhau/pkg/bacerrors"
-	"github.com/rs/zerolog/log"
 )
 
 type CreateStrategy string
 
 const (
-	// Try to infer the type (file vs directory) from path
-	Infer CreateStrategy = "infer"
-
 	// Create as directory
 	Dir CreateStrategy = "dir"
 
@@ -24,12 +19,11 @@ const (
 	NoCreate CreateStrategy = "nocreate"
 )
 
-const DefaultCreateStrategy = Infer
+const DefaultCreateStrategy = NoCreate
 const CreateStrategySpecKey = "CreateAs"
 
 func KnownCreateStrategies() []string {
 	return []string{
-		Infer.String(),
 		Dir.String(),
 		File.String(),
 		NoCreate.String(),
@@ -38,7 +32,6 @@ func KnownCreateStrategies() []string {
 
 func PermissiveCreateStrategies() []string {
 	return []string{
-		Infer.String(),
 		Dir.String(),
 		File.String(),
 	}
@@ -46,8 +39,6 @@ func PermissiveCreateStrategies() []string {
 
 func CreateStrategyFromString(s string) (CreateStrategy, error) {
 	switch s {
-	case Infer.String():
-		return Infer, nil
 	case Dir.String():
 		return Dir, nil
 	case File.String():
@@ -58,30 +49,10 @@ func CreateStrategyFromString(s string) (CreateStrategy, error) {
 		return DefaultCreateStrategy, nil
 	default:
 		// TODO: Create a constant for JobSpec to be used in WithComponent for this and similar errors
-		return "", bacerrors.Newf("invalid CreateAs value %s", s).
+		return "", bacerrors.Newf("invalid %s value %s", CreateStrategySpecKey, s).
 			WithHint("%s must be one of [%s]", CreateStrategySpecKey, strings.Join(KnownCreateStrategies(), ", ")).
 			WithCode(bacerrors.ValidationError)
 	}
-}
-
-// Attempts to infer whether the given path represents a file or directory.
-// This is a best-effort attempt based on common conventions.
-func InferCreateStrategyFromPath(path string) CreateStrategy {
-	// Leverage filepath.Split, it handles edge cases like trailing slashes, no slashes, etc.
-	// For now this is smart enough, but we can improve it later if needed.
-	// Note that there are some noticeable exceptions for which this will return the wrong strategy,
-	// For example folders with a dot in the name (e.g. /etc/conf.d) and without a trailing slash
-	// will be considered files. However such paths are likely to be non-empty and CreateStrategy will not be called for them.
-	// Additionally, we can look at Target value to see if it gives more insight on whether we should create a file or directory.
-	_, file := filepath.Split(path)
-	var inferredStrategy CreateStrategy
-	if file == "" {
-		inferredStrategy = Dir
-	} else {
-		inferredStrategy = File
-	}
-	log.Debug().Str("path", path).Msgf("inferred create strategy: %s", inferredStrategy)
-	return inferredStrategy
 }
 
 func (c CreateStrategy) String() string {
--- pkg/storage/local/create_strategy_test.go
+++ pkg/storage/local/create_strategy_test.go
@@ -8,7 +8,7 @@ import (
 )
 
 func TestKnownCreateStrategies(t *testing.T) {
-	expected := []string{Infer.String(), Dir.String(), File.String(), NoCreate.String()}
+	expected := []string{Dir.String(), File.String(), NoCreate.String()}
 	actual := KnownCreateStrategies()
 
 	assert.Equal(t, expected, actual, "AllowedCreateStrategies should return all valid strategies")
@@ -22,12 +22,6 @@ func TestCreateStrategyFromString(t *testing.T) {
 		expectError   bool
 		errorContains string
 	}{
-		{
-			name:        "infer strategy",
-			input:       "infer",
-			expected:    Infer,
-			expectError: false,
-		},
 		{
 			name:        "directory strategy",
 			input:       "dir",
@@ -72,57 +66,6 @@ func TestCreateStrategyFromString(t *testing.T) {
 				require.NoError(t, err)
 				assert.Equal(t, tc.expected, strategy)
 			}
-		})
-	}
-}
-
-func TestInferCreateStrategyFromPath(t *testing.T) {
-	testCases := []struct {
-		name     string
-		path     string
-		expected CreateStrategy
-	}{
-		{
-			name:     "empty path should be directory",
-			path:     "",
-			expected: Dir,
-		},
-		{
-			name:     "path with trailing slash should be directory",
-			path:     "/path/to/dir/",
-			expected: Dir,
-		},
-		{
-			name:     "path without trailing slash should be file",
-			path:     "/path/to/file",
-			expected: File,
-		},
-		{
-			name:     "path with extension should be file",
-			path:     "/path/to/file.txt",
-			expected: File,
-		},
-		{
-			name:     "root directory should be directory",
-			path:     "/",
-			expected: Dir,
-		},
-		{
-			name:     "relative path to file",
-			path:     "file.txt",
-			expected: File,
-		},
-		{
-			name:     "relative path to directory with trailing slash",
-			path:     "dir/",
-			expected: Dir,
-		},
-	}
-
-	for _, tc := range testCases {
-		t.Run(tc.name, func(t *testing.T) {
-			strategy := InferCreateStrategyFromPath(tc.path)
-			assert.Equal(t, tc.expected, strategy)
 		})
 	}
 }
